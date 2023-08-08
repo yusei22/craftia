48c95ff945577d76725c9ff14a691f713fe533b2
PineBokkuri <126656209+PineBokkuri@users.noreply.github.com>
2023-08-08 09:43:09 +0900

    update:#5 簡易リセットcss追加
    update:#34 Themeの設定追加

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,26 @@
+import { withThemeFromJSXProvider } from '@storybook/addon-styling';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { theme } from '../src/themes'
 import type { Preview } from "@storybook/react";
-
-const preview: Preview = {
+const GlobalStyles = createGlobalStyle`
+  html,
+  body,
+  textarea {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  a {
+    text-decoration: none;
+    transition: .25s;
+    color: #000000;
+  }
+`
+export const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
@@ -11,5 +31,13 @@ const preview: Preview = {
     },
   },
 };
-
-export default preview;
+export const decorators = [
+  withThemeFromJSXProvider({
+    GlobalStyles, // Adds your GlobalStyle component to all stories
+    themes: {
+      light: theme,
+    },
+    defaultTheme: 'light',
+    Provider: ThemeProvider,
+  }),
+];
