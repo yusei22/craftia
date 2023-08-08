6d2d09cb27ddf01a770e9bab8a985c5c5fb491dd
PineBokkuri <126656209+PineBokkuri@users.noreply.github.com>
2023-08-08 09:42:18 +0900

    update:#21babelとwebpackの設定追加

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const path = require('path')
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
@@ -6,6 +8,7 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    '@storybook/addon-styling',
   ],
   framework: {
     name: "@storybook/nextjs",
@@ -14,6 +17,22 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
-  staticDirs: ['public']
+  staticDirs: ['public'],
+  babel: async options => ({
+    ...options,
+    plugins: [
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-private-methods',
+      '@babel/plugin-proposal-private-property-in-object',
+    ],
+  }),
+  webpackFinal: async (config:any) => {
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, '../tsconfig.json')
+      }),
+    ];
+    return config
+  },
 };
 export default config;
