880f40b4b4c0a65757f67abfe45632b108e1bd54
PineBokkuri <126656209+PineBokkuri@users.noreply.github.com>
2023-08-10 21:20:25 +0900

    update: Meta,StoryObjに変更
    ComponentMeta, ComponentStoryが非推奨になったので

--- src/components/atoms/Text/index.stories.tsx
+++ src/components/atoms/Text/index.stories.tsx
@@ -1,138 +1,15 @@
-import { ComponentMeta, ComponentStory } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react';
 import Text from './index'
 
-export default {
+const meta = {
   title: 'Atoms/Text',
-  argTypes: {
-    $variant: {
-      options: [
-        'extraSmall',
-        'small',
-        'medium',
-        'mediumLarge',
-        'large',
-        'extraLarge',
-      ],
-      control: { type: 'select' },
-      defaultValue: 'medium',
-      // docsに表示する内容を設定
-      description: 'テキストバリアント',
-      table: {
-        type: {
-          summary: 'extraSmall , small, medium, mediumLarge, large, extraLarge',
-        },
-        defaultValue: { summary: 'medium' },
-      },
-    },
-    children: {
-      control: { type: 'text' },
-      description: 'テキスト',
-      table: {
-        type: { summary: 'string' },
-      },
-    },
-    $fontWeight: {
-      control: { type: 'text' },
-      description: 'フォントの太さ',
-      table: {
-        type: { summary: 'string' },
-      },
-    },
-    lineHeight: {
-      control: { type: 'text' },
-      description: '行の高さ',
-      table: {
-        type: { summary: 'string' },
-      },
-    },
-    color: {
-      control: { type: 'color' },
-      description: 'テキストの色',
-      table: {
-        type: { summary: 'string' },
-      },
-    },
-    backgroundColor: {
-      control: { type: 'color' },
-      description: '背景色',
-      table: {
-        type: { summary: 'string' },
-      },
-    },
-    m: {
-      control: { type: 'number' },
-      description: 'マージン',
-      table: {
-        type: { summary: 'number' },
-      },
-    },
-    mt: {
-      control: { type: 'number' },
-      description: 'マージントップ',
-      table: {
-        type: { summary: 'number' },
-      },
-    },
-    mr: {
-      control: { type: 'number' },
-      description: 'マージンライト',
-      table: {
-        type: { summary: 'number' },
-      },
-    },
-    mb: {
-      control: { type: 'number' },
-      description: 'マージンボトム',
-      table: {
-        type: { summary: 'number' },
-      },
-    },
-    ml: {
-      control: { type: 'number' },
-      description: 'マージンレフト',
-      table: {
-        type: { summary: 'number' },
-      },
-    },
-    p: {
-      control: { type: 'number' },
-      description: 'パディング',
-      table: {
-        type: { summary: 'number' },
-      },
-    },
-    pt: {
-      control: { type: 'number' },
-      description: 'パディングトップ',
-      table: {
-        type: { summary: 'number' },
-      },
-    },
-    pr: {
-      control: { type: 'number' },
-      description: 'パディングライト',
-      table: {
-        type: { summary: 'number' },
-      },
-    },
-    pb: {
-      control: { type: 'number' },
-      description: 'パディングボトム',
-      table: {
-        type: { summary: 'number' },
-      },
-    },
-    pl: {
-      control: { type: 'number' },
-      description: 'パディングレフト',
-      table: {
-        type: { summary: 'number' },
-      },
-    },
-  },
-} as ComponentMeta<typeof Text>
+  component: Text,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Text>;
 
-const Template: ComponentStory<typeof Text> = (args) => <Text {...args} />
+export default meta;
+
+type Story = StoryObj<typeof Text>;
 
 const longText = `It is a long established fact that a reader will be
 distracted by the readable content of a page when looking at its layout.
@@ -144,20 +21,42 @@ search for lorem ipsum will uncover many web sites still in their infancy.
 Various versions have evolved over the years, sometimes by accident,
 sometimes on purpose(injected humour and the like).`
 
-export const ExtraSmall = Template.bind({})
-ExtraSmall.args = { $variant: 'extraSmall', children: longText }
+export const ExtraSmall:Story={
+  args:{
+    children: longText,
+    $variant:'extraSmall'
+  }
+}
+export const Small :Story={
+  args:{
+    children: longText,
+    $variant:'small'
+  }
+}
 
-export const Small = Template.bind({})
-Small.args = { $variant: 'small', children: longText }
+export const Medium:Story={
+  args:{
+    children: longText,
+    $variant:'medium'
+  }
+}
+export const MediumLarge:Story={
+  args:{
+    children: longText,
+    $variant:'mediumLarge'
+  }
+}
 
-export const Medium = Template.bind({})
-Medium.args = { $variant: 'medium', children: longText }
+export const Large:Story={
+  args:{
+    children: longText,
+    $variant:'large'
+  }
+}
 
-export const MediumLarge = Template.bind({})
-MediumLarge.args = { $variant: 'mediumLarge', children: longText }
-
-export const Large = Template.bind({})
-Large.args = { $variant: 'large', children: longText }
-
-export const ExtraLarge = Template.bind({})
-ExtraLarge.args = { $variant: 'extraLarge', children: longText }
+export const ExtraLarge:Story={
+  args:{
+    children: longText,
+    $variant:'extraLarge'
+  }
+}
