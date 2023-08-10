9808bb3f57008233cd4ad79880cfcb13e0dd2b36
PineBokkuri <126656209+PineBokkuri@users.noreply.github.com>
2023-08-10 21:18:47 +0900

    update:#20 ボタンのバリアント追加

--- src/components/atoms/Button/index.stories.tsx
+++ src/components/atoms/Button/index.stories.tsx
@@ -12,8 +12,26 @@ export default meta;
 type Story = StoryObj<typeof Button>;
 export const Primary: Story={
   args:{
-    children:'Primary',
-    $variant:'primary'
+    children:'ボタン',
+    $variant:'primary',
   }
 }
 
+export const Warn:Story={
+  args:{
+    children:'⚠警告',
+    $variant:'warn',
+  }
+}
+export const Danger:Story={
+  args:{
+    children:'危険！',
+    $variant:'danger',
+  }
+}
+export const Dark:Story={
+  args:{
+    children:'ファイル',
+    $variant:'dark',
+  }
+}
