5d3d8a6706a2b2f332aeae736461554e50851e63
PineBokkuri <126656209+PineBokkuri@users.noreply.github.com>
2023-08-10 21:21:25 +0900

    add:ウィンドウイベントのカスタムフック

---
+++ src/hooks/useWindowEvent.ts
@@ -0,0 +1,18 @@
+import { DependencyList, useEffect } from "react";
+
+const useWindowEvent = <K extends keyof WindowEventMap>(
+    type: K,
+    listener: (this: Window, ev: WindowEventMap[K]) => any,
+    deps: DependencyList,
+    options?: boolean | AddEventListenerOptions
+) =>
+    useEffect(() => {
+        if (window) {
+            window.addEventListener(type, listener, options);
+            return () => {
+                window.removeEventListener(type, listener, options);
+            };
+        }
+    }, deps)
+
+export default useWindowEvent;
