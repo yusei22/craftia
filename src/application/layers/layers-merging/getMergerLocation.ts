00e9e9d6f9ee341758c14634feeaa4aba057bed3
PineBokkuri <126656209+PineBokkuri@users.noreply.github.com>
2023-08-15 23:21:16 +0900

    add:レイヤーマージするときのサイズと位置決める関数

---
+++ src/application/layers/layers-merging/getMergerLocation.ts
@@ -0,0 +1,15 @@
+import { Vec2 } from "../../units";
+import { Layer } from "../../types";
+
+function getMergerLocation(layers: Layer[]): Vec2 {
+    if (layers.length <= 0) return new Vec2(0, 0)
+    let minLocationX: number = layers[0].settings.globalLocation.x;
+    let minLocationY: number = layers[0].settings.globalLocation.y;
+
+    for (let i = 1; i < layers.length; i++) {
+        minLocationX = Math.min(minLocationX, layers[i].settings.globalLocation.x);
+        minLocationY = Math.min(minLocationY, layers[i].settings.globalLocation.y);
+    }
+    return new Vec2(minLocationX, minLocationY);
+}
+export { getMergerLocation };
