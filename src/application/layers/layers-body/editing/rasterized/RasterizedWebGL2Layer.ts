0f7a1e562acb35dbb410c94eeb3e824855cc95cf
PineBokkuri <126656209+PineBokkuri@users.noreply.github.com>
2023-08-15 23:19:26 +0900

    update:#20 `clear`メゾットと`destroy`メゾットを抽象クラスのほうに移動
    update:#49 `viewport`メゾットを`viewportCanvas`に名称変更

--- src/application/layers/layers-body/editing/rasterized/RasterizedWebGL2Layer.ts
+++ src/application/layers/layers-body/editing/rasterized/RasterizedWebGL2Layer.ts
@@ -1,8 +1,7 @@
 import { LayerSettings } from "../../../layer-settings/LayerSettings";
 import { SystemLayerSettings } from "../../../layer-settings/SystemLayerSettings";
 import { WebGL2Layer } from "../WebGL2Layer";
-import { Vec2 } from "../../../../units";
-import { Vec4 } from "../../../../units";
+import { Vec2} from "../../../../units";
 import { createRasterizedImgBitmapLayer } from "../../rasterized/RasterizedImgBitmapLayer";
 import { createRasterizedImgElementLayer } from "../../rasterized/RasterizedImgElementLayer";
 import { createSmartImgBitmapLayer } from "../../smart-object/SmartImgBitmapLayer";
@@ -17,28 +16,36 @@ class RasterizedWebGL2Layer extends WebGL2Layer {
         super();
         this.settings = settings.clone();
     }
-    public clear(color?: Vec4): void {
-        super.clear(color);
-    }
-    public destroy(): void {
-        super.destroy();
-    }
-    public viewport(size: Vec2): void {
+    public viewportCanvas(size: Vec2): void {
         super.viewport(size);
     }
     public createRasterizedImgBitmapLayer() {
-        return createRasterizedImgBitmapLayer(this.canvas, this.settings);
+        return createRasterizedImgBitmapLayer(
+            this.canvas,
+            this.settings
+        );
     }
     public createRasterizedImgElementLayer() {
         const dataURL = this.canvas.toDataURL();
-        return createRasterizedImgElementLayer(dataURL, this.settings);
+        return createRasterizedImgElementLayer(
+            dataURL,
+            this.settings
+        );
     }
     public createSmartImgBitmapLayer() {
-        return createSmartImgBitmapLayer(this.canvas, this.settings, this.systemSettings.resize);
+        return createSmartImgBitmapLayer(
+            this.canvas,
+            this.settings,
+            this.systemSettings.resize
+        );
     }
     public createSmartImgElementLayer() {
         const dataURL = this.canvas.toDataURL();
-        return createSmartImgElementLayer(dataURL, this.settings, this.systemSettings.resize);
+        return createSmartImgElementLayer(
+            dataURL,
+            this.settings,
+            this.systemSettings.resize
+        );
     }
 }
 export { RasterizedWebGL2Layer }
