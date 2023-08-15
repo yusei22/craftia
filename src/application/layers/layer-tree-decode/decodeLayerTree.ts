b79b2bb693002fcc9311d41a5de72ae6e496600a
PineBokkuri <126656209+PineBokkuri@users.noreply.github.com>
2023-08-15 23:15:53 +0900

    update:#3 importの部分

--- src/application/layers/layer-tree-decode/decodeLayerTree.ts
+++ src/application/layers/layer-tree-decode/decodeLayerTree.ts
@@ -1,7 +1,7 @@
 import { LayerTree } from "../layers-rendering-system/tree-renderer/LayerTreeRenderer";
 import { EncodedLayer } from "../../types";
-import { Vec2 } from "../../units";
 import { decodeLayer } from "./decode-layer/decodeLayer";
+import { Vec2 } from "../../units";
 
 function decodeLayerTree(encodedLayerTree: EncodedLayer[], projectSize: Vec2, useImageBitmap: boolean): Promise<LayerTree> {
     return Promise.all(
