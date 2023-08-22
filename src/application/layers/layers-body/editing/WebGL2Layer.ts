385951a0f7ee7187424195b94180e9f2281c7655
PineBokkuri <126656209+PineBokkuri@users.noreply.github.com>
2023-08-23 07:03:02 +0900

    update:#34 `editSettings`メゾットを追加

--- src/application/layers/layers-body/editing/WebGL2Layer.ts
+++ src/application/layers/layers-body/editing/WebGL2Layer.ts
@@ -1,14 +1,14 @@
-import { LayerSettings } from '../../layer-settings/LayerSettings';
+import { LayerSettings, LayerSettingsParam } from '../../layer-settings/LayerSettings';
 import { SystemLayerSettings } from '../../layer-settings/SystemLayerSettings';
 import { ILayer } from '../Ilayer';
 import { WebGL2Consumer } from 'application/canvas/WebGL2Consumer';
-import { createCanvasAndWebGL2Context } from 'application/canvas/createCanvas';
 import { Vec4 } from 'application/units';
+
 abstract class WebGL2Layer extends WebGL2Consumer implements ILayer {
   public abstract settings: LayerSettings;
   public abstract systemSettings: SystemLayerSettings;
-  constructor() {
-    super(createCanvasAndWebGL2Context().gl2);
+  constructor(fleshGl2: WebGL2RenderingContext) {
+    super(fleshGl2);
   }
   public get canvas() {
     return this.getCanvas();
@@ -30,6 +30,10 @@ abstract class WebGL2Layer extends WebGL2Consumer implements ILayer {
     funcWhenNotDrawable?: (gl: WebGL2RenderingContext) => any
   ): void {
     super.useWebGl2Context(funcWhenDrawable, funcWhenNotDrawable);
+  }
+  public editSettings(editItem: Partial<LayerSettingsParam>) {
+    this.settings = this.settings.cloneEdit(editItem);
+    return this;
   }
 }
 export { WebGL2Layer };
