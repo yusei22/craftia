fd570dfe26a1b67d1352863e0d9baac676c5e95a
PineBokkuri <126656209+PineBokkuri@users.noreply.github.com>
2023-08-23 07:05:43 +0900

    update:#28 `createSmartImgBitmapLayer`メゾット削除
    update:#32 `createSmartImgElementLayer`メゾット削除

--- src/application/layers/layers-body/editing/smart-object/SmartWebGL2Layer.ts
+++ src/application/layers/layers-body/editing/smart-object/SmartWebGL2Layer.ts
@@ -1,11 +1,8 @@
 import { LayerSettings } from '../../../layer-settings/LayerSettings';
 import { SystemLayerSettings } from '../../../layer-settings/SystemLayerSettings';
-import { createRasterizedImgBitmapLayer } from '../../rasterized/RasterizedImgBitmapLayer';
-import { createRasterizedImgElementLayer } from '../../rasterized/RasterizedImgElementLayer';
 import { createSmartImgBitmapLayer } from '../../smart-object/SmartImgBitmapLayer';
 import { createSmartImgElementLayer } from '../../smart-object/SmartImgElementLayer';
 import { WebGL2Layer } from '../WebGL2Layer';
-import { ImageRenderer } from 'application/image-utils/ImageRenderer';
 import { Vec2 } from 'application/units';
 
 class SmartWebGL2Layer extends WebGL2Layer {
@@ -14,8 +11,8 @@ class SmartWebGL2Layer extends WebGL2Layer {
   get systemSettings() {
     return this._systemSettings;
   }
-  constructor(settings: LayerSettings, resize: Vec2) {
-    super();
+  constructor(fleshGl2: WebGL2RenderingContext, settings: LayerSettings, resize: Vec2) {
+    super(fleshGl2);
     this.settings = settings.clone();
     this._systemSettings = new SystemLayerSettings({ resize: resize });
   }
@@ -25,27 +22,12 @@ class SmartWebGL2Layer extends WebGL2Layer {
   public changeResizeSettings(newSize: Vec2) {
     this._systemSettings = this._systemSettings.cloneEdit({ resize: newSize });
   }
-  public createRasterizedImgBitmapLayer() {
-    const rasterizedSource = this.createRasterizedSource();
-    return createRasterizedImgBitmapLayer(rasterizedSource, this.settings);
-  }
-  public createRasterizedImgElementLayer() {
-    const rasterizedSource = this.createRasterizedSource();
-    const dataURL = rasterizedSource.toDataURL();
-    return createRasterizedImgElementLayer(dataURL, this.settings);
-  }
   public createSmartImgBitmapLayer() {
     return createSmartImgBitmapLayer(this.canvas, this.settings, this.systemSettings.resize);
   }
   public createSmartImgElementLayer() {
     const dataURL = this.canvas.toDataURL();
     return createSmartImgElementLayer(dataURL, this.settings, this.systemSettings.resize);
-  }
-  private createRasterizedSource() {
-    const renderer = new ImageRenderer();
-    renderer.viewport(this._systemSettings.resize);
-    renderer.drawImage(this.canvas, new Vec2(0, 0), this._systemSettings.resize);
-    return renderer.canvas;
   }
 }
 export { SmartWebGL2Layer };
