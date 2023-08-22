49b13a61ece56682e8c9d50bb64a0b8ef010a4f4
PineBokkuri <126656209+PineBokkuri@users.noreply.github.com>
2023-08-23 07:05:55 +0900

    update:#16 `editSettings`メゾットを追加

--- src/application/layers/layers-body/empty/EmptyLayer.ts
+++ src/application/layers/layers-body/empty/EmptyLayer.ts
@@ -1,18 +1,21 @@
-import { LayerSettings } from '../../layer-settings/LayerSettings';
+import { LayerSettings, LayerSettingsParam } from '../../layer-settings/LayerSettings';
 import { SystemLayerSettings } from '../../layer-settings/SystemLayerSettings';
-import { createCanvasAnd2DContext } from 'application/canvas/createCanvas';
 import { Vec2 } from 'application/units';
 
 class EmptyLayer {
   public settings: LayerSettings;
   readonly source: HTMLCanvasElement;
   readonly systemSettings: SystemLayerSettings;
-  constructor() {
-    this.source = createCanvasAnd2DContext().canvas;
+  constructor(fleshContext: CanvasRenderingContext2D, layerSettings?: LayerSettings) {
+    this.source = fleshContext.canvas;
     this.source.width = 1;
     this.source.height = 1;
-    this.settings = new LayerSettings();
+    this.settings = layerSettings ? layerSettings.clone() : new LayerSettings();
     this.systemSettings = new SystemLayerSettings({ resize: new Vec2(1, 1) });
+  }
+  public editSettings(editItem: Partial<LayerSettingsParam>) {
+    this.settings = this.settings.cloneEdit(editItem);
+    return this;
   }
 }
 export { EmptyLayer };
