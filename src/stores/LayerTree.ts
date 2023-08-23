b1b9ef27131d8c9b76081e898990f56d1e58cc5c
PineBokkuri <126656209+PineBokkuri@users.noreply.github.com>
2023-08-23 21:49:48 +0900

    add:グローバルなレイヤーツリーのstate

---
+++ src/stores/LayerTree.ts
@@ -0,0 +1,25 @@
+import { atom } from 'recoil';
+import { LayerTree, StaticLayerTree } from 'application/types';
+import { useLayerTreeUndoRedo } from 'hooks';
+
+const HISTORY_LENGTH = 20;
+const useImageBitmap = true;
+const defualt: StaticLayerTree = [];
+
+const layerTreeState = atom<LayerTree>({
+  key: 'layerTree',
+  default: defualt,
+});
+
+const layerTreeHistoryState = atom<LayerTree>({
+  key: 'layerTreeHistoryState',
+  default: defualt,
+});
+
+const layerTreeUndoRedo = useLayerTreeUndoRedo({
+  key: 'layerTreeUndoRedo ',
+  storageLength: HISTORY_LENGTH,
+  defualt: defualt,
+  useImageBitmap: true,
+});
+export { layerTreeState, layerTreeUndoRedo, useImageBitmap, layerTreeHistoryState };
