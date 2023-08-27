432c999496a04ef35667b025394925ee1ca8e7e5
PineBokkuri <126656209+PineBokkuri@users.noreply.github.com>
2023-08-27 18:35:45 +0900

    update:クラスのメゾット・プロパティにdocs追加

--- src/application/layers/layers-body/buffering/RasterizedBufferLayer.ts
+++ src/application/layers/layers-body/buffering/RasterizedBufferLayer.ts
@@ -5,19 +5,29 @@ import { Layer } from 'application/types';
 import { Vec2 } from 'application/units';
 
 class RasterizedBufferLayer extends Ctx2DConsumer {
+  /** レイヤーの設定 */
   public settings: LayerSettings;
-  private originalSettings: LayerSettings;
+  /** レイヤーのシステム設定 */
   public get systemSettings() {
     return new SystemLayerSettings({
       resize: new Vec2(this.canvas.width, this.canvas.height),
     });
   }
+  /**バッファリング先のcanvas */
   public get canvas() {
     return this.getCanvas();
   }
+  /**レイヤーのソース */
   public get source() {
     return this.canvas;
   }
+  /** オリジナルのレイヤーの設定 */
+  private originalSettings: LayerSettings;
+
+  /**
+   * @param context ベースのコンテクスト
+   * @param layer バッファリングするレイヤー
+   */
   constructor(context: CanvasRenderingContext2D, layer: Layer) {
     super(context);
     this.setLayer(layer);
@@ -29,22 +39,44 @@ class RasterizedBufferLayer extends Ctx2DConsumer {
     this.viewport(layer.systemSettings.resize);
     this.drawImage(layer.source, new Vec2(0, 0), layer.systemSettings.resize);
   }
+
+  /**
+   * 設定を全てリセット
+   * @returns 自身のクラス
+   */
   public resetAllSettings() {
     this.settings = new LayerSettings();
+    return this;
   }
+
+  /**
+   * 元の設定を全て復元
+   * @returns
+   */
   public restoreAllSettings() {
     this.settings = this.originalSettings.clone();
+    return this;
   }
-  public EditSettings(editItem: Partial<LayerSettingsParam>) {
-    const newSettings = this.settings.cloneEdit(editItem);
-    this.settings = newSettings;
-  }
+  /**
+   * バッファを破壊
+   */
   public destroy() {
     super.destroy();
   }
+  /**
+   * ImageDataを取得する
+   * @param location 取得開始位置
+   * @param size 取得するサイズ
+   * @returns 取得したImageData
+   */
   public getImageData(location: Vec2, size: Vec2): ImageData {
     return super.getImageData(location, size);
   }
+  /**
+   * レイヤーの設定を変更する
+   * @param editItem
+   * @returns 自身のクラス
+   */
   public editSettings(editItem: Partial<LayerSettingsParam>) {
     this.settings = this.settings.cloneEdit(editItem);
     return this;
--- src/application/layers/layers-body/composite/CompositeLayer.ts
+++ src/application/layers/layers-body/composite/CompositeLayer.ts
@@ -6,32 +6,61 @@ import { Layer } from 'application/types';
 import { Vec2 } from 'application/units';
 
 class CompositeLayer implements ILayer {
+  /** レンダラ */
   private renderer: LayerRenderer;
+  /** レイヤーの設定 */
   public settings: LayerSettings;
+  /**レイヤーのシステム設定 */
   public get systemSettings() {
     return new SystemLayerSettings({
       resize: new Vec2(this.source.width, this.source.height),
     });
   }
+  /**レイヤーのソース */
   public get source() {
     return this.renderer.canvas;
   }
+  /**
+   * @param context ベースにするコンストラクタ
+   * @param settings レイヤーの設定
+   * @param size レイヤーのサイズ
+   */
   constructor(context: CanvasRenderingContext2D, settings: LayerSettings, size: Vec2) {
     this.renderer = new LayerRenderer(context);
     this.renderer.viewport(size);
     this.settings = settings.clone();
   }
+
+  /**
+   * 描画するレイヤーを変更
+   * @param layers 新しいレイヤーの配列
+   * @returns 自身のクラス
+   */
   public change(layers: Layer[]) {
     if (layers.length <= 0) {
       this.renderer.clear();
-      return;
+      return this;
     }
     this.renderer.redraw(layers);
+    return this;
   }
+
+  /**
+   * 描画するレイヤーを上に追加
+   * @param layers 追加するレイヤーの配列
+   * @returns 自身のクラス
+   */
   public appened(layers: Layer[]) {
-    if (layers.length <= 0) return;
+    if (layers.length <= 0) return this;
     this.renderer.drawAbove(layers);
+    return this;
   }
+
+  /**
+   * レイヤーの設定を変更する
+   * @param editItem
+   * @returns 自身のクラス
+   */
   public editSettings(editItem: Partial<LayerSettingsParam>) {
     this.settings = this.settings.cloneEdit(editItem);
     return this;
