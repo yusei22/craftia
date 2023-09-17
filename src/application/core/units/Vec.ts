5f66e3e266201b8d412685b5442bcb478158fcd0
PineBokkuri <126656209+PineBokkuri@users.noreply.github.com>
2023-09-17 21:18:17 +0900

    update:全てのプロパティをreadonlyに変更
    move:フォルダ構成変更

---
+++ src/application/core/units/Vec.ts
@@ -0,0 +1,63 @@
+import { Vec2 } from './Vec2';
+import { Vec3 } from './Vec3';
+import { Vec4 } from './Vec4';
+
+type Vec = Vec2 | Vec3 | Vec4;
+
+interface IVec {
+    readonly inverse: Vec;
+    /**
+     * 足し算を行う
+     * @param v 足すベクトル
+     * @returns 結果(新しいベクトル)
+     */
+    readonly add: (v: this) => Vec;
+    /**
+     * 引き算を行う
+     * @param v 引くベクトル
+     * @returns 結果(新しいベクトル)
+     */
+    readonly sub: (v: this) => Vec;
+    /**
+     * 任意の数をベクトルの値それぞれに掛ける
+     * @param n 掛ける値
+     * @returns 結果(新しいベクトル)
+     */
+    readonly times: (n: number) => Vec;
+    /**
+     * 内積を求める
+     * @param v 他方のベクトル
+     * @returns 結果(新しいベクトル)
+     */
+    readonly dot: (v: this) => number;
+
+    /**
+     * 値が等しいか調べる
+     * @param v 比較するベクトル
+     * @returns 真偽地
+     */
+    readonly equal: (v: this) => boolean;
+
+    /**
+     * ユーグリッド距離を求める
+     * @param v 他方のベクトル
+     * @returns 結果( number型 )
+     */
+
+    readonly distance: (v: this) => number;
+    /**
+     * 配列に変換
+     * @returns number型配列
+     */
+    readonly toArray: () => number[];
+}
+
+function isVec(value: any): value is IVec {
+    if (value instanceof Vec2 || value instanceof Vec3 || value instanceof Vec4) {
+        return true;
+    }
+    return false;
+}
+
+export { isVec };
+export type { IVec, Vec };
---
+++ src/application/core/units/Vec2.ts
@@ -0,0 +1,55 @@
+import { IVec } from './Vec';
+export class Vec2 implements IVec {
+    readonly x: number;
+    readonly y: number;
+    public get r() {
+        return this.x;
+    }
+    public get g() {
+        return this.y;
+    }
+    public get rg() {
+        return this;
+    }
+    public get xy() {
+        return this;
+    }
+    public get inverse() {
+        return this.times(-1);
+    }
+    constructor(x: number, y: number);
+    constructor(coordArray: [number, number, ...number[]]);
+    constructor(a: number | [number, number, ...number[]], b?: number) {
+        if (typeof a === 'number' && typeof b === 'number') {
+            this.x = a;
+            this.y = b;
+        } else if (Array.isArray(a)) {
+            this.x = a[0];
+            this.y = a[1];
+        } else {
+            this.x = 0;
+            this.y = 0;
+        }
+    }
+    public add(v: Vec2): Vec2 {
+        return new Vec2(this.x + v.x, this.y + v.y);
+    }
+    public sub(v: Vec2): Vec2 {
+        return new Vec2(this.x - v.x, this.y - v.y);
+    }
+    public times(n: number): Vec2 {
+        return new Vec2(this.x * n, this.y * n);
+    }
+    public dot(v: Vec2): number {
+        return this.x * v.x + this.y * v.y;
+    }
+    public equal(v: Vec2): boolean {
+        return v.x === this.x && v.y === this.y;
+    }
+    public distance(v: Vec2): number {
+        return Math.sqrt((v.x - this.x) ** 2 + (v.y - this.y) ** 2);
+    }
+    public toArray(): [number, number] {
+        return [this.x, this.y];
+    }
+}
