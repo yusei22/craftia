9fd3c330f97525b7aa90330d9b14116dad3b6163
PineBokkuri <126656209+PineBokkuri@users.noreply.github.com>
2023-09-17 21:12:02 +0900

    move: webglユーティリティ群の フォルダ構成変更

--- src/application/types/layers-rendering/CroppingSettings.ts
+++
@@ -1,5 +0,0 @@
-import { Vec2 } from '../../units';
-export type CroppingSettings = {
-  upperLeft: Vec2;
-  size: Vec2;
-};
--- src/application/webgl-utils/webgl/uniforms/Uniform.ts
+++
@@ -1,7 +0,0 @@
-import { Mat2, Mat3, Mat4, Vec2, Vec3, Vec4 } from '../../../units';
-
-type UniformValue = Mat2 | Mat3 | Mat4 | Vec2 | Vec3 | Vec4 | number;
-interface Uniform {
-  set(value: UniformValue | UniformValue): void;
-}
-export type { Uniform, UniformValue };
