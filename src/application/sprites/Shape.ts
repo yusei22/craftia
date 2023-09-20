e05d11e43dfa21c3543fc6cd728c3333a6b91e28
PineBokkuri <126656209+PineBokkuri@users.noreply.github.com>
2023-09-20 22:24:18 +0900

    update:#5 ` ShapePrefs`インターフェースにプロパティ追加

--- src/application/sprites/Shape.ts
+++ src/application/sprites/Shape.ts
@@ -1,17 +1,22 @@
-import { Sprite, SpriteConfig, SpritePrefs } from "./Sprite";
-import { Context2D } from "../core/context-2d";
-import { Vec2 } from "../core/units";
+import { Context2D } from '../core/context-2d';
+import { Vec2 } from '../core/units';
+import { Sprite, SpriteConfig, SpritePrefs } from './Sprite';
 
 interface ShapePrefs extends SpritePrefs {
     readonly size: Vec2;
-    readonly strokeWidth: number;
-    readonly strokeStyle: string | CanvasGradient | CanvasPattern;
-    readonly drawStrokeToBack: boolean;
+    readonly strokeStyle: string | CanvasGradient | CanvasPattern | null;
+    readonly strokeCap: CanvasLineCap | null;
+    readonly strokeDashOffset: number | null;
+    readonly strokeJoin: CanvasLineJoin | null;
+    readonly strokeWidth: number | null;
+    readonly fillStyle: string | CanvasGradient | CanvasPattern | null;
+    readonly scale: Vec2;
 }
-abstract class Shape<T extends ShapePrefs> extends Sprite<T>{
+abstract class Shape<T extends ShapePrefs = ShapePrefs> extends Sprite<T> {
     constructor(config: SpriteConfig, prefs: T) {
-        super(config, prefs)
+        super(config, prefs);
     }
-    public abstract drawFunc(context: Context2D): void
+    public abstract drawFunc(context: Context2D): void;
 }
+export type { ShapePrefs };
 export { Shape };
