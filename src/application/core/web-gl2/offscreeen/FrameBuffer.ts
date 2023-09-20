021072eef279aa6b07a39630805dad87e6cf9e4c
PineBokkuri <126656209+PineBokkuri@users.noreply.github.com>
2023-09-20 22:21:45 +0900

    update:メゾット・プロパティのアクセス修飾子変更

--- src/application/core/web-gl2/offscreeen/FrameBuffer.ts
+++ src/application/core/web-gl2/offscreeen/FrameBuffer.ts
@@ -1,9 +1,9 @@
 /**WebGLFramebufferを管理するクラス */
 class FrameBuffer {
     /** WebGL2のコンテキスト*/
-    readonly gl: WebGL2RenderingContext;
+    private gl: WebGL2RenderingContext;
     /**内包するWebGLFramebuffer */
-    readonly webGLFramebuffer: WebGLFramebuffer | null;
+    private webGLFramebuffer: WebGLFramebuffer | null;
 
     /**
      * @param gl  WebGL2のコンテキスト
@@ -14,12 +14,12 @@ class FrameBuffer {
     }
 
     /**バインドする */
-    bind() {
+    public bind() {
         this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, this.webGLFramebuffer);
     }
 
     /**既定のフレームバッファを使う。（キャンバスにレンダリングされるようになる） */
-    unbind() {
+    public unbind() {
         this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, null);
     }
     static bindDefaultFrameBuffer(gl: WebGL2RenderingContext) {
