b0b8d04b78d1b230209f0c692094e5d16d971828
PineBokkuri <126656209+PineBokkuri@users.noreply.github.com>
2023-09-28 08:44:21 +0900

    Revert "update:#20 `SpriteConfig`のプロパティがとる型を制限"

--- src/application/sprites/Sprite.ts
+++ src/application/sprites/Sprite.ts
@@ -1,44 +1,31 @@
-import {
-    Context2D,
-    ContextLineConfig,
-    ContextShadowConfig,
-    ContextTextConfig,
-} from 'application/core/context-2d';
-import { Vec, Vec2 } from 'application/core/units';
+import { Vec2 } from "application/core/units"
+import { Context2D, ContextLineConfig, ContextShadowConfig, ContextTextConfig } from "application/core/context-2d"
 
 interface SpriteConfig {
-    readonly line: ContextLineConfig | null;
-    readonly shadow: ContextShadowConfig | null;
-    readonly text: ContextTextConfig | null;
-    readonly fillStyle: string | CanvasGradient | CanvasPattern | null;
-    readonly globalAlpha: number | null;
-    readonly globalCompositeOperation: GlobalCompositeOperation | null;
-    readonly strokeStyle: string | CanvasGradient | CanvasPattern | null;
+    readonly line: ContextLineConfig | null
+    readonly shadow: ContextShadowConfig | null,
+    readonly text: ContextTextConfig | null,
+    readonly fillStyle: string | CanvasGradient | CanvasPattern | null,
+    readonly globalAlpha: number | null,
+    readonly globalCompositeOperation: GlobalCompositeOperation | null,
+    readonly strokeStyle: string | CanvasGradient | CanvasPattern | null,
 }
 
 interface SpritePrefs {
-    [key: string]: string | number | boolean | null | Vec | CanvasGradient | CanvasPattern;
-
     /**名前 */
     readonly name: string;
     /**基準点 */
     readonly anchor: Vec2;
     /**グローバル位置 */
     readonly globalLocation: Vec2;
-    /**回転 */
-    readonly rotation: number;
     /**可視 */
     readonly visible: boolean;
     /**ブレンドモード */
     readonly blendMode: GlobalCompositeOperation;
     /**透明度 */
     readonly opacity: number;
-    /**影のぼかし */
-    readonly shadowBlur: number | null;
-    /**影の色 */
-    readonly shadowColor: string | null;
-    /**影の位置 */
-    readonly shadowOffset: Vec2 | null;
+    /**影 */
+    readonly shadow: ContextShadowConfig | null;
 }
 
 abstract class Sprite<T extends SpritePrefs = SpritePrefs> {
@@ -56,15 +43,15 @@ abstract class Sprite<T extends SpritePrefs = SpritePrefs> {
             .setTextConfig(this.config.text)
             .setFillStyle(this.config.fillStyle)
             .setGlobalAlpha(this.config.globalAlpha)
-            .setGlobalCompositeOperation(this.config.globalCompositeOperation);
+            .setGlobalCompositeOperation(this.config.globalCompositeOperation)
     }
-    public draw(context: Context2D) {
+    draw(context: Context2D) {
         context.resetTransform();
         this.setconfig(context);
         this.drawFunc(context);
     }
-    abstract drawFunc(context: Context2D): void;
+    abstract drawFunc(context: Context2D): void
 }
 
-export type { SpriteConfig, SpritePrefs };
-export { Sprite };
+export type { SpriteConfig, SpritePrefs }
+export { Sprite }
