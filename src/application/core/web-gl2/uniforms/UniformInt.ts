5376cc89fdf33ef7748ae52eb37d081295ef3a8b
PineBokkuri <126656209+PineBokkuri@users.noreply.github.com>
2023-09-28 08:28:03 +0900

    update:ベクター配列はVec[]で表現

--- src/application/core/web-gl2/uniforms/UniformInt.ts
+++ src/application/core/web-gl2/uniforms/UniformInt.ts
@@ -1,23 +1,27 @@
-import { Program } from '../program/Program';
-import { IUniform, IUniformValue } from './IUniform';
-import { Mat, Vec, Vec2, Vec3, Vec4, isVec } from 'application/core/units';
+import { IUniform, IUniformValue, expandVecs } from './IUniform';
+import { Vec, Vec2, Vec3, Vec4, isVec } from 'application/core/units';
 
-class UniformInt<T extends IUniformValue, U extends string = string> implements IUniform<T, U> {
-    readonly name: U;
+class UniformInt<T extends IUniformValue> implements IUniform<T> {
     private gl: WebGL2RenderingContext;
     private location: WebGLUniformLocation | null;
-    constructor(program: Program, name: U) {
-        this.name = name;
-        this.gl = program.gl;
-        this.location = program.getUniformLocation(name);
+    constructor(gl2: WebGL2RenderingContext, location: WebGLUniformLocation | null) {
+        this.gl = gl2;
+        this.location = location;
     }
     public set(value: T) {
         if (typeof value === 'number') {
             this.setNumber(value);
             return;
         }
-        if (value instanceof Mat) {
-            this.setMat(value);
+        if (Array.isArray(value)) {
+            if (typeof value[0] === 'number') {
+                this.setArray(value as number[]);
+                return;
+            }
+            if (isVec(value[0])) {
+                this.setVecs(value as Vec2[] | Vec3[] | Vec4[]);
+                return;
+            }
             return;
         }
         if (isVec(value)) {
@@ -25,18 +29,7 @@ class UniformInt<T extends IUniformValue, U extends string = string> implements 
             return;
         }
     }
-    private setMat(mat: Mat) {
-        if (mat.width === 1) {
-            this.gl.uniform1iv(this.location, mat.item);
-        } else if (mat.width === 1) {
-            this.gl.uniform2iv(this.location, mat.item);
-        } else if (mat.width === 3) {
-            this.gl.uniform3iv(this.location, mat.item);
-        } else if (mat.width === 4) {
-            this.gl.uniform4iv(this.location, mat.item);
-        }
-    }
-    private setVec(vec: Vec) {
+    private setVec(vec: Vec2 | Vec3 | Vec4) {
         if (vec instanceof Vec2) {
             this.gl.uniform2i(this.location, vec.x, vec.y);
         } else if (vec instanceof Vec3) {
@@ -47,6 +40,18 @@ class UniformInt<T extends IUniformValue, U extends string = string> implements 
     }
     private setNumber(number: number) {
         this.gl.uniform1i(this.location, number);
+    }
+    private setArray(arr: number[]) {
+        this.gl.uniform1iv(this.location, arr);
+    }
+    private setVecs(vecs: Vec2[] | Vec3[] | Vec4[]) {
+        if (vecs[0] instanceof Vec2) {
+            this.gl.uniform2iv(this.location, expandVecs(vecs));
+        } else if (vecs[0] instanceof Vec3) {
+            this.gl.uniform3iv(this.location, expandVecs(vecs));
+        } else if (vecs[0] instanceof Vec4) {
+            this.gl.uniform4iv(this.location, expandVecs(vecs));
+        }
     }
 }
 export { UniformInt };
