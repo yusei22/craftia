f119f659e5745acb0c0ebd7a17b5cdd9b43efce1
PineBokkuri <126656209+PineBokkuri@users.noreply.github.com>
2023-09-28 08:40:11 +0900

    add:スマートオブジェクトのスプライト

--- src/application/sprites/SmartImage.ts
+++ src/application/sprites/SmartImage.ts
@@ -1,21 +1,30 @@
-import { Vec2 } from "application/core/units";
-import { Sprite, SpriteConfig, SpritePrefs } from "./Sprite";
-import { Context2D } from "application/core/context-2d";
+import { Sprite, SpriteConfig, SpritePrefs } from './Sprite';
+import { Context2D } from 'application/core/context-2d';
+import { Vec2 } from 'application/core/units';
+
 interface SmartImagePrefs extends SpritePrefs {
     readonly scale: Vec2;
 }
-class SmartImage extends Sprite<SmartImagePrefs>{
+
+class SmartImage extends Sprite<SmartImagePrefs> {
     readonly image: ImageBitmap;
+
+    private cacheContext: Context2D | null = null;
+
     constructor(image: ImageBitmap, prefs: SmartImagePrefs) {
         const config: SpriteConfig = {
             line: null,
-            shadow: prefs.shadow,
+            shadow: {
+                shadowBlur: prefs.shadowBlur,
+                shadowColor: prefs.shadowColor,
+                shadowOffset: prefs.shadowOffset,
+            },
             text: null,
             fillStyle: null,
             globalAlpha: prefs.opacity,
             globalCompositeOperation: prefs.blendMode,
             strokeStyle: null,
-        }
+        };
         super(config, prefs);
         this.image = image;
     }
@@ -23,15 +32,20 @@ class SmartImage extends Sprite<SmartImagePrefs>{
         return new Vec2(
             this.prefs.anchor.x * this.prefs.scale.x,
             this.prefs.anchor.y * this.prefs.scale.y
-        )
+        );
     }
     public drawFunc(context: Context2D) {
         if (!this.prefs.visible) return;
-        context.drawImage(
-            this.image,
-            this.prefs.globalLocation.sub(this.getAnchorRerativeLoc()),
-            this.prefs.scale
-        )
+
+        const startPint = this.prefs.globalLocation.sub(this.getAnchorRerativeLoc());
+        const centerPoint = startPint.add(this.prefs.scale.times(0.5));
+        context
+            .translate(centerPoint)
+            .rotate(this.prefs.rotation)
+            .drawImage(this.image, startPint, this.prefs.scale);
+    }
+    public clone() {
+        return new SmartImage(this.image, this.prefs);
     }
 }
 export { SmartImage };
