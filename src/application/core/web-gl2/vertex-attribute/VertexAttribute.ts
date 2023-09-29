23ac5cfd4b99a853ca4942c5c6e96e1efd9403e0
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-09-29 23:41:50 +0900

    update:#9 コメント編集

--- src/application/core/web-gl2/vertex-attribute/VertexAttribute.ts
+++ src/application/core/web-gl2/vertex-attribute/VertexAttribute.ts
@@ -6,7 +6,7 @@ class VertexAttribute {
     readonly location: GLenum;
     /**頂点属性あたりの要素数 */
     readonly size: number;
-    /** 連続する頂点属性の始端どうしの間にある、オフセット数,*/
+    /** 連続する頂点属性の始端どうしの間にある、オフセット数*/
     readonly stride: number = 0;
     /** 頂点属性配列の最初の要素のオフセット*/
     readonly offset: number = 0;
