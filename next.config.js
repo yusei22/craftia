b9b3aa4c5507155faf0db2f0ecaa98da0b6f56fe
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-09-29 23:31:19 +0900

    update:#21 webpackの設定追加
    raw-loader導入のため

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   compiler: (() => {
@@ -17,6 +18,18 @@ const nextConfig = {
 
     return compilerConfig
   })(),
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.(glsl|frag|vert)$/,
+      use: [
+        options.defaultLoaders.babel,
+        { loader: "raw-loader" },
+        { loader: "glslify-loader" },
+      ],
+      exclude: /node_modules/,
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig
