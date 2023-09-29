a3ab042c9bcbeb3e88f05a812f47d96ae2443a55
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-09-30 08:36:10 +0900

    add:継承元のレンダラクラスを新しくしたキャッシュ可能レンダラ

---
+++ src/application/core/web-gl2/renderer/CacheableRenderer.ts
@@ -0,0 +1,42 @@
+import { OffscreenRenderer } from '../offscreeen';
+import { Texture2D } from '../textures';
+import { Vec2 } from 'application/core/units';
+import { RendererBufferData, RendererShader, Renderer, RenderOptions } from "./Renderer";
+
+class CacheableRenderer extends Renderer {
+    private offScreens: OffscreenRenderer[] = [];
+    private resultTex: Texture2D;
+    private count: number = 0;
+
+    constructor(bufferData: RendererBufferData, shader: RendererShader, cacheSize: Vec2) {
+        super(shader, bufferData);
+        this.offScreens[0] = this.createOffscreen(cacheSize);
+        this.offScreens[1] = this.createOffscreen(cacheSize);
+
+        this.resultTex = this.offScreens[0].resultTexture;
+    }
+    public renderToCache(renderOp: RenderOptions={}) {
+        super.setOffscreen(this.offScreens[this.count % 2]);
+        
+        super.render({
+            mode: renderOp.mode,
+            type: renderOp.type,
+            offset: renderOp.offset,
+        });
+        super.setOffscreen(null);
+
+        this.resultTex = this.offScreens[this.count % 2].resultTexture;
+        this.count++;
+
+        return this;
+    }
+    public getCacheTex() {
+        return this.resultTex;
+    }
+    public setNewCache(cacheSize: Vec2) {
+        this.offScreens[0] = new OffscreenRenderer(this.gl2, cacheSize);
+        this.offScreens[1] = new OffscreenRenderer(this.gl2, cacheSize);
+        return this;
+    }
+}
+export { CacheableRenderer };
