4a405b87bf57253fc2ea05ee307c45fb5d1e7437
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-01 23:40:56 +0900

    update:#90 抽象クラス追加
    update:#20 環境設定の値にはreadonlyで固められたオブジェクトやリテラル型などのみを使う
    
    update:#19 環境設定のプロパティを変更可能に

--- src/application/sprites/Sprite.ts
+++ src/application/sprites/Sprite.ts
@@ -1,3 +1,4 @@
+import { FillLinerGradient, FillPattern, FillRadialGradient, FillSolid } from './SpriteFill';
 import {
     Context2D,
     ContextLineConfig,
@@ -6,39 +7,44 @@ import {
 } from 'application/core/context-2d';
 import { Vec, Vec2 } from 'application/core/units';
 
+type SpriteFillStyle = null | FillLinerGradient | FillRadialGradient | FillPattern | FillSolid;
+
 interface SpriteConfig {
     readonly line: ContextLineConfig | null;
     readonly shadow: ContextShadowConfig | null;
     readonly text: ContextTextConfig | null;
-    readonly fillStyle: string | CanvasGradient | CanvasPattern | null;
+    readonly fillStyle: SpriteFillStyle;
     readonly globalAlpha: number | null;
     readonly globalCompositeOperation: GlobalCompositeOperation | null;
-    readonly strokeStyle: string | CanvasGradient | CanvasPattern | null;
+    readonly strokeStyle: SpriteFillStyle;
 }
 
-interface SpritePrefs {
-    [key: string]: string | number | boolean | null | Vec | CanvasGradient | CanvasPattern;
+type SpritePrefsValue = string | number | boolean | null | Vec | SpriteFillStyle;
 
+interface SpritePrefs {
+    [key: string]: SpritePrefsValue;
+    /**id */
+    id: string;
     /**名前 */
-    readonly name: string;
+    name: string;
     /**基準点 */
-    readonly anchor: Vec2;
+    anchor: Vec2;
     /**グローバル位置 */
-    readonly globalLocation: Vec2;
+    globalLocation: Vec2;
     /**回転 */
-    readonly rotation: number;
+    rotation: number;
     /**可視 */
-    readonly visible: boolean;
+    visible: boolean;
     /**ブレンドモード */
-    readonly blendMode: GlobalCompositeOperation;
+    blendMode: GlobalCompositeOperation;
     /**透明度 */
-    readonly opacity: number;
+    opacity: number;
     /**影のぼかし */
-    readonly shadowBlur: number | null;
+    shadowBlur: number | null;
     /**影の色 */
-    readonly shadowColor: string | null;
+    shadowColor: string | null;
     /**影の位置 */
-    readonly shadowOffset: Vec2 | null;
+    shadowOffset: Vec2 | null;
 }
 
 abstract class Sprite<T extends SpritePrefs = SpritePrefs> {
@@ -54,17 +60,38 @@ abstract class Sprite<T extends SpritePrefs = SpritePrefs> {
             .setLineConfig(this.config.line)
             .setShadowConfig(this.config.shadow)
             .setTextConfig(this.config.text)
-            .setFillStyle(this.config.fillStyle)
             .setGlobalAlpha(this.config.globalAlpha)
-            .setGlobalCompositeOperation(this.config.globalCompositeOperation);
+            .setGlobalCompositeOperation(this.config.globalCompositeOperation)
+            .setFillStyle(
+                this.config.fillStyle === null
+                    ? null
+                    : this.config.fillStyle.createCanvasFillStyle(context)
+            )
+            .setStrokeStyle(
+                this.config.strokeStyle === null
+                    ? null
+                    : this.config.strokeStyle.createCanvasFillStyle(context)
+            );
     }
     public draw(context: Context2D) {
+        if (!this.prefs.visible) return;
+
         context.resetTransform();
         this.setconfig(context);
         this.drawFunc(context);
     }
+    public drawPoint(context: Context2D, point: Vec2) {
+        if (!this.prefs.visible) return;
+        context.resetTransform();
+        this.setconfig(context);
+        this.drawPointFunc(context, point);
+    }
     abstract drawFunc(context: Context2D): void;
+    abstract drawPointFunc(context: Context2D, point: Vec2): void;
+    abstract clone(): Sprite;
+    abstract getStartPoint(): Vec2;
+    abstract getCenterPoint(): Vec2;
 }
 
-export type { SpriteConfig, SpritePrefs };
+export type { SpriteConfig, SpritePrefs, SpritePrefsValue, SpriteFillStyle };
 export { Sprite };
