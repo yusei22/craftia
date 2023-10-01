2a705f7ea8c1d5ecf16f23228b2d5caf003fa552
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-01 23:34:40 +0900

    remove:生のスプライトのまま運用するため

--- src/application/layers/Layer.ts
+++
@@ -1,8 +0,0 @@
-import { Sprite } from 'application/sprites/Sprite';
-
-class Layer<T extends Sprite> {
-    readonly sprite: T;
-    constructor(sprite: T) {
-        this.sprite = sprite;
-    }
-}
