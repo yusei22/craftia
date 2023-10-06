6873f3093a74c44aaac2558bc46e60b4f645d92a
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-07 08:10:37 +0900

    remove:レンダ―ビューサイズのatomがあれば不要なので削除

--- src/stores/ArtboardBaseZoomScaleState.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from "recoil";
-
-const ArtboardBaseZoomScaleState = atom<[number, number]>({
-    key: 'artboardBaseZoomScaleState',
-    default: [1, 1]
-})
-
-export { ArtboardBaseZoomScaleState };
