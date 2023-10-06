ebc49026075700270691197bdb6097781efee0bf
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-07 08:16:58 +0900

    update:emotion適用

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,54 +1,54 @@
-import { AppProps } from 'next/app';
-import Head from 'next/head';
+import { AppProps } from "next/app";
+import PropTypes from "prop-types";
+import Head from "next/head";
 import { RecoilRoot } from 'recoil';
-import { ThemeProvider, createGlobalStyle } from 'styled-components';
-import { theme } from 'themes';
-const GlobalStyle = createGlobalStyle`
-html,
-body,
-textarea {
-  padding: 0;
-  margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import { CssBaseline, CssVarsProvider } from "@mui/joy";
+import createEmotionCache from "createEmotionCache";
+import theme from "theme";
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
 }
 
-* {
-  box-sizing: border-box;
+function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (<>
+    <Head>
+      <meta key="charset" name="charset" content="utf-8" />
+      <meta
+        key="viewport"
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
+      />
+      <meta property="og:locale" content="ja_JP" />
+      <meta property="og:type" content="website" />
+    </Head>
+    <CacheProvider value={emotionCache}>
+      <CssVarsProvider theme={theme} >
+
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+
+        <RecoilRoot>
+          <Component {...pageProps} />
+        </RecoilRoot>
+
+      </CssVarsProvider>
+    </CacheProvider>
+  </>
+
+  );
 }
 
-a {
-  cursor: pointer;
-  text-decoration: none;
-  transition: .25s;
-  color: #000;
-}
-
-ol, ul {
-  list-style: none;
-}
-`;
-const MyApp = ({ Component, pageProps }: AppProps) => {
-    return (
-        <>
-            <Head>
-                <meta key="charset" name="charset" content="utf-8" />
-                <meta
-                    key="viewport"
-                    name="viewport"
-                    content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
-                />
-                <meta property="og:locale" content="ja_JP" />
-                <meta property="og:type" content="website" />
-            </Head>
-            <GlobalStyle />
-            <RecoilRoot>
-                <ThemeProvider theme={theme}>
-                    <Component {...pageProps} />
-                </ThemeProvider>
-            </RecoilRoot>
-        </>
-    );
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired,
 };
 
 export default MyApp;
