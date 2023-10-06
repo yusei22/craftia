974507e1eebd6b1c3d2df20d6be0f4581583093b
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-07 08:21:19 +0900

    add:recoilのキーのenum

---
+++ src/stores/recoilKeys.ts
@@ -0,0 +1,25 @@
+export enum AtomKeys {
+    SPRITE_TREE_ATOM = "spriteTreeAtom",
+
+    SPRITE_TREEHISTORY_PAST = 'spriteTreeHistoryPast',
+    SPRITE_TREEHISTORY_CURRENT = 'spriteTreeHistoryCurrent',
+    SPRITE_TREEHISTORY_FUTURE = 'spriteTreeHistoryFuture',
+
+    ACTIVE_SPRITEID_ATOM = 'activeSpriteIdAtom',
+
+    RENDERVIEW_SCALE_ATOM = 'renderViewResolutionAtom',
+    RENDERVIEW_LISTENERS_ATOM = 'renderViewListenersAtom',
+
+    ARTBOARD_SCALE_ATOM = 'artboardScaleAtom',
+    ARTBOARD_ROTATE_ATOM = 'artboardRotateAtom',
+    ARTBOARD_RESOLUTION_ATOM = 'artboardResolutionAtom',
+    ARTBOARD_REFERENCE_SCALE_ATOM = 'artboardReferenceScaleAtom',
+    ARTBOARD_LOCATION_ATOM = 'artboardLocationAtom',
+    ARTBOARD_ANCHOR_ATOM = 'artboardAnchorAtom'
+}
+
+export enum SelectorKeyes {
+    ARTBORD_ZOOM_SELECTOR = 'ArtbordZoomSelector',
+    ARTBOARD_POSITION_SELECTOR = 'artboardPositionAtom',
+    ARTBOARD_ANCHOR_VIEW_POINT_SELECTOR = 'artboardAnchorViewPointSelector'
+}
