c4a8a1558b1d28eb004aedf7b7376460be0aca33
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-06 17:16:22 +0900

    update:getAnchorRerativeLocメゾット廃止

--- src/application/sprites/Shape.ts
+++ src/application/sprites/Shape.ts
@@ -16,18 +16,19 @@ abstract class Shape<T extends ShapePrefs = ShapePrefs> extends Sprite<T> {
         super(config, prefs);
     }
     public abstract drawFunc(context: Context2D): void;
-    protected getAnchorRerativeLoc() {
-        return new Vec2(
+
+    public getStartPoint() {
+        const anchorRerativeLoc = new Vec2(
             this.prefs.anchor.x * this.prefs.scale.x,
             this.prefs.anchor.y * this.prefs.scale.y
         );
+        return this.prefs.globalLocation.sub(anchorRerativeLoc);
     }
-    public getStartPoint() {
-        return this.prefs.globalLocation.sub(this.getAnchorRerativeLoc());
-    }
+    
     public getCenterPoint() {
         return this.getStartPoint().add(this.prefs.scale.times(0.5));
     }
+
     public drawPointFunc(context: Context2D) {
         this.drawFunc(context);
     }
