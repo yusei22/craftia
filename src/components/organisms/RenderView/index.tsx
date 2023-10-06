b9648f99f58c438fa86d5804d42aca02cbc84371
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-06 17:23:59 +0900

    add:レンダ―ビューコンポーネント
    Artboardをrecoilのstareに基づいて表示する

---
+++ src/components/organisms/RenderView/index.tsx
@@ -0,0 +1,40 @@
+import useArtboardRender from "hooks/artboard/useArtboardRender"
+import { useEffect, useState, useRef } from 'react';
+
+const RenderView = () => {
+
+    const [context, setContext] = useState<CanvasRenderingContext2D | null>(null);
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    const { source: renderViewSource, deps: renderViewDeps } = useArtboardRender();
+    useEffect(() => {
+        if (!canvasRef.current) {
+            console.error('canvas is null')
+            return;
+        }
+        const canvas = canvasRef.current;
+        setContext(canvas.getContext('2d'));
+    }, []);
+    useEffect(() => {
+        if (context === null) {
+            return;
+        }
+        if (renderViewSource === null) {
+            return;
+        }
+
+        context.imageSmoothingEnabled = false;
+        context.canvas.width = renderViewSource.width;
+        context.canvas.height = renderViewSource.height;
+
+        context.drawImage(renderViewSource, 0, 0);
+
+    }, [context, renderViewDeps]);
+
+    return (
+        <>
+            <canvas ref={canvasRef}></canvas>
+        </>
+    )
+}
+export { RenderView };
