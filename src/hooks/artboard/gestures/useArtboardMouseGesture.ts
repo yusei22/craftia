5e6510b0cd40b54db9b2459f88f46be1a7b643f9
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-07 20:50:11 +0900

    add:レンダ―ビューでマウスジェスチャーを使うためのフック

---
+++ src/hooks/artboard/gestures/useArtboardMouseGesture.ts
@@ -0,0 +1,26 @@
+import { RenderViewListeners } from 'dataflow';
+import { Vec2 } from 'application/core/units';
+import { artboardTransformAtom } from 'dataflow';
+import { useSetRecoilState } from 'recoil';
+
+const useArtboardMouseGesture = () => {
+    const setArtboardTransform = useSetRecoilState(artboardTransformAtom);
+
+    let PinchPreviousOffset = new Vec2(0, 0);
+
+    const onMove: RenderViewListeners['onMove'] = ({ event, offset, down }) => {
+        event.preventDefault();
+        if (event.buttons === 4) {
+            setArtboardTransform(({ location, ...param }) => {
+                return {
+                    ...param,
+                    location: new Vec2(location).add(new Vec2(offset)).sub(PinchPreviousOffset).toArray()
+                }
+            })
+        }
+        PinchPreviousOffset = new Vec2(offset);
+    }
+
+    return { onMove }
+}
+export { useArtboardMouseGesture }
