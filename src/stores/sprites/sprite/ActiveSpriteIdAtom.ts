18b7c1b7a9439f0d381ce52845220f66bb046a3f
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-07 20:36:18 +0900

    remove:アクティブなスプライトのIDのステートを使用する箇所がないため

--- src/stores/sprites/sprite/ActiveSpriteIdAtom.ts
+++
@@ -1,9 +0,0 @@
-import { atom } from 'recoil';
-import { AtomKeys } from 'stores/recoilKeys';
-
-const ActiveSpriteIdAtom = atom<string[]>({
-    key: AtomKeys.ACTIVE_SPRITEID_ATOM,
-    default: [],
-});
-
-export { ActiveSpriteIdAtom };
