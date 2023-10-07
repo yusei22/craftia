47898023b1391dc97ae5fa0bfc199b115e90084c
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-07 20:38:18 +0900

    move:ウィンドウイベント用フックをhooks/window-eventsフォルダ以下に移動

--- src/hooks/window-events/useWindowSize.ts
+++ src/hooks/window-events/useWindowSize.ts
@@ -1,7 +1,7 @@
 import { useLayoutEffect, useState } from 'react';
 
-const useWindowSize = (): number[] => {
-    const [size, setSize] = useState([0, 0]);
+const useWindowSize = (): [number,number] => {
+    const [size, setSize] = useState<[number, number]>([0, 0]);
     useLayoutEffect(() => {
         const updateSize = (): void => {
             setSize([window.innerWidth, window.innerHeight]);
