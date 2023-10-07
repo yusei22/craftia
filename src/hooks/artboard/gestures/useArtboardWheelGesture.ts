e45969c23ba5d5d06620dabbd7fd1b1b424c63d2
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-07 20:50:26 +0900

    add:レンダ―ビューでホイールジェスチャーを使うためのフック

--- src/hooks/artboard/gestures/useArtboardWheelGesture.ts
+++ src/hooks/artboard/gestures/useArtboardWheelGesture.ts
@@ -1,39 +1,31 @@
-import { RenderViewListeners } from 'stores';
+import { RenderViewListeners } from 'dataflow';
 import { Vec2 } from 'application/core/units';
-import { useSetRecoilState } from 'recoil';
-import { ArtboardLocationAtom, ArtboardScaleAtom } from 'stores';
-import { getRectZoomLoc } from 'application/utils';
 import { useCallback } from 'react';
+import { useChangeArtboardAnchorFromViewPoint } from '../anchor/useChangeArtboardAnchorFromViewPoint';
 import { WHEEL_CLOSE_UP_DEGREES } from 'consts';
-import { ArtboardTransFixedAnchorSelector } from 'stores/artboard/anchor/ArtboardTransFixedAnchorSelector';
-
+import { artboardTransformAtom } from 'dataflow';
+import { useSetRecoilState } from 'recoil';
 
 
 const useArtboardWheelGesture = () => {
+    const changeArtboardAnchorFromViewPoint = useChangeArtboardAnchorFromViewPoint()
+    const setArtboardTransform = useSetRecoilState(artboardTransformAtom);
 
-    const setArtboardLocation = useSetRecoilState(ArtboardLocationAtom);
-    const setArtboardScale = useSetRecoilState(ArtboardScaleAtom);
-    const setArtboardAnchorTransformFixed = useSetRecoilState(ArtboardTransFixedAnchorSelector)
-
-    const onWheel: RenderViewListeners['onWheel'] = ({ delta, event }) => {
+    const onWheel: RenderViewListeners['onWheel'] = ({ delta, event, startTime }) => {
         event.preventDefault()
 
-        setArtboardAnchorTransformFixed([0.9,.9]);
-
-        /**
         const zoom = delta[1] === 0
             ? 1
             : (delta[1] < 0 ? 1 + WHEEL_CLOSE_UP_DEGREES : 1 - WHEEL_CLOSE_UP_DEGREES);
 
-        setArtboardLocation((currentLoc) =>
-            getRectZoomLoc(
-                new Vec2(currentLoc),
-                zoom,
-                new Vec2(event.offsetX, event.offsetY)
-            ).toArray()
-        );
-        setArtboardScale((currentSize) => new Vec2(currentSize).times(zoom).toArray());
-         */
+        changeArtboardAnchorFromViewPoint(new Vec2(event.offsetX, event.offsetY), true);
+
+        setArtboardTransform(({ scale, ...param }) => {
+            return {
+                ...param,
+                scale: new Vec2(scale).times(zoom).toArray(),
+            }
+        })
     }
 
     return {
