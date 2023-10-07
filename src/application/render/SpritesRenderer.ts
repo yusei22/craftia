dd0d69dbc0bbbf5060dd1265a3e31a947f9450a5
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-07 20:19:32 +0900

    update:#26 ポインタを描画するメソッド追加
    デバック用

--- src/application/render/SpritesRenderer.ts
+++ src/application/render/SpritesRenderer.ts
@@ -1,6 +1,9 @@
 import { Context2D } from 'application/core/context-2d';
-import { Vec2 } from 'application/core/units';
+import { Vec, Vec2, Vec4 } from 'application/core/units';
 import { Sprite } from 'application/sprites/Sprite';
+import { FillSolid } from 'application/sprites/SpriteFill';
+import { Arc } from 'application/sprites/shapes/Arc';
+import { Rect } from 'application/sprites/shapes/Rect';
 
 /**
  * TODO:キャシュとかで最適化できるようにする
@@ -19,6 +22,31 @@ class SpritesRenderer {
         sprites.forEach((sprite) => {
             sprite.draw(this.context);
         });
+    }
+    public drawPoint(loc: Vec2) {
+        const arc = new Arc({
+            id: '000000000000',
+            name: 'arc00000',
+            anchor: new Vec2(0.5,0.5),
+            globalLocation: loc,
+            rotation: 0 / 180 * Math.PI,
+            visible: true,
+            blendMode: 'source-over',
+            opacity: 1.0,
+            shadowBlur: 0,
+            shadowColor: '#0000',
+            shadowOffset: new Vec2(0, 0),
+            fillStyle: new FillSolid({ color: new Vec4(255, 0, 0, 1) }),
+            strokeCap: 'round',
+            strokeDashOffset: 0,
+            strokeJoin: 'bevel',
+            strokeWidth: 0,
+            strokeStyle: new FillSolid({ color: new Vec4(0, 0, 0, 0) }),
+            scale: new Vec2(10,10),
+            startAngle:0 * Math.PI / 180,
+            endAngle:360 * Math.PI / 180,
+        })
+        arc.draw(this.context)
     }
     public getResult() {
         return this.context.getCanvas();
