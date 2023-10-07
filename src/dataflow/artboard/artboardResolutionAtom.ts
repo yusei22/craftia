43a0beeb7624b21cd957715cd2832a6d13b75acc
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-07 20:26:08 +0900

    move:ステート管理をdataflowフォルダ内に移動

---
+++ src/dataflow/artboard/artboardResolutionAtom.ts
@@ -0,0 +1,7 @@
+import { atom } from "recoil";
+
+const artboardResolutionAtom = atom<[number, number]>({
+    key: 'artboardResolutionAtom',
+    default: [1, 1]
+})
+export { artboardResolutionAtom }
--- src/stores/artboard/resolution/ArtboardResolutionAtom.ts
+++
@@ -1,9 +0,0 @@
-import { atom } from 'recoil';
-import { AtomKeys } from 'stores/recoilKeys';
-
-const ArtboardResolutionAtom = atom<[number, number]>({
-    key: AtomKeys.ARTBOARD_RESOLUTION_ATOM,
-    default: [1, 1],
-});
-
-export { ArtboardResolutionAtom };
