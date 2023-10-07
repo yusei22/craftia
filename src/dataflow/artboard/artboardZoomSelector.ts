2f8455509ddc4f492bbf8284e3f3e9a3befe820f
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-07 20:33:41 +0900

    move:ステート管理をdataflowフォルダ内に移動

---
+++ src/dataflow/artboard/artboardZoomSelector.ts
@@ -0,0 +1,33 @@
+import { selector } from "recoil"
+import { artboardTransformAtom } from "./artboardTransformAtom"
+import { renderViewScaleAtom } from "../renderview/renderViewScaleAtom";
+import { Vec2 } from "application/core/units";
+
+const artbordZoomSelector = selector<number>({
+    key: 'artbordZoomSelector',
+    get: ({ get }) => {
+        const artboardTrans = get(artboardTransformAtom);
+        const renderViewScale = get(renderViewScaleAtom);
+
+        const widthZoom = artboardTrans.scale[0] / renderViewScale[0]
+        const heightZoom = artboardTrans.scale[1] / renderViewScale[1]
+
+        return Math.max(widthZoom, heightZoom)
+    },
+    set: ({ set, get }, newValue) => {
+
+        if (typeof newValue === 'number') {
+            const newScale = new Vec2(get(renderViewScaleAtom)).times(newValue).toArray()
+
+            set(artboardTransformAtom, (
+                { anchor, location, rotation }) => {
+                return {
+                    anchor, location, scale: newScale, rotation
+                }
+            }
+            );
+        }
+    }
+})
+
+export { artbordZoomSelector };
