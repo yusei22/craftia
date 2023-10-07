16a32d472a8f5feef0e437e40684bf7a01853545
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-07 20:34:44 +0900

    move:ステート管理をdataflowフォルダ内に移動

---
+++ src/dataflow/sprites/spriteTreeAtom.ts
@@ -0,0 +1,8 @@
+import { atom } from 'recoil';
+import { Sprite } from 'application/sprites/Sprite';
+
+const spriteTreeAtom = atom<Sprite[]>({
+    key: 'spriteTreeAtom',
+    default: [],
+});
+export { spriteTreeAtom }
--- src/stores/sprites/tree/SpriteTreeAtom.ts
+++
@@ -1,9 +0,0 @@
-import { atom } from 'recoil';
-import { Sprite } from 'application/sprites/Sprite';
-import { AtomKeys } from 'stores/recoilKeys';
-
-const SpriteTreeAtom = atom<Sprite[]>({
-    key: AtomKeys.SPRITE_TREE_ATOM,
-    default: [],
-});
-export { SpriteTreeAtom }
