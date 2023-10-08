9f340a64967ef19f6ba404a8e4a5dc09c2c58830
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-08 15:48:45 +0900

    update:"@mui/joy"用の設定削除、emotionのThemeProvider追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,54 +1,48 @@
-import { AppProps } from "next/app";
-import PropTypes from "prop-types";
-import Head from "next/head";
+import { CacheProvider, EmotionCache, ThemeProvider } from '@emotion/react';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import PropTypes from 'prop-types';
 import { RecoilRoot } from 'recoil';
-import { CacheProvider, EmotionCache } from "@emotion/react";
-import { CssBaseline, CssVarsProvider } from "@mui/joy";
-import createEmotionCache from "createEmotionCache";
-import theme from "theme";
+import createEmotionCache from 'createEmotionCache';
+import defaultTheme from 'theme';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
 interface MyAppProps extends AppProps {
-  emotionCache?: EmotionCache;
+    emotionCache?: EmotionCache;
 }
 
 function MyApp(props: MyAppProps) {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+    const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
-  return (<>
-    <Head>
-      <meta key="charset" name="charset" content="utf-8" />
-      <meta
-        key="viewport"
-        name="viewport"
-        content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
-      />
-      <meta property="og:locale" content="ja_JP" />
-      <meta property="og:type" content="website" />
-    </Head>
-    <CacheProvider value={emotionCache}>
-      <CssVarsProvider theme={theme} >
-
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-
-        <RecoilRoot>
-          <Component {...pageProps} />
-        </RecoilRoot>
-
-      </CssVarsProvider>
-    </CacheProvider>
-  </>
-
-  );
+    return (
+        <>
+            <Head>
+                <meta key="charset" name="charset" content="utf-8" />
+                <meta
+                    key="viewport"
+                    name="viewport"
+                    content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
+                />
+                <meta property="og:locale" content="ja_JP" />
+                <meta property="og:type" content="website" />
+            </Head>
+            <CacheProvider value={emotionCache}>
+                <ThemeProvider theme={defaultTheme}>
+                    <RecoilRoot>
+                        <Component {...pageProps} />
+                    </RecoilRoot>
+                </ThemeProvider>
+            </CacheProvider>
+        </>
+    );
 }
 
 MyApp.propTypes = {
-  Component: PropTypes.elementType.isRequired,
-  emotionCache: PropTypes.object,
-  pageProps: PropTypes.object.isRequired,
+    Component: PropTypes.elementType.isRequired,
+    emotionCache: PropTypes.object,
+    pageProps: PropTypes.object.isRequired,
 };
 
 export default MyApp;
