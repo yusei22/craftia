f7c88d36a298fc45f0ab3e7aead066d9d742d918
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-08 15:45:43 +0900

    move,rename:compoents/providerフォルダ以下に移動

--- src/components/provider/RenderViewProvider.ts
+++ src/components/provider/RenderViewProvider.ts
@@ -1,16 +1,16 @@
 import React, { useEffect } from 'react';
 import { useSetRecoilState } from 'recoil';
-import { renderViewScaleAtom, } from 'dataflow';
+import { renderViewScaleAtom } from 'dataflow';
 import { useWindowSize } from 'hooks';
 
-const RenderingAreaRoot = ({ children }: { children: React.ReactNode }) => {
+const RenderViewProvider = ({ children }: { children?: React.ReactNode }) => {
     const setRenderViewScale = useSetRecoilState(renderViewScaleAtom);
     const windowSize = useWindowSize();
 
     useEffect(() => {
-        setRenderViewScale(windowSize);
-    }, [windowSize])
+        //setRenderViewScale(windowSize);
+    }, [windowSize]);
     return children;
-}
+};
 
-export { RenderingAreaRoot }
+export { RenderViewProvider };
