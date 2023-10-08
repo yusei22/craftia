3c9bb5ecb2deee3944b020fd6cb8b655d5021824
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-08 15:39:24 +0900

    add: emotion用メディアクエリーのキー

---
+++ src/mediaQuery.ts
@@ -0,0 +1,6 @@
+export const MQ = {
+    sm: `@media (min-width: ${640}px)`,
+    md: `@media (min-width: ${768}px)`,
+    lg: `@media (min-width: ${1024}px)`,
+    xl: `@media (min-width: ${1280}px)`,
+};
