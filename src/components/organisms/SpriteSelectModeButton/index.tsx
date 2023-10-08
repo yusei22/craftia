157cd4b80b9a44097fedbe8f291bfc1e4a90c19a
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-08 15:50:19 +0900

    update:onDragの動作追加

--- src/components/organisms/SpriteSelectModeButton/index.tsx
+++ src/components/organisms/SpriteSelectModeButton/index.tsx
@@ -1,31 +1,44 @@
-import { useRenderViewTouchGesture } from "hooks/renderViews/useRenderViewTouchGesture";
-import { useRenderViewWheelGesture } from "hooks/renderViews/useRenderViewWheelGesture";
+import { IconButton } from '@mui/material';
 import { useSetRecoilState } from 'recoil';
-import { renderViewListenersAtom } from 'dataflow';
-import { useRenderViewMouseGesture } from "hooks/renderViews/useRenderViewMouseGesture";
+import { Vec2 } from 'application/core/units';
+import Wrapper from 'components/layout/Wrapper';
+import { renderViewListenersAtom, spriteTreeAtom } from 'dataflow';
+import { useRenderViewMouseGesture } from 'hooks/renderViews/useRenderViewMouseGesture';
+import { useRenderViewTouchGesture } from 'hooks/renderViews/useRenderViewTouchGesture';
+import { useRenderViewWheelGesture } from 'hooks/renderViews/useRenderViewWheelGesture';
+import { useSpriteHitDetection } from 'hooks/renderViews/useSpriteHitDetection';
 
-
-const SpriteSelectModeButton = () => {
+const SpriteSelectModeButton = ({ children }: { children?: React.ReactNode }) => {
     const { onWheel } = useRenderViewWheelGesture();
     const { onPinch } = useRenderViewTouchGesture();
-    const { onMove, } = useRenderViewMouseGesture();
+    const { onMove } = useRenderViewMouseGesture();
+
+    const detectHitSprite = useSpriteHitDetection();
 
     const setRenderViewListeners = useSetRecoilState(renderViewListenersAtom);
+    const setSpriteTree = useSetRecoilState(spriteTreeAtom);
 
     const onClick = () => {
         setRenderViewListeners({
             onPinch,
             onWheel,
-            onDrag: () => { },
-            onMove,
-        })
-    }
+            onDrag: ({ delta }) => {
+                setSpriteTree((sprites) => {
+                    const [spriteFront, spriteMiddle, ...spriteBAck] = sprites;
 
-    return (
-        <div onClick={onClick}>
-            セレクト!
-        </div>
-    )
-}
+                    const newSprite = spriteMiddle.clone();
+                    newSprite.prefs.globalLocation = newSprite.prefs.globalLocation.add(
+                        new Vec2(delta)
+                    );
+                    return [spriteFront, newSprite, ...spriteBAck];
+                });
+            },
+            onMove,
+            onClick: () => {},
+        });
+    };
+
+    return <Wrapper onClick={onClick}> {children} </Wrapper>;
+};
 
 export { SpriteSelectModeButton };
