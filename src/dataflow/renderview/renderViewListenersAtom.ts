378a9ceb6f8a5d005f0f4d76f6d3d55e08580baa
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-08 15:51:10 +0900

    update:フォーマット

--- src/dataflow/renderview/renderViewListenersAtom.ts
+++ src/dataflow/renderview/renderViewListenersAtom.ts
@@ -1,21 +1,29 @@
+import { Handler, SharedGestureState, WebKitGestureEvent } from '@use-gesture/react';
 import { atom } from 'recoil';
-import { Handler, WebKitGestureEvent } from '@use-gesture/react';
 
 type RenderViewListeners = {
-    onMove: Handler<"move", PointerEvent>;
-    onPinch: Handler<"pinch", PointerEvent | TouchEvent | WheelEvent | WebKitGestureEvent>;
-    onWheel: Handler<"wheel", WheelEvent>;
-    onDrag: Handler<"drag", PointerEvent | MouseEvent | TouchEvent | KeyboardEvent>;
-}
+    onMove: Handler<'move', PointerEvent>;
+    onPinch: Handler<'pinch', PointerEvent | TouchEvent | WheelEvent | WebKitGestureEvent>;
+    onWheel: Handler<'wheel', WheelEvent>;
+    onDrag: Handler<'drag', PointerEvent | MouseEvent | TouchEvent | KeyboardEvent>;
+    onClick: (
+        state: SharedGestureState & {
+            event: MouseEvent;
+            args: any;
+        },
+        ...args: any
+    ) => void;
+};
 const renderViewListenersAtom = atom<RenderViewListeners>({
     key: 'renderViewListenersAtom',
     default: {
-        onMove: () => { },
-        onPinch: () => { },
-        onWheel: () => { },
-        onDrag: () => { }
-    }
+        onMove: () => {},
+        onPinch: () => {},
+        onWheel: () => {},
+        onDrag: () => {},
+        onClick: () => {},
+    },
 });
 
-export type { RenderViewListeners }
-export { renderViewListenersAtom }
+export type { RenderViewListeners };
+export { renderViewListenersAtom };
