df025ffcef8243f874cd85f84d84e35728cda95a
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-11 23:00:58 +0900

    update:artboard解像度のステートを配列からVec2に変更

--- src/dataflow/artboard/artboardResolutionAtom.ts
+++ src/dataflow/artboard/artboardResolutionAtom.ts
@@ -1,7 +1,8 @@
 import { atom } from 'recoil';
+import { Vec2 } from 'application/core/units';
 
-const artboardResolutionAtom = atom<[number, number]>({
+const artboardResolutionAtom = atom<Vec2>({
     key: 'artboardResolutionAtom',
-    default: [1, 1],
+    default: new Vec2(1, 1),
 });
 export { artboardResolutionAtom };
