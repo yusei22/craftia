0710dcce1842831ca8cc95cd26316a23911438a2
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-11 22:58:12 +0900

    add:アクティブなスプライトのIDの配列を得るフック

---
+++ src/hooks/sprites/useGetActiveSpriteIdsSync.ts
@@ -0,0 +1,17 @@
+import { RecoilValue, useRecoilCallback } from 'recoil';
+import { activeSpriteIdsAtom } from 'dataflow/sprites/activeSpriteIdAtom';
+
+const useGetActiveSpriteIdsSync = () => {
+    const getActiveSpriteIdStateSync = useRecoilCallback<[state: RecoilValue<string[]>], string[]>(
+        ({ snapshot }) =>
+            (state) => {
+                return snapshot.getLoadable(state).contents;
+            },
+        []
+    );
+    const getActiveSpriteIdsSync = () => {
+        return getActiveSpriteIdStateSync(activeSpriteIdsAtom);
+    }
+    return getActiveSpriteIdsSync;
+}
+export { useGetActiveSpriteIdsSync };
