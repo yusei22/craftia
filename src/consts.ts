44ab459dd7a1350c84e92bfd56b2f98728140005
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-15 08:46:04 +0900

    update:パラメータ追加

--- src/consts.ts
+++ src/consts.ts
@@ -1,1 +1,18 @@
 export const WHEEL_CLOSE_UP_DEGREES = 0.1;
+
+export const HISTORY_STORAGE_LENGTH = 20;
+
+export const MAX_PEN_WIDTH = 200;
+export const MIN_PEN_WIDTH = 1;
+
+export const MIN_PEN_OPACITY = 0;
+export const MAX_PEN_OPACITY = 1.0;
+
+export const MIN_PEN_Stabilization = 0;
+export const MAX_PEN_Stabilization = 40;
+
+export const MAX_CONVOLUTION_RADIUS = 50;
+export const MIN_CONVOLUTION_RADIUS = 0;
+
+export const MAX_UNSHARPMASKING_THRESHOLD = 5.0;
+export const MIN_UNSHARPMASKING_THRESHOLD = 0.1;
