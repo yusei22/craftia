87a433a4abf451f9587b76a8f9709eeb27254b84
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-15 08:49:24 +0900

    update:型`ValueUpdater`を追加
    値か値の更新関数の型

--- src/application/core/types/index.ts
+++ src/application/core/types/index.ts
@@ -3,11 +3,10 @@ type Append<Elm, T extends unknown[]> = ((arg: Elm, ...rest: T) => void) extends
 ) => void
     ? T2
     : never;
-
-type AtLeastArray<N extends number, T> = AtLeastRec<N, T, T[], []>;
 type AtLeastRec<Num, Elm, T extends unknown[], C extends unknown[]> = {
     0: T;
     1: AtLeastRec<Num, Elm, Append<Elm, T>, Append<unknown, C>>;
 }[C extends { length: Num } ? 0 : 1];
 
-export type { AtLeastArray };
+export type AtLeastArray<N extends number, T> = AtLeastRec<N, T, T[], []>;
+export type ValueUpdater<T> = (prefs: T) => T;
