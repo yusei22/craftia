12e915f6c2eba6b548754a57697931d0410b8a7b
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-15 08:54:45 +0900

    update:`ContextPenWorker`クラス追加

--- src/application/pens/Pen.ts
+++ src/application/pens/Pen.ts
@@ -1,25 +1,105 @@
-import { PointerType } from '@use-gesture/react';
-import { Vec, Vec2 } from 'application/core/units';
+import { ISpriteWorker } from 'application/ISpriteWorker';
+import {
+    Context2D,
+    ContextLineConfig,
+    ContextShadowConfig,
+    ContextTextConfig,
+} from 'application/core/context-2d';
+import { ValueUpdater } from 'application/core/types';
+import { Vec2 } from 'application/core/units';
+import { Rasterizedmage } from 'application/sprites/RasterizedImage';
+import { RasterizedPreviewer } from 'application/sprites/RasterizedPreviewer';
+import { SpriteFillStyle } from 'application/sprites/Sprite';
 
-type pointerProps = {
-    readonly pointerLoc: Vec;
+export type PenEvent = {
+    /**ポインタの位置 */
+    readonly pointerLoc: Vec2;
+    /**筆圧 0 から 1*/
     readonly pressure: number;
-    readonly tangentialPressure: number;
-    readonly tilt: Vec2;
-    readonly twist: number;
-    readonly pointerType: PointerType;
 };
-
-type PointerMoveProps = pointerProps & { IsDragged: boolean };
-
-abstract class Pen {
-    abstract getPenWorker(image: TexImageSource): void;
+export interface PenPrefs {
+    /**手振れ補正 */
+    readonly stabilization: number;
+    /**リアルタイム手振れ補正 */
+    readonly realTimeStabilization: boolean;
+    /**塗りつぶし */
+    readonly fillStyle: SpriteFillStyle;
+    /**ブレンドモード */
+    readonly blendMode: GlobalCompositeOperation;
+    /**透明度 */
+    readonly opacity: number;
+    /**線の太さ */
+    readonly lineWidth: number;
 }
 
-abstract class PenWorker {
-    abstract pointerDown(props: pointerProps): void;
-    abstract pointermove(props: PointerMoveProps): void;
-    abstract pointerUp(props: pointerProps): void;
+export abstract class Pen<T extends PenPrefs = PenPrefs> {
+    readonly prefs: T;
+    readonly artboardSize: Vec2;
+    constructor(artboardSize: Vec2, prefs: T) {
+        this.artboardSize = artboardSize;
+        this.prefs = prefs;
+    }
+    abstract getWorker(rasterizedImage: Rasterizedmage): PenWorker;
+    abstract setPrefs(valOrUpdater: ValueUpdater<T>): Pen<T>;
 }
 
-export { Pen, PenWorker };
+export abstract class PenWorker implements ISpriteWorker {
+    abstract pointerDown(props: PenEvent): void;
+    abstract pointerDrag(props: PenEvent): void;
+    abstract pointerUp(props: PenEvent): void;
+    abstract getPreviewSprite(): RasterizedPreviewer;
+}
+export interface ContextPenWorkerConfig {
+    readonly line: ContextLineConfig | null;
+    readonly shadow: ContextShadowConfig | null;
+    readonly text: ContextTextConfig | null;
+    readonly fillStyle: SpriteFillStyle;
+    readonly globalAlpha: number | null;
+    readonly globalCompositeOperation: GlobalCompositeOperation | null;
+    readonly strokeStyle: SpriteFillStyle;
+}
+
+export abstract class ContextPenWorker extends PenWorker {
+    protected readonly baseContext: Context2D;
+    protected readonly lineContext: Context2D;
+    protected readonly targetSprite: Rasterizedmage;
+    protected readonly artboardSize: Vec2;
+
+    constructor(targetSprite: Rasterizedmage, artboardSize: Vec2) {
+        super();
+        this.baseContext = new Context2D();
+        this.lineContext = new Context2D();
+        this.targetSprite = targetSprite;
+        this.artboardSize = artboardSize;
+    }
+    private setConfig(context: Context2D, config: ContextPenWorkerConfig) {
+        context
+            .setLineConfig(config.line)
+            .setShadowConfig(config.shadow)
+            .setTextConfig(config.text)
+            .setGlobalAlpha(config.globalAlpha)
+            .setGlobalCompositeOperation(config.globalCompositeOperation)
+            .setFillStyle(
+                config.fillStyle === null ? null : config.fillStyle.createCanvasFillStyle(context)
+            )
+            .setStrokeStyle(
+                config.strokeStyle === null
+                    ? null
+                    : config.strokeStyle.createCanvasFillStyle(context)
+            );
+    }
+    protected setBaseContextconfig(config: ContextPenWorkerConfig) {
+        this.setConfig(this.baseContext, config);
+    }
+    protected setLineContextconfig(config: ContextPenWorkerConfig) {
+        this.setConfig(this.lineContext, config);
+    }
+    public getPreviewSprite() {
+        return new RasterizedPreviewer(this.baseContext.getCanvas(), {
+            ...this.targetSprite.prefs,
+            scale: this.baseContext.size,
+            rotation: 0,
+            globalLocation: new Vec2(0, 0),
+        });
+    }
+}
