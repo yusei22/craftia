6dd9ba10f6a9e5e74d6f85235f1d68e1573a671b
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-15 08:55:06 +0900

    add:ノーマルなペン

---
+++ src/application/pens/normalpen/index.ts
@@ -0,0 +1,115 @@
+import { DeferredStabilizer } from '../DeferredStabilizer';
+import { ContextPenWorker, ContextPenWorkerConfig, Pen, PenEvent, PenPrefs } from '../Pen';
+import { RealTimeStabilizer } from '../RealTimeStabilizer';
+import { ValueUpdater } from 'application/core/types';
+import { Vec2 } from 'application/core/units';
+import { Rasterizedmage } from 'application/sprites/RasterizedImage';
+
+export interface NormalPenPrefs extends PenPrefs {}
+
+export class NormalPen extends Pen<NormalPenPrefs> {
+    constructor(artboardSize: Vec2, prefs: NormalPenPrefs) {
+        super(artboardSize, prefs);
+    }
+    public getWorker(rasterizedImage: Rasterizedmage): NormalPenWorker {
+        return new NormalPenWorker(this.artboardSize, rasterizedImage, this.prefs);
+    }
+    public setPrefs = (valOrUpdater: ValueUpdater<NormalPenPrefs>) => {
+        return new NormalPen(
+            this.artboardSize,
+            typeof valOrUpdater === 'function' ? valOrUpdater(this.prefs) : valOrUpdater
+        );
+    };
+}
+const baseContextConfig = (prefs: NormalPenPrefs): ContextPenWorkerConfig => ({
+    line: null,
+    shadow: null,
+    text: null,
+    fillStyle: null,
+    globalAlpha: prefs.opacity,
+    globalCompositeOperation: prefs.blendMode,
+    strokeStyle: null,
+});
+const lineContextConfig = (prefs: NormalPenPrefs): ContextPenWorkerConfig => ({
+    line: {
+        lineCap: 'round',
+        lineDashOffset: 0,
+        lineJoin: 'round',
+        lineWidth: prefs.lineWidth,
+    },
+    shadow: null,
+    text: null,
+    fillStyle: null,
+    globalAlpha: prefs.opacity,
+    globalCompositeOperation: 'source-over',
+    strokeStyle: prefs.fillStyle,
+});
+export class NormalPenWorker extends ContextPenWorker {
+    private readonly prefs: NormalPenPrefs;
+    private readonly stabilizer: DeferredStabilizer | RealTimeStabilizer;
+    private readonly targetSpriteVisible: Rasterizedmage;
+
+    constructor(artboardSize: Vec2, targetSprite: Rasterizedmage, prefs: NormalPenPrefs) {
+        super(targetSprite, artboardSize);
+
+        this.targetSpriteVisible = targetSprite.setPrefs((culPrefs) => ({
+            ...culPrefs,
+            visible: true,
+        }));
+
+        this.prefs = prefs;
+
+        this.stabilizer = this.prefs.realTimeStabilization
+            ? new RealTimeStabilizer(this.prefs.stabilization)
+            : new DeferredStabilizer(this.prefs.stabilization);
+
+        this.baseContext.viewport(artboardSize);
+        this.lineContext.viewport(artboardSize);
+        this.applyLine();
+    }
+    private applyLine() {
+        this.baseContext.clear();
+        this.targetSpriteVisible.draw(this.baseContext);
+
+        this.setBaseContextconfig(baseContextConfig(this.prefs));
+
+        this.baseContext.drawImage(this.lineContext.getCanvas(), new Vec2(0, 0));
+    }
+    private setLineConfig() {
+        this.setLineContextconfig(lineContextConfig(this.prefs));
+    }
+    public drawPoints(points: Vec2[]) {
+        this.setLineConfig();
+        this.lineContext.resetTransform();
+        this.lineContext.clear();
+        this.lineContext.beginPath();
+
+        this.lineContext.moveTo(points[0]);
+
+        for (let i = 1; i < points.length - 1; i++) {
+            const midPoint = points[i].midpoint(points[i - 1]);
+
+            this.lineContext.quadraticCurveTo(points[i - 1], midPoint);
+        }
+
+        this.lineContext.stroke();
+        this.applyLine();
+    }
+
+    public pointerDown(e: PenEvent) {
+        this.stabilizer.begin();
+        this.stabilizer.add(e.pointerLoc);
+
+        this.drawPoints(this.stabilizer.getAll());
+    }
+    public pointerDrag(e: PenEvent) {
+        this.stabilizer.add(e.pointerLoc);
+
+        this.drawPoints(this.stabilizer.getAll());
+    }
+    public pointerUp(e: PenEvent) {
+        this.stabilizer.add(e.pointerLoc);
+
+        this.drawPoints(this.stabilizer.getStabilizedAll());
+    }
+}
