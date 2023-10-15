2c83df206f78a5bbfe7d94a9675d2c917f78147b
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-15 10:31:05 +0900

    rename:SpriteSelectModeButtonWrapperに

---
+++ src/components/organisms/SpriteSelectModeButtonWrapper/index.tsx
@@ -0,0 +1,54 @@
+import { useSetRecoilState } from 'recoil';
+import { Vec2 } from 'application/core/units';
+import Wrapper from 'components/layout/Wrapper';
+import { renderViewListenersAtom, useSpriteTreeSaver } from 'dataflow';
+import { activeSpriteIdsAtom } from 'dataflow/sprites/activeSpriteIdAtom';
+import { useMoveSprite } from 'hooks/renderViews/useMoveSprite';
+import { useRenderViewMouseGesture } from 'hooks/renderViews/useRenderViewMouseGesture';
+import { useRenderViewTouchGesture } from 'hooks/renderViews/useRenderViewTouchGesture';
+import { useRenderViewWheelGesture } from 'hooks/renderViews/useRenderViewWheelGesture';
+import { useSpriteHitDetection } from 'hooks/renderViews/useSpriteHitDetection';
+import { useGetActiveSprites } from 'hooks/sprites/useGetActiveSprites';
+
+const SpriteSelectModeButtonWrapper = ({ children }: { children?: React.ReactNode }) => {
+    const { onWheel } = useRenderViewWheelGesture();
+    const { onPinch } = useRenderViewTouchGesture();
+    const { onMove } = useRenderViewMouseGesture();
+
+    const moveSprite = useMoveSprite();
+    const setActiveSpriteIds = useSetRecoilState(activeSpriteIdsAtom);
+    const detectHitSprite = useSpriteHitDetection();
+    const setRenderViewListeners = useSetRecoilState(renderViewListenersAtom);
+    const getActiveSprites = useGetActiveSprites();
+    const saveSpriteTree = useSpriteTreeSaver();
+
+    const onClick = () => {
+        setRenderViewListeners({
+            onPinch,
+            onWheel,
+            onDrag: ({ delta, first, xy, touches, last }) => {
+                if (touches > 1) {
+                    return;
+                }
+                if (first) {
+                    const hitSprite = detectHitSprite(new Vec2(xy));
+                    setActiveSpriteIds(hitSprite?.prefs.id ? [hitSprite?.prefs.id] : []);
+                    return;
+                }
+                const activeSprite = getActiveSprites()[0];
+                if (activeSprite) {
+                    moveSprite(activeSprite, new Vec2(delta));
+                    if (last) {
+                        saveSpriteTree();
+                    }
+                }
+            },
+            onMove,
+            onClick: () => {},
+        });
+    };
+
+    return <Wrapper onClick={onClick}> {children} </Wrapper>;
+};
+
+export { SpriteSelectModeButtonWrapper };
