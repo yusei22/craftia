92103eb39d12f784de0a752e0d512f9e5544a970
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-15 10:33:16 +0900

    add:レンダ―ビューのイベントリスナ管理をするコンポーネント

---
+++ src/components/provider/RenderViewListenerProvider.ts
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+import { useSetRecoilState } from 'recoil';
+import { renderViewListenersAtom } from 'dataflow';
+import { useRenderViewMouseGesture } from 'hooks/renderViews/useRenderViewMouseGesture';
+import { useRenderViewTouchGesture } from 'hooks/renderViews/useRenderViewTouchGesture';
+import { useRenderViewWheelGesture } from 'hooks/renderViews/useRenderViewWheelGesture';
+
+export const RenderViewListenerProvider = ({ children }: { children?: React.ReactNode }) => {
+    const setRenderViewListeners = useSetRecoilState(renderViewListenersAtom);
+
+    const { onWheel } = useRenderViewWheelGesture();
+    const { onPinch } = useRenderViewTouchGesture();
+    const { onMove } = useRenderViewMouseGesture();
+
+    useEffect(() => {
+        setRenderViewListeners({
+            onPinch,
+            onWheel,
+            onDrag: () => {},
+            onMove,
+            onClick: () => {},
+        });
+    }, []);
+
+    return children;
+};
