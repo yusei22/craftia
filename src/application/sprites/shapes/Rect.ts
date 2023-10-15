76a86f1163652367114aeb109081336ddef4fae4
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-16 00:40:17 +0900

    update:context.roundRect使わない

--- src/application/sprites/shapes/Rect.ts
+++ src/application/sprites/shapes/Rect.ts
@@ -40,7 +40,7 @@ class Rect extends Shape<RectPrefs> {
         context.translate(this.prefs.globalLocation);
         context.rotate(this.prefs.rotation);
         context.translate(this.prefs.globalLocation.times(-1));
-        context.roundRect(this.getStartPoint(), this.prefs.scale);
+        context.rect(this.getStartPoint(), this.prefs.scale);
         context.fill();
         context.stroke();
     }
