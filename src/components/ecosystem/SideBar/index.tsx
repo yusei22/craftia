52222fa88b7f370fefe91c004320f86eae194c5f
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-15 09:36:50 +0900

    add:サイドバーコンポーネント

---
+++ src/components/ecosystem/SideBar/index.tsx
@@ -0,0 +1,91 @@
+import ModeEditOutlineIcon from '@mui/icons-material/ModeEditOutline';
+import NearMeIcon from '@mui/icons-material/NearMe';
+import OpenWithIcon from '@mui/icons-material/OpenWith';
+import PanToolIcon from '@mui/icons-material/PanTool';
+import { MouseEventHandler, useState } from 'react';
+import IconButton from 'components/atoms/IconButton';
+import Container from 'components/layout/Container';
+import { ArtboardMoveModeButtonWrapper } from 'components/organisms/ArtboardMoveModeButtonWrapper';
+import { PenModeButtonWrapper } from 'components/organisms/PenModeButtonWrapper';
+import { SpriteMoveModeButtonWrapper } from 'components/organisms/SpriteMoveModeButtonWrapper';
+import { SpriteSelectModeButtonWrapper } from 'components/organisms/SpriteSelectModeButtonWrapper';
+
+type SideBarIconProps = {
+    children?: React.ReactNode;
+    isActive?: boolean;
+    onClick?: MouseEventHandler<HTMLButtonElement>;
+};
+
+const SideBarIcon = ({ children, isActive, onClick }: SideBarIconProps) => {
+    return (
+        <IconButton
+            variant={isActive ? 'primary' : 'translucent'}
+            css={{
+                borderRadius: '5px',
+            }}
+            onClick={onClick}
+        >
+            {children}
+        </IconButton>
+    );
+};
+type SideBarButtonNames = 'select' | 'pen' | 'artboardPan' | 'spriteMove';
+
+export const SideBar = () => {
+    const [active, setActive] = useState<SideBarButtonNames | null>(null);
+
+    return (
+        <Container
+            css={(theme) => ({
+                width: '50px',
+                height: '100%',
+                backgroundColor: theme.colors.white,
+                boxShadow: '0 0 35px 0 rgba(0, 0, 0, .2)',
+                flexDirection: 'column',
+                justifyContent: 'flex-start',
+                padding: '20px 0px',
+            })}
+        >
+            <SpriteSelectModeButtonWrapper>
+                <SideBarIcon
+                    onClick={() => {
+                        setActive('select');
+                    }}
+                    isActive={active === 'select'}
+                >
+                    <NearMeIcon></NearMeIcon>
+                </SideBarIcon>
+            </SpriteSelectModeButtonWrapper>
+            <PenModeButtonWrapper>
+                <SideBarIcon
+                    onClick={() => {
+                        setActive('pen');
+                    }}
+                    isActive={active === 'pen'}
+                >
+                    <ModeEditOutlineIcon></ModeEditOutlineIcon>
+                </SideBarIcon>
+            </PenModeButtonWrapper>
+            <ArtboardMoveModeButtonWrapper>
+                <SideBarIcon
+                    onClick={() => {
+                        setActive('artboardPan');
+                    }}
+                    isActive={active === 'artboardPan'}
+                >
+                    <PanToolIcon></PanToolIcon>
+                </SideBarIcon>
+            </ArtboardMoveModeButtonWrapper>
+            <SpriteMoveModeButtonWrapper>
+                <SideBarIcon
+                    onClick={() => {
+                        setActive('spriteMove');
+                    }}
+                    isActive={active === 'spriteMove'}
+                >
+                    <OpenWithIcon></OpenWithIcon>
+                </SideBarIcon>
+            </SpriteMoveModeButtonWrapper>
+        </Container>
+    );
+};
