073ed35b36e2c0f5f1e5bc5a342bd9030a463ef8
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-15 10:42:43 +0900

    add:ペンを使うフック。ドラッグイベントのリスナを返す

---
+++ src/hooks/pens/usePen.ts
@@ -0,0 +1,83 @@
+import { useCallback } from 'react';
+import { useSetRecoilState } from 'recoil';
+import { useGetActivePen } from './useGetActivePen';
+import { Vec2 } from 'application/core/units';
+import { PenWorker } from 'application/pens/Pen';
+import { Rasterizedmage } from 'application/sprites/RasterizedImage';
+import { searchSpriteFromID } from 'application/sprites/Sprite';
+import { RenderViewListeners, spriteTreeAtom, useSpriteTreeSaver } from 'dataflow';
+import { useGetViewPointToArtboardPointConverter } from 'hooks/artboards/useGetViewPointToArtboardPointConverter';
+import { useGetActiveSprites } from 'hooks/sprites/useGetActiveSprites';
+
+const usePen = () => {
+    const getActiveSprite = useGetActiveSprites();
+    const viewPointToArtboardPoint = useGetViewPointToArtboardPointConverter();
+    const getActivePen = useGetActivePen();
+    const setSpriteTree = useSetRecoilState(spriteTreeAtom);
+    const saveSpriteTree = useSpriteTreeSaver();
+
+    let penWorker: PenWorker | null = null;
+    let targetID: string = '';
+
+    const setPenWorkerPreviewSprite = () => {
+        setSpriteTree((currentTree) => {
+            const newTree = [...currentTree];
+            const [targetSprite, index] = searchSpriteFromID(currentTree, targetID);
+
+            if (targetSprite instanceof Rasterizedmage && penWorker && index !== null) {
+                newTree[index] = penWorker.getPreviewSprite();
+            }
+            return newTree;
+        });
+    };
+    const onDragStart: RenderViewListeners['onDrag'] = useCallback(({ xy, touches }) => {
+        if (touches > 1) {
+            return;
+        }
+
+        const activeSprite = getActiveSprite()[0];
+        if (!activeSprite) return;
+
+        if (activeSprite instanceof Rasterizedmage) {
+            const artboardPoint = viewPointToArtboardPoint(new Vec2(xy));
+
+            targetID = activeSprite.prefs.id;
+            penWorker = getActivePen().getWorker(activeSprite);
+            penWorker.pointerDown({
+                pointerLoc: artboardPoint,
+                pressure: 0.5,
+            });
+            console.log(penWorker);
+            setPenWorkerPreviewSprite();
+        }
+    }, []);
+
+    const onDrag: RenderViewListeners['onDrag'] = useCallback(({ xy, touches }) => {
+        if (touches > 1) {
+            return;
+        }
+        if (!penWorker) return;
+
+        const artboardPoint = viewPointToArtboardPoint(new Vec2(xy));
+        penWorker.pointerDrag({
+            pointerLoc: artboardPoint,
+            pressure: 0.5,
+        });
+        setPenWorkerPreviewSprite();
+    }, []);
+    const onDragEnd: RenderViewListeners['onDrag'] = useCallback(({ xy }) => {
+        if (!penWorker) return;
+
+        const artboardPoint = viewPointToArtboardPoint(new Vec2(xy));
+        penWorker.pointerUp({
+            pointerLoc: artboardPoint,
+            pressure: 0.5,
+        });
+        setPenWorkerPreviewSprite();
+        saveSpriteTree();
+        penWorker = null;
+        targetID = '';
+    }, []);
+    return { onDrag, onDragStart, onDragEnd };
+};
+export { usePen };
