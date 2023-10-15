c3d7766a39048b25288f97063d7171d460197820
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-15 10:38:57 +0900

    update:ステート値がVec2型に変わったので合わせる

--- src/hooks/artboards/useChangeArtboardAnchor.ts
+++ src/hooks/artboards/useChangeArtboardAnchor.ts
@@ -1,3 +1,4 @@
+import { useCallback } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { Vec2 } from 'application/core/units';
 import { rotatePoint } from 'application/utils';
@@ -6,41 +7,37 @@ import { artboardTransformAtom } from 'dataflow';
 const useChangeArtboardAnchor = () => {
     const setArtboardTrans = useSetRecoilState(artboardTransformAtom);
 
-    return (newAnchor: Vec2, freezeTransform: boolean) => {
+    return useCallback((newAnchor: Vec2, freezeTransform: boolean) => {
         if (freezeTransform) {
             setArtboardTrans(({ anchor, location, scale, rotation }) => {
-                const currentAnchor = new Vec2(anchor);
-                const currentLocation = new Vec2(location);
-                const currentScale = new Vec2(scale);
-
                 const AnchorsRelativeDifference = new Vec2(
-                    (newAnchor.x - currentAnchor.x) * currentScale.x,
-                    (newAnchor.y - currentAnchor.y) * currentScale.y
+                    (newAnchor.x - anchor.x) * scale.x,
+                    (newAnchor.y - anchor.y) * scale.y
                 );
 
                 const newLocation = rotatePoint(
-                    currentLocation,
-                    currentLocation.add(AnchorsRelativeDifference),
+                    location,
+                    location.add(AnchorsRelativeDifference),
                     rotation
                 );
                 return {
-                    anchor: newAnchor.toArray(),
-                    location: newLocation.toArray(),
-                    scale: currentScale.toArray(),
-                    rotation: rotation,
+                    anchor: newAnchor,
+                    location: newLocation,
+                    scale,
+                    rotation,
                 };
             });
         } else {
             setArtboardTrans(({ location, scale, rotation }) => {
                 return {
-                    anchor: newAnchor.toArray(),
+                    anchor: newAnchor,
                     location,
                     scale,
                     rotation,
                 };
             });
         }
-    };
+    }, []);
 };
 
 export { useChangeArtboardAnchor };
