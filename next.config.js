e79f082a65114082779b261a169f7f73e105ecea
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-15 13:01:34 +0900

    update:#17 eslint無効化しない

--- next.config.js
+++ next.config.js
@@ -13,12 +13,7 @@ const nextConfig = {
       exclude: /node_modules/,
     });
     return config;
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  }
 }
 
 module.exports = nextConfig
