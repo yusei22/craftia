f84af204ed2d23335a274abde8553352b862280c
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-15 10:35:49 +0900

    update:ステ―トの値をnumber[]型からVec2に変更

--- src/dataflow/renderview/renderViewScaleAtom.ts
+++ src/dataflow/renderview/renderViewScaleAtom.ts
@@ -1,8 +1,9 @@
 import { atom } from 'recoil';
+import { Vec2 } from 'application/core/units';
 
-const renderViewScaleAtom = atom<[number, number]>({
+const renderViewScaleAtom = atom<Vec2>({
     key: 'enderViewScaleAtom',
-    default: [1, 1],
+    default: new Vec2(1, 1),
 });
 
 export { renderViewScaleAtom };
