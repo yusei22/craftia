5c846ea94e50f17916d11b267bd1789a322cd5b2
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-15 09:52:09 +0900

    add:ペンモードにするボタンのラッパー

---
+++ src/components/organisms/PenModeButtonWrapper/index.tsx
@@ -0,0 +1,39 @@
+import { useSetRecoilState } from 'recoil';
+import Wrapper from 'components/layout/Wrapper';
+import { renderViewListenersAtom } from 'dataflow';
+import { usePen } from 'hooks/pens/usePen';
+import { useRenderViewMouseGesture } from 'hooks/renderViews/useRenderViewMouseGesture';
+import { useRenderViewTouchGesture } from 'hooks/renderViews/useRenderViewTouchGesture';
+import { useRenderViewWheelGesture } from 'hooks/renderViews/useRenderViewWheelGesture';
+
+const PenModeButtonWrapper = ({ children }: { children?: React.ReactNode }) => {
+    const { onWheel } = useRenderViewWheelGesture();
+    const { onPinch } = useRenderViewTouchGesture();
+    const { onMove } = useRenderViewMouseGesture();
+    const { onDrag, onDragStart, onDragEnd } = usePen();
+    const setRenderViewListeners = useSetRecoilState(renderViewListenersAtom);
+
+    const onClick = () => {
+        setRenderViewListeners({
+            onPinch,
+            onWheel,
+            onDrag: (e) => {
+                if (e.first) {
+                    onDragStart(e);
+                    return;
+                }
+                if (e.last) {
+                    onDragEnd(e);
+                    return;
+                }
+                onDrag(e);
+            },
+            onMove,
+            onClick: () => {},
+        });
+    };
+
+    return <Wrapper onClick={onClick}> {children} </Wrapper>;
+};
+
+export { PenModeButtonWrapper };
