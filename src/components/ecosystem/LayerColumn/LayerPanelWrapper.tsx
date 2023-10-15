991d68b35cd74f97a8ca0381c616d09c7a0c83a1
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-15 09:33:38 +0900

    add:レイヤーパネルのラッパーコンポーネント

---
+++ src/components/ecosystem/LayerColumn/LayerPanelWrapper.tsx
@@ -0,0 +1,20 @@
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+import { forwardRef } from 'react';
+import { Sprite } from 'application/sprites/Sprite';
+import Wrapper from 'components/layout/Wrapper';
+import { LayerPanel } from 'components/organisms/LayerPanel';
+
+export const LayerPanelWrapper = forwardRef(({ id, sprite }: { id: string; sprite: Sprite }) => {
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: id });
+    const style = {
+        transform: CSS.Transform.toString(transform),
+        transition,
+    };
+
+    return (
+        <Wrapper ref={setNodeRef} style={style} {...attributes} {...listeners}>
+            <LayerPanel sprite={sprite} />
+        </Wrapper>
+    );
+});
