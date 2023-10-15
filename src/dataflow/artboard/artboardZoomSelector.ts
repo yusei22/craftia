b49ab945326635f19b85e3410250a194a3025d3e
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-15 10:34:57 +0900

    update:ズーム値の基準をレンダ―ビューのサイズからartboardの解像度に変更

--- src/dataflow/artboard/artboardZoomSelector.ts
+++ src/dataflow/artboard/artboardZoomSelector.ts
@@ -1,13 +1,12 @@
 import { selector } from 'recoil';
-import { renderViewScaleAtom } from '../renderview/renderViewScaleAtom';
+import { artboardResolutionAtom } from './artboardResolutionAtom';
 import { artboardTransformAtom } from './artboardTransformAtom';
-import { Vec2 } from 'application/core/units';
 
 const artbordZoomSelector = selector<number>({
     key: 'artbordZoomSelector',
     get: ({ get }) => {
         const artboardTrans = get(artboardTransformAtom);
-        const renderViewScale = get(renderViewScaleAtom);
+        const renderViewScale = get(artboardResolutionAtom);
 
         const widthZoom = artboardTrans.scale.x / renderViewScale.x;
         const heightZoom = artboardTrans.scale.y / renderViewScale.y;
@@ -16,7 +15,7 @@ const artbordZoomSelector = selector<number>({
     },
     set: ({ set, get }, newValue) => {
         if (typeof newValue === 'number') {
-            const newScale = get(renderViewScaleAtom).times(newValue);
+            const newScale = get(artboardResolutionAtom).times(newValue);
 
             set(artboardTransformAtom, ({ anchor, location, rotation }) => {
                 return {
