294152feead5d906c7366c419ae9dcd5996f6cd9
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-15 09:32:48 +0900

    add:ファイルタブコンポーネント

---
+++ src/components/ecosystem/FileTabPanel.ts/index.tsx
@@ -0,0 +1,28 @@
+import Container from 'components/layout/Container';
+import { TabSection } from 'components/molecules/TabSection';
+import { RenoButton } from 'components/organisms/RenoButton';
+import { UndoButton } from 'components/organisms/UndoButton';
+
+export const FileTabPanel = () => {
+    return (
+        <>
+            <Container
+                css={{
+                    flexWrap: 'wrap',
+                    justifyContent: 'flex-start',
+                    alignItems: 'flex-start',
+                    height: '100%',
+                    overflowY: 'auto',
+                    padding: '0px 10px',
+                }}
+            >
+                <TabSection>
+                    <UndoButton />
+                </TabSection>
+                <TabSection>
+                    <RenoButton />
+                </TabSection>
+            </Container>
+        </>
+    );
+};
