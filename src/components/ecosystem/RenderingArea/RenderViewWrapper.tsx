81ec00d69e97547e3bfd32c97259412202743c0b
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-15 13:37:58 +0900

    update:ジェスチャーの当て方変更

--- src/components/ecosystem/RenderingArea/RenderViewWrapper.tsx
+++ src/components/ecosystem/RenderingArea/RenderViewWrapper.tsx
@@ -1,4 +1,4 @@
-import { useGesture } from '@use-gesture/react';
+import { useDrag, useMove, usePinch, useWheel } from '@use-gesture/react';
 import { useRef } from 'react';
 import Wrapper from 'components/layout/Wrapper';
 import { RenderViewListeners } from 'dataflow';
@@ -9,21 +9,15 @@ type RenderViewWrapperProps = {
 
 const RenderViewWrapper = ({ children, events }: RenderViewWrapperProps) => {
     const wrapperRef = useRef<HTMLDivElement>(null);
-    useGesture(
-        {
-            ...events,
-            onWheel: () => {
-                console.log('onwheel');
-            },
-            onPinch: () => {
-                console.log('onPinch');
-            },
-        },
-        { eventOptions: { passive: false }, target: wrapperRef }
-    );
+
+    useDrag(events.onDrag, { eventOptions: { passive: false }, target: wrapperRef });
+    usePinch(events.onPinch, { eventOptions: { passive: false }, target: wrapperRef });
+    useWheel(events.onWheel, { eventOptions: { passive: false }, target: wrapperRef });
+    useMove(events.onMove, { eventOptions: { passive: false }, target: wrapperRef });
+
     console.log('RenderViewListeners : ', events);
     return (
-        <Wrapper ref={wrapperRef} css={{ touchAction: 'none' }}>
+        <Wrapper css={{ touchAction: 'none' }} ref={wrapperRef}>
             {children}
         </Wrapper>
     );
