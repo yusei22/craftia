83c383d446688f45ab6f063827abcb1f6d76b1c5
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-15 09:43:17 +0900

    update:#4 propsでClassAttributesも受け取る

--- src/components/layout/Wrapper/index.tsx
+++ src/components/layout/Wrapper/index.tsx
@@ -1,21 +1,26 @@
 import { Interpolation, Theme } from '@emotion/react';
-import { HTMLAttributes, forwardRef } from 'react';
+import { ClassAttributes, HTMLAttributes, forwardRef } from 'react';
 
-type WrapperProps = HTMLAttributes<HTMLDivElement> & {
+type WrapperProps = {
     css?: Interpolation<Theme>;
-};
+} & HTMLAttributes<HTMLDivElement> &
+    ClassAttributes<HTMLDivElement>;
 
 const Wrapper = forwardRef<HTMLDivElement, WrapperProps>(({ css, ...props }, ref) => {
     return (
         <div
             ref={ref}
-            css={[{
-                position: 'relative',
-                width: 'fit-content',
-                margin: '0px',
-                padding: '0px',
-                fontSize: '0px',
-            }, css]}
+            css={[
+                {
+                    position: 'relative',
+                    width: 'fit-content',
+                    height: 'fit-content',
+                    margin: '0px',
+                    padding: '0px',
+                    fontSize: '0px',
+                },
+                css,
+            ]}
             {...props}
         ></div>
     );
