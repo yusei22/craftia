021e7a2d9e78b31f1a7532edb85f3859e34965e1
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-21 08:52:09 +0900

    update:setSpriteメソッド追加

--- src/application/layers/Layer.ts
+++ src/application/layers/Layer.ts
@@ -1,12 +1,17 @@
+import { ValueUpdater } from 'application/core/types';
 import { Sprite } from 'application/sprites/Sprite';
 
 class Layer<T extends Sprite> {
     readonly sprite: T;
-    get id() {
-        return this.sprite.prefs.id;
-    }
+    readonly id: string;
     constructor(sprite: T) {
         this.sprite = sprite;
+        this.id = '';
+    }
+    public setSprite(valOrUpdater: ValueUpdater<Sprite> | Sprite) {
+        return new Layer(
+            typeof valOrUpdater === 'function' ? valOrUpdater(this.sprite) : valOrUpdater
+        );
     }
 }
 export { Layer };
