e1a0fc340bafdbb175ebe94f30a9895d4be4a4b2
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-21 08:54:30 +0900

    update:フィルタのボタン追加

--- src/components/ecosystem/FilterTabPabel/index.tsx
+++ src/components/ecosystem/FilterTabPabel/index.tsx
@@ -2,6 +2,10 @@ import Button from 'components/atoms/Button';
 import Container from 'components/layout/Container';
 import { TabSection } from 'components/molecules/TabSection';
 import { BilateralButton } from 'components/organisms/Filters/BilateralButton';
+import { GaussianBlurButton } from 'components/organisms/Filters/GaussianBlurButton';
+import { IllustrationButton } from 'components/organisms/Filters/IllustrationButton';
+import { InversionButton } from 'components/organisms/Filters/InversionButton';
+import { SobelButton } from 'components/organisms/Filters/SobelButton';
 import { UnsharpMaskingButton } from 'components/organisms/Filters/UnsharpMaskingButton';
 
 export const FilterTabPabel = () => {
@@ -26,6 +30,26 @@ export const FilterTabPabel = () => {
                     <BilateralButton>
                         <Button>バイラテラル</Button>
                     </BilateralButton>
+                </TabSection>
+                <TabSection>
+                    <GaussianBlurButton>
+                        <Button>ガウシアンフィルタ</Button>
+                    </GaussianBlurButton>
+                </TabSection>
+                <TabSection>
+                    <SobelButton>
+                        <Button>ソーベル</Button>
+                    </SobelButton>
+                </TabSection>
+                <TabSection>
+                    <InversionButton>
+                        <Button>ネガポジ反転</Button>
+                    </InversionButton>
+                </TabSection>
+                <TabSection>
+                    <IllustrationButton>
+                        <Button variant="danger">{'[NEW]'} イラスト化</Button>
+                    </IllustrationButton>
                 </TabSection>
             </Container>
         </>
