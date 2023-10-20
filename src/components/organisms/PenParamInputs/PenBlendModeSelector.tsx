411185d03ba4bd6cb9aaf05b73dd87310a86ec62
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-21 08:57:06 +0900

    update:フォーマット

--- src/components/organisms/PenParamInputs/PenBlendModeSelector.tsx
+++ src/components/organisms/PenParamInputs/PenBlendModeSelector.tsx
@@ -22,7 +22,7 @@ const options: BlendModeOption[] = [
     { value: 'soft-light', label: 'ソフトライト' },
     { value: 'difference', label: '差' },
     { value: 'exclusion', label: '除外' },
-    { value: 'destination-out', label: '消しごむ' }
+    { value: 'destination-out', label: '消しごむ' },
 ];
 
 export const PenBlendModeSelector = () => {
