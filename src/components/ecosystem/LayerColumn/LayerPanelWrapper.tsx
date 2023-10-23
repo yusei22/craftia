2beaa35ef436989861d8edae2add5c0e86bd8ad3
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-24 07:09:19 +0900

    update:#7 SpriteのPrefsの型引数追加

--- src/components/ecosystem/LayerColumn/LayerPanelWrapper.tsx
+++ src/components/ecosystem/LayerColumn/LayerPanelWrapper.tsx
@@ -1,10 +1,16 @@
 import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
-import { Sprite } from 'application/sprites/Sprite';
+import { Sprite, SpritePrefs } from 'application/sprites/Sprite';
 import Wrapper from 'components/layout/Wrapper';
 import { LayerPanel } from 'components/organisms/LayerPanel';
 
-export const LayerPanelWrapper = ({ id, sprite }: { id: string; sprite: Sprite }) => {
+export const LayerPanelWrapper = <T extends SpritePrefs>({
+    id,
+    sprite,
+}: {
+    id: string;
+    sprite: Sprite<T>;
+}) => {
     const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: id });
     const style = {
         transform: CSS.Transform.toString(transform),
