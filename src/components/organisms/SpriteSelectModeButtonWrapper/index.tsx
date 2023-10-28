dc083f021afe4a6d947535b078fdc6983e4c7873
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-28 21:57:55 +0900

    update:#18 フック名更新

--- src/components/organisms/SpriteSelectModeButtonWrapper/index.tsx
+++ src/components/organisms/SpriteSelectModeButtonWrapper/index.tsx
@@ -3,23 +3,23 @@ import { Vec2 } from 'application/core/units';
 import Wrapper from 'components/layout/Wrapper';
 import { renderViewListenersAtom, useSpriteTreeSaver } from 'dataflow';
 import { activeSpriteIdsAtom } from 'dataflow/sprites/activeSpriteIdAtom';
-import { useMoveSprite } from 'hooks/renderViews/useMoveSprite';
 import { useRenderViewMouseGesture } from 'hooks/renderViews/useRenderViewMouseGesture';
 import { useRenderViewTouchGesture } from 'hooks/renderViews/useRenderViewTouchGesture';
 import { useRenderViewWheelGesture } from 'hooks/renderViews/useRenderViewWheelGesture';
 import { useSpriteHitDetection } from 'hooks/renderViews/useSpriteHitDetection';
-import { useGetActiveSprites } from 'hooks/sprites/useGetActiveSprites';
+import { useSpriteMover } from 'hooks/renderViews/useSpriteMover';
+import { useActiveSpritesReader } from 'hooks/sprites/useActiveSpritesReader';
 
 const SpriteSelectModeButtonWrapper = ({ children }: { children?: React.ReactNode }) => {
     const { onWheel } = useRenderViewWheelGesture();
     const { onPinch } = useRenderViewTouchGesture();
     const { onMove } = useRenderViewMouseGesture();
 
-    const moveSprite = useMoveSprite();
+    const moveSprite = useSpriteMover();
     const setActiveSpriteIds = useSetRecoilState(activeSpriteIdsAtom);
     const detectHitSprite = useSpriteHitDetection();
     const setRenderViewListeners = useSetRecoilState(renderViewListenersAtom);
-    const getActiveSprites = useGetActiveSprites();
+    const getActiveSprites = useActiveSpritesReader();
     const saveSpriteTree = useSpriteTreeSaver();
 
     const onClick = () => {
