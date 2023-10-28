0ff190279df45ade3bf0c26b9778d98819f16cc8
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-28 15:09:55 +0900

    update:#7 setPrefsメソッドを`setPreviewerPrefs`に改名
    update:#7 `setSpritePrefs`メソッド追加
    
    update:メソッド並び替え

--- src/application/sprites/SmartImagePreviewer.ts
+++ src/application/sprites/SmartImagePreviewer.ts
@@ -1,15 +1,23 @@
 import { Previewer, PreviewerPrefs } from './Previewer';
 import { SmartImage } from './SmartImage';
+import { SpritePrefs } from './Sprite';
 import { ValueUpdater } from 'application/core/types';
 
 export class SmartImagePreviewer extends Previewer {
-    public async createStatic() {
-        const image = await createImageBitmap(this.source);
-        return new SmartImage(image, this.prefs);
+    public setSpritePrefs(valOrUpdater: ValueUpdater<SpritePrefs> | SpritePrefs) {
+        const newPrefs =
+            typeof valOrUpdater === 'function' ? valOrUpdater(this.prefs) : valOrUpdater;
+        const newPreviewerPrefs = { ...this.prefs, ...newPrefs };
+
+        return new SmartImagePreviewer(this.source, newPreviewerPrefs);
     }
-    public setPrefs(valOrUpdater: ValueUpdater<PreviewerPrefs> | PreviewerPrefs) {
+    public setPreviewerPrefs(valOrUpdater: ValueUpdater<PreviewerPrefs> | PreviewerPrefs) {
         const newPrefs =
             typeof valOrUpdater === 'function' ? valOrUpdater(this.prefs) : valOrUpdater;
         return new SmartImagePreviewer(this.source, newPrefs);
+    }
+    public async createStatic() {
+        const image = await createImageBitmap(this.source);
+        return new SmartImage(image, this.prefs);
     }
 }
