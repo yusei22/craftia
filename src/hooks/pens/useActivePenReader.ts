076960ce609c8a68618c892e9350a8bbd887feb9
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-28 21:45:51 +0900

    rename:フック名変更

--- src/hooks/pens/useActivePenReader.ts
+++ src/hooks/pens/useActivePenReader.ts
@@ -1,16 +1,10 @@
 import { useCallback } from 'react';
-import { RecoilValue, useRecoilCallback } from 'recoil';
 import { Pen } from 'application/pens/Pen';
 import { penAtom } from 'dataflow/pens/penAtom';
+import { useRecoilValueSyncReader } from 'hooks/useRecoilValueSyncReader';
 
 const useGetActivePen = () => {
-    const getActivePenStateSync = useRecoilCallback<[state: RecoilValue<Pen>], Pen>(
-        ({ snapshot }) =>
-            (state) => {
-                return snapshot.getLoadable(state).contents;
-            },
-        []
-    );
+    const getActivePenStateSync = useRecoilValueSyncReader<Pen>();
     const getArtboardTransSync = useCallback(() => {
         return getActivePenStateSync(penAtom);
     }, []);
