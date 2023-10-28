094e6b644fe8717f71c17eaedb61094e71d64d9b
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-28 14:53:59 +0900

    update:ブレンドモードに関する定数を追加

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,5 @@
+import { BlendMode } from 'types';
+
 export const WHEEL_CLOSE_UP_DEGREES = 0.1;
 
 export const HISTORY_STORAGE_LENGTH = 20;
@@ -13,6 +15,77 @@ export const MAX_PEN_Stabilization = 40;
 
 export const MAX_CONVOLUTION_RADIUS = 50;
 export const MIN_CONVOLUTION_RADIUS = 0.5;
+
+export const BLEND_MODE_LABEL: {
+    [K in BlendMode]: string;
+} = {
+    'source-over': 'ノーマル',
+    'source-atop': 'source-atop',
+    'source-in': 'source-in',
+    'source-out': 'source-out',
+    lighter: 'ライター',
+    xor: '交差',
+    multiply: '乗算',
+    screen: 'スクリーン',
+    overlay: 'オーバーレイ',
+    darken: '暗くする',
+    lighten: '明るくする',
+    'color-dodge': '覆い焼き',
+    'color-burn': '焼き込み',
+    'hard-light': 'ハードライト',
+    'soft-light': 'ソフトライト',
+    difference: '差',
+    exclusion: '除外',
+    'destination-atop': 'destination-atop',
+    'destination-in': 'destination-in',
+    'destination-out': '消しゴム',
+    hue: '色相',
+    saturation: '彩度',
+    color: 'カラー',
+    luminosity: '輝度',
+} as const;
+
+export const PEN_BLEND_MODE_VALUE: ReadonlyArray<BlendMode> = [
+    'source-over',
+    'lighter',
+    'xor',
+    'multiply',
+    'screen',
+    'overlay',
+    'darken',
+    'lighten',
+    'color-dodge',
+    'color-burn',
+    'hard-light',
+    'soft-light',
+    'difference',
+    'exclusion',
+    'destination-out',
+    'hue',
+    'saturation',
+    'color',
+    'luminosity',
+];
+
+export const LAYER_BLEND_MODE_VALUE: ReadonlyArray<BlendMode> = [
+    'source-over',
+    'lighter',
+    'multiply',
+    'screen',
+    'overlay',
+    'darken',
+    'lighten',
+    'color-dodge',
+    'color-burn',
+    'hard-light',
+    'soft-light',
+    'difference',
+    'exclusion',
+    'hue',
+    'saturation',
+    'color',
+    'luminosity',
+];
 
 export const MAX_UNSHARPMASKING_THRESHOLD = 5.0;
 export const MIN_UNSHARPMASKING_THRESHOLD = 0.1;
