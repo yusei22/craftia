5ac2943d0707d18f61831aac786769c85ad36f40
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-28 21:55:13 +0900

    update:atomの型引数にPen追加

--- src/dataflow/pens/penAtom.ts
+++ src/dataflow/pens/penAtom.ts
@@ -1,9 +1,10 @@
 import { atom } from 'recoil';
 import { Vec2, Vec4 } from 'application/core/units';
+import { Pen } from 'application/pens/Pen';
 import { NormalPen } from 'application/pens/normalpen';
 import { FillSolid } from 'application/sprites/SpriteFill';
 
-export const penAtom = atom({
+export const penAtom = atom<Pen>({
     key: 'penAtom',
     default: new NormalPen(new Vec2(1, 1), {
         stabilization: 0,
