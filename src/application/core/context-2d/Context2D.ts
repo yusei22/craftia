f437b0e6734c50e99d5ae1426e44e777f2acc87b
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-28 22:07:47 +0900

    update:メソッドtoDataURL追加

--- src/application/core/context-2d/Context2D.ts
+++ src/application/core/context-2d/Context2D.ts
@@ -745,6 +745,12 @@ class Context2D {
         this.setAttr('strokeStyle', value);
         return this;
     }
+    public toDataURL(){
+        if(this.context.canvas instanceof OffscreenCanvas){
+            return ''
+        }
+        return this.context.canvas.toDataURL()
+    }
 }
 
 function createCanvasAndContext2D(
