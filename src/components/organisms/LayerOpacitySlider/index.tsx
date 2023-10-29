11f4f2e6497cb6e9b8c4c313293b77a63f84f62a
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-29 11:32:40 +0900

    add:レイヤー不透明度のスライダ

---
+++ src/components/organisms/LayerOpacitySlider/index.tsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { searchSpritesFromIDs } from 'application/sprites';
+import Slider from 'components/molecules/Slider';
+import { spriteTreeAtom } from 'dataflow';
+import { activeSpriteIdsAtom } from 'dataflow/sprites/activeSpriteIdAtom';
+import { useRecoilValueSyncReader } from 'hooks/useRecoilValueSyncReader';
+
+export const LayerOpacitySlider = () => {
+    const [opacity, setOpacity] = useState(0);
+    const getActiveSpriteIds = useRecoilValueSyncReader<string[]>();
+    const [spriteTree, setSpriteTree] = useRecoilState(spriteTreeAtom);
+    const activeSpriteIds = useRecoilValue(activeSpriteIdsAtom);
+
+    useEffect(() => {
+        let n = 1;
+        let opacity = 0;
+
+        const sprites = searchSpritesFromIDs(spriteTree, activeSpriteIds);
+
+        sprites.forEach((sprite) => {
+            opacity = (sprite.prefs.opacity + opacity) / n;
+            n++;
+        });
+
+        setOpacity(Math.round(opacity * 100));
+    }, [activeSpriteIds]);
+
+    const onPenOpacityChange = (opacity: number) => {
+        const _opacity = opacity / 100;
+        const activeIds = getActiveSpriteIds(activeSpriteIdsAtom);
+
+        setSpriteTree((sprites) =>
+            sprites.map((sprite) =>
+                activeIds.includes(sprite.prefs.id)
+                    ? sprite.setSpritePrefs((prefs) => ({
+                          ...prefs,
+                          opacity: _opacity,
+                      }))
+                    : sprite
+            )
+        );
+        setOpacity(Math.round(_opacity * 100));
+    };
+
+    return (
+        <>
+            <Slider
+                uniqueId="LayerOpacitySlider"
+                setValue={onPenOpacityChange}
+                step={1}
+                value={opacity}
+                min={0}
+                max={100}
+            ></Slider>
+        </>
+    );
+};
