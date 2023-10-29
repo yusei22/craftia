a80c22b30541c0039255ddef629f2e4f1b921e15
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-29 11:51:00 +0900

    add:新規レイヤー作成ボタン

---
+++ src/components/organisms/LayerCreateButton/index.tsx
@@ -0,0 +1,40 @@
+import { useSetRecoilState } from 'recoil';
+import { v4 as uuidv4 } from 'uuid';
+import { Context2D } from 'application/core/context-2d';
+import { Vec2 } from 'application/core/units';
+import { RasterizedImagePrefs, Rasterizedmage } from 'application/sprites';
+import Button from 'components/atoms/Button';
+import { spriteTreeAtom, useSpriteTreeSaver } from 'dataflow';
+
+export const LayerCreateButton = () => {
+    const setSpriteTree = useSetRecoilState(spriteTreeAtom);
+    const saveSpriteTree = useSpriteTreeSaver();
+
+    const onClick = () => {
+        const initalPrefs: RasterizedImagePrefs = {
+            id: uuidv4(),
+            name: '新規レイヤー',
+            anchor: new Vec2(0, 0),
+            globalLocation: new Vec2(0, 0),
+            rotation: (0 / 180) * Math.PI,
+            visible: true,
+            blendMode: 'source-over',
+            opacity: 1.0,
+            shadowBlur: 0,
+            shadowColor: '#0000',
+            shadowOffset: new Vec2(0, 0),
+        };
+        (async () => {
+            const image = await createImageBitmap(
+                new Context2D().viewport(new Vec2(1, 1)).getCanvas()
+            );
+            setSpriteTree((curVal) => [...curVal, new Rasterizedmage(image, initalPrefs)]);
+            saveSpriteTree();
+        })();
+    };
+    return (
+        <>
+            <Button onClick={onClick}>新規レイヤーを追加</Button>
+        </>
+    );
+};
