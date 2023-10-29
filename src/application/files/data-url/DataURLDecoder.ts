7a942f8462c4825c2b57fd827e35e9339f362b27
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-30 08:26:46 +0900

    rename

--- src/application/files/data-url/DataURLDecoder.ts
+++
@@ -1,7 +0,0 @@
-class DataURLDecoder {
-    async decode(dataurl: string) {
-        const blob = await fetch(dataurl).then((res) => res.blob());
-        return createImageBitmap(blob);
-    }
-}
-export { DataURLDecoder };
