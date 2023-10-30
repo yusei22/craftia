fdc6ed41a021a3fe6d692f9ea469ad85773fb76f
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-30 21:49:32 +0900

    add:レイヤー削除ボタン

---
+++ src/components/organisms/LayerDeleteButton/index.tsx
@@ -0,0 +1,31 @@
+import { useSetRecoilState } from 'recoil';
+import { Sprite, SpriteTree } from 'application/sprites';
+import Button from 'components/atoms/Button';
+import { spriteTreeAtom, useSpriteTreeSaver } from 'dataflow';
+import { activeSpriteIdsAtom } from 'dataflow/sprites/activeSpriteIdAtom';
+import { useRecoilValueSyncReader } from 'hooks/useRecoilValueSyncReader';
+
+export const LayerDeleteButton = () => {
+    const getActiveSpriteIds = useRecoilValueSyncReader<string[]>();
+    const getSpriteTree = useRecoilValueSyncReader<SpriteTree>();
+
+    const saveSpriteTree = useSpriteTreeSaver();
+    const setRecoilState = useSetRecoilState(spriteTreeAtom);
+
+    const onClick = () => {
+        const activeIds = getActiveSpriteIds(activeSpriteIdsAtom);
+        const currentSprites = getSpriteTree(spriteTreeAtom);
+
+        const newSprites: Sprite[] = [];
+
+        currentSprites.forEach((sprite) => {
+            if (!activeIds.includes(sprite.prefs.id)) {
+                newSprites.push(sprite);
+            }
+        });
+
+        setRecoilState(newSprites);
+        saveSpriteTree();
+    };
+    return <Button onClick={onClick}>レイヤーを削除</Button>;
+};
