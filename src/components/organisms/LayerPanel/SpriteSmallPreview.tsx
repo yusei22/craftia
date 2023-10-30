8d59e2da5c00a5e3ac4e1a4e0d1661ed439d62f1
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-10-30 21:52:34 +0900

    update:CanvasCOntextではなくContext2Dをステート管理する

--- src/components/organisms/LayerPanel/SpriteSmallPreview.tsx
+++ src/components/organisms/LayerPanel/SpriteSmallPreview.tsx
@@ -19,10 +19,12 @@ export const SpriteSmallPreview = <T extends SpritePrefs>({
     height,
     sprite,
 }: SpriteSmallPreviewProps<T>) => {
-    const [context, setContext] = useState<CanvasRenderingContext2D | null>(null);
-    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [context2D, setContext2D] = useState<Context2D | null>(null);
+
     const getArtobardResolution = useRecoilValueSyncReader<Vec2>();
-    const artobardResolution = useRecoilValue(artboardResolutionAtom);
+
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const artboardResolution = useRecoilValue(artboardResolutionAtom);
 
     useEffect(() => {
         if (!canvasRef.current) {
@@ -30,32 +32,36 @@ export const SpriteSmallPreview = <T extends SpritePrefs>({
             return;
         }
         const canvas = canvasRef.current;
-        setContext(canvas.getContext('2d'));
+        setContext2D(
+            new Context2D({ context: canvas.getContext('2d') as CanvasRenderingContext2D })
+        );
     }, []);
 
     useEffect(() => {
-        if (context === null) {
+        if (context2D === null) {
             return;
         }
         const artboardResolution = getArtobardResolution(artboardResolutionAtom);
 
         const zoom = Math.max(width / artboardResolution.x, height / artboardResolution.y);
 
-        context.canvas.width = artboardResolution.x * zoom;
-        context.canvas.height = artboardResolution.y * zoom;
-
         const _sprite = sprite.setSpritePrefs((curVal) => ({
             ...curVal,
             visible: true,
+            blendMode: 'source-over',
+            opacity: 1.0,
         }));
-        _sprite.drawZoom(new Context2D({ context }), zoom);
-    }, [context, sprite]);
+
+        context2D.clear();
+        _sprite.drawZoom(context2D, zoom);
+    }, [context2D, sprite]);
+
     return (
         <>
             <canvas
                 ref={canvasRef}
-                width={artobardResolution.x * getZoom(width, height, artobardResolution)}
-                height={artobardResolution.y * getZoom(width, height, artobardResolution)}
+                width={artboardResolution.x * getZoom(width, height, artboardResolution)}
+                height={artboardResolution.y * getZoom(width, height, artboardResolution)}
             />
         </>
     );
