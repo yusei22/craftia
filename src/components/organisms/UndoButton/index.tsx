29f7735026be98601a6db368267e25ae4b508b60
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-18 19:50:55 +0900

    update:#6 ボタンのデザイン変更

--- src/components/organisms/UndoButton/index.tsx
+++ src/components/organisms/UndoButton/index.tsx
@@ -2,5 +2,9 @@ import Button from 'components/atoms/Button';
 import { useSpriteTreeUndo } from 'dataflow';
 export const UndoButton = () => {
     const undo = useSpriteTreeUndo();
-    return <Button onClick={undo}>元に戻す</Button>;
+    return (
+        <Button onClick={undo} variant="translucent">
+            元に戻す
+        </Button>
+    );
 };
