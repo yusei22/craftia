d8a97734cd84c4b67ca8d9a13673b6493c246260
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-18 16:43:30 +0900

    add:フレームバッファのバインド、アップデートなどを管理するクラス

---
+++ src/application/core/web-gl2/frameBuffer/FrameBufferManager.ts
@@ -0,0 +1,31 @@
+import { Renderer } from '../Renderer';
+import { FrameBuffer } from './FrameBuffer';
+import { Vec2 } from 'application/core/units';
+
+export class FrameBufferManager {
+    private renderer: Renderer;
+    private gl2: WebGL2RenderingContext;
+
+    constructor(renderer: Renderer) {
+        this.renderer = renderer;
+        this.gl2 = this.renderer.gl2;
+    }
+    public update(frameBuffer: FrameBuffer) {
+        frameBuffer.update(this.renderer.gl2);
+        this.setViewport(new Vec2(0, 0), frameBuffer.size);
+    }
+    public bind(frameBuffer: FrameBuffer) {
+        frameBuffer.bind(this.renderer.gl2);
+    }
+    public unbind() {
+        this.gl2.bindFramebuffer(this.gl2.FRAMEBUFFER, null);
+    }
+    public setViewport(pos: Vec2, size: Vec2): void {
+        this.gl2.viewport(
+            Math.round(pos.x),
+            Math.round(pos.y),
+            Math.round(size.x),
+            Math.round(size.y)
+        );
+    }
+}
