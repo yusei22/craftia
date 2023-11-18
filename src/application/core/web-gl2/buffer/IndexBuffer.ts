b6711e165c5c2ef018ebe379d34ee4ffea8f2653
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-18 16:37:55 +0900

    add:add:インデックスバッファのデータをラップするクラス

--- src/application/core/web-gl2/buffer/IndexBuffer.ts
+++ src/application/core/web-gl2/buffer/IndexBuffer.ts
@@ -1,40 +1,11 @@
-import { Buffer, BufferParam } from './Buffer';
+import { AbstractBuffer } from './AbstractBuffer';
+import { GLBuffer } from './GLBuffer';
 
 /**gzIboに渡せる`ArrayBuffer` */
 type IndexBufferData = Int8Array | Int16Array | Int32Array;
-/**
- * WegGLのインデックスバッファを管理するクラス。
- * インデックスの情報を格納したWebGLBufferを保持。
- */
-class IndexBuffer extends Buffer {
-    private _length: number;
-    /**
-     * インデックスデータの要素素を得る
-     */
-    public get length() {
-        return this._length;
-    }
 
-    /**
-     * @param gl WebGL2のコンストラクター
-     * @param bufferData データストアへ格納する型付き配列
-     * @param param3
-     * @param param3.usage データストアの用途を指定する GLenum
-     */
-    constructor(gl: WebGL2RenderingContext, param: BufferParam = {}) {
-        super(gl, gl.ELEMENT_ARRAY_BUFFER, param);
-        this._length = 0;
-    }
-
-    /**
-     * データをセットする
-     * @param bufferData セットするデータ
-     */
-    public setData(bufferData: IndexBufferData): this {
-        super.setData(bufferData);
-        this._length = bufferData.length;
-        return this;
+export class IndexBuffer extends AbstractBuffer<GLBuffer, IndexBufferData> {
+    protected generateGLBuffer(gl2: WebGL2RenderingContext) {
+        return (this.glBuffer = new GLBuffer(gl2, gl2.ELEMENT_ARRAY_BUFFER));
     }
 }
-export { IndexBuffer };
-export type { IndexBufferData };
