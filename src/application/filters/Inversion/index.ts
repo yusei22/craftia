0d18e4ab260ef69eba462f509521f8af1ee2b1ab
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-18 20:13:04 +0900

    update:テクスチャをレンダラに管理させる

--- src/application/filters/Inversion/index.ts
+++ src/application/filters/Inversion/index.ts
@@ -1,7 +1,7 @@
 import { FilterTarget, FilterWorker, GLFilter } from '../Filter';
 import { TexRenderer } from '../TexRenderer';
 import { Vec2 } from 'application/core/units';
-import { Texture, UniformInt } from 'application/core/web-gl2';
+import { UniformInt } from 'application/core/web-gl2';
 
 const inversion = require('./inversion.frag');// eslint-disable-line
 const TEX_UNITNUMBER = 0;
@@ -15,31 +15,27 @@ export class Inversion extends GLFilter<InversionConfig> {
 }
 
 export class InversionWorker extends FilterWorker<InversionConfig> {
-    private rectRenderer: TexRenderer;
-    private texture: Texture;
+    private renderer: TexRenderer;
 
     constructor(gl: WebGL2RenderingContext, sprite: FilterTarget) {
         super(sprite);
 
         const imageSize = new Vec2(sprite.image.width, sprite.image.height);
 
-        this.rectRenderer = new TexRenderer(gl);
-        this.rectRenderer.setFragmentShader(inversion.default, [
+        this.renderer = new TexRenderer(gl);
+        this.renderer.setFragmentShader(inversion.default, [
             new UniformInt('u_texture', TEX_UNITNUMBER),
         ]);
-        this.rectRenderer.setTexVertex(new Vec2(0, 0), imageSize);
-
-        this.texture = new Texture().setPixcels(sprite.image, imageSize);
-        this.rectRenderer.renderer.texture.update(this.texture);
+        this.renderer.setTexVertex(new Vec2(0, 0), imageSize);
+        this.renderer.setTexPixels(sprite.image);
     }
     public getResult() {
-        return this.rectRenderer.renderer.getCanvas();
+        return this.renderer.renderer.getCanvas();
     }
     public execute() {
-        this.rectRenderer.activate();
-
-        this.rectRenderer.renderer.texture.bind(this.texture, TEX_UNITNUMBER);
-        this.rectRenderer.draw({ flipY: true });
-        this.rectRenderer.deactivate();
+        this.renderer.setTexUnitnumber(TEX_UNITNUMBER);
+        this.renderer.activate();
+        this.renderer.draw({ flipY: true });
+        this.renderer.deactivate();
     }
 }
