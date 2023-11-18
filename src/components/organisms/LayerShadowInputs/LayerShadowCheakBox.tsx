7a486530c5030898400eb50bb6d2b1f14ddc1312
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-18 19:50:38 +0900

    add:レイヤーの影の有無をセットするチェックボックスコンポーネント

---
+++ src/components/organisms/LayerShadowInputs/LayerShadowCheakBox.tsx
@@ -0,0 +1,50 @@
+import { useRecoilValue } from 'recoil';
+import { Vec2 } from 'application/core/units';
+import { SpriteTree, searchSpritesFromIDs } from 'application/sprites';
+import Checkbox from 'components/molecules/Checkbox';
+import { spriteTreeAtom, useSpriteTreeSaver } from 'dataflow';
+import { activeSpriteIdsAtom } from 'dataflow/sprites/activeSpriteIdAtom';
+import { useSpritesSetterIds } from 'hooks/sprites/useSritePrefsSetter';
+import { useRecoilValueSyncReader } from 'hooks/useRecoilValueSyncReader';
+
+export const LayerShadowCheakBox = () => {
+    const activeIds = useRecoilValue(activeSpriteIdsAtom);
+    const spriteTree = useRecoilValue(spriteTreeAtom);
+    const getActiveSpriteIds = useRecoilValueSyncReader<string[]>();
+    const saveSpriteTree = useSpriteTreeSaver();
+    const setSpritesFromIds = useSpritesSetterIds();
+
+    const getShadowCheaked = (spriteTree: SpriteTree, activeIds: string[]) => {
+        let cheaked: boolean = false;
+
+        searchSpritesFromIDs(spriteTree, activeIds).forEach((sprite, i) => {
+            if (sprite.prefs.shadowBlur && sprite.prefs.shadowBlur > 0) {
+                cheaked = i === 0 ? true : cheaked;
+            }
+        });
+        return cheaked;
+    };
+
+    const onChange = (val: boolean) => {
+        const activeIds = getActiveSpriteIds(activeSpriteIdsAtom);
+        setSpritesFromIds(activeIds, (sprite) =>
+            sprite.setSpritePrefs((curPrefs) => ({
+                ...curPrefs,
+                shadowBlur: val ? 5 : 0,
+                shadowColor: '#000',
+                shadowOffset: new Vec2(0, 0),
+            }))
+        );
+        saveSpriteTree();
+    };
+
+    return (
+        <Checkbox
+            size="sm"
+            uniqueId="LayerShadowCheakBox"
+            label="シャドウ"
+            checked={getShadowCheaked(spriteTree, activeIds)}
+            setChecked={onChange}
+        />
+    );
+};
