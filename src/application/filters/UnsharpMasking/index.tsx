dcd60572c9906f790ed716ee44758a6e9d979f1f
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-18 19:28:53 +0900

    update:レンダラの仕様変更に合わせる

--- src/application/filters/UnsharpMasking/index.tsx
+++ src/application/filters/UnsharpMasking/index.tsx
@@ -1,44 +1,54 @@
-import { Filter, FilterTarget, FilterWorker } from '../Filter';
-import { EditorShader, ImageEditor } from '../ImageEditor';
+import { FilterTarget, FilterWorker, GLFilter } from '../Filter';
+import { TexRenderer } from '../TexRenderer';
 import { Vec2 } from 'application/core/units';
+import { UniformFloat, UniformGroup, UniformInt } from 'application/core/web-gl2';
 
 const unsharpMasking = require('./unsharpMasking.frag');// eslint-disable-line
+const TEX_UNITNUMBER = 0;
 
 export interface UnsharpMaskingConfig {
     radius: number;
     threshold: number;
 }
 
-export class UnsharpMasking extends Filter<UnsharpMaskingConfig> {
-    public getWorker(sprite: FilterTarget): UnsharpMaskingWorker {
-        return new UnsharpMaskingWorker(sprite);
+export class UnsharpMasking extends GLFilter<UnsharpMaskingConfig> {
+    public getWorker(gl: WebGL2RenderingContext, sprite: FilterTarget): UnsharpMaskingWorker {
+        return new UnsharpMaskingWorker(gl, sprite);
     }
 }
 
 export class UnsharpMaskingWorker extends FilterWorker<UnsharpMaskingConfig> {
-    private editor: ImageEditor;
+    private renderer: TexRenderer;
 
-    constructor(sprite: FilterTarget) {
+    constructor(gl: WebGL2RenderingContext, sprite: FilterTarget) {
         super(sprite);
-        const imageSize = new Vec2(sprite.image.width, sprite.image.height);
-        const shader: EditorShader = {
-            fragmentShaderSource: unsharpMasking.default,
-        };
 
-        this.editor = new ImageEditor(imageSize, shader);
-        this.editor.setImage(sprite.image, imageSize, false);
+        const imageSize = new Vec2(sprite.image.width, sprite.image.height);
+
+        this.renderer = new TexRenderer(gl);
+        this.renderer.setFragmentShader(unsharpMasking.default, [
+            new UniformInt('u_texture', TEX_UNITNUMBER),
+        ]);
+        this.renderer.setTexVertex(new Vec2(0, 0), imageSize);
+        this.renderer.setTexPixels(sprite.image);
     }
     public getResult() {
-        return this.editor.getResult();
+        return this.renderer.renderer.getCanvas();
     }
     public execute(config: UnsharpMaskingConfig) {
         const { threshold, radius } = config;
 
-        this.editor.listeners[0] = ({ setUniformFloat, setUniformInt }) => {
-            setUniformFloat('u_threshold', threshold);
-            setUniformInt('u_radius', radius);
-        };
+        this.renderer.activate();
 
-        this.editor.execute(1);
+        this.renderer.setTexUnitnumber(TEX_UNITNUMBER);
+        this.renderer.renderer.uniforms.transfer(
+            new UniformGroup([
+                new UniformFloat('u_threshold', threshold),
+                new UniformInt('u_radius', radius),
+            ])
+        );
+
+        this.renderer.draw({ flipY: true });
+        this.renderer.deactivate();
     }
 }
