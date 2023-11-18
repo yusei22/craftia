dafb847031e2e6513057a8d63f936dd7914ec0b9
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-18 19:24:45 +0900

    update:レンダラの仕様変更に合わせる

--- src/application/filters/Bilateral/index.ts
+++ src/application/filters/Bilateral/index.ts
@@ -1,42 +1,50 @@
-import { Filter, FilterTarget, FilterWorker } from '../Filter';
-import { EditorShader, ImageEditor } from '../ImageEditor';
+import { FilterTarget, FilterWorker, GLFilter } from '../Filter';
+import { TexRenderer } from '../TexRenderer';
 import { Vec2 } from 'application/core/units';
+import { FrameBuffer, UniformFloat, UniformGroup, UniformInt } from 'application/core/web-gl2';
 
 const bilateral = require('./bilateral.frag');// eslint-disable-line
+
 const MAX_RADIUS = 10;
+const TEX_UNITNUMBER = 0;
 
 export interface BilateralConfig {
     radius: number;
 }
 
-export class Bilateral extends Filter<BilateralConfig> {
-    public getWorker(sprite: FilterTarget): FilterWorker<BilateralConfig> {
-        return new BilateralWorker(sprite);
+export class Bilateral extends GLFilter {
+    public getWorker(gl: WebGL2RenderingContext, sprite: FilterTarget) {
+        return new BilateralWorker(sprite, gl);
     }
 }
-
+const sigmaSpace = (r: number) => {
+    return r / 1.5;
+};
 export class BilateralWorker extends FilterWorker<BilateralConfig> {
-    private editor: ImageEditor;
+    private renderer: TexRenderer;
+    private frameBuffers: [FrameBuffer, FrameBuffer];
 
-    constructor(sprite: FilterTarget) {
+    constructor(sprite: FilterTarget, gl2: WebGL2RenderingContext) {
         super(sprite);
-        const imageSize = new Vec2(sprite.image.width, sprite.image.height);
-        const shader: EditorShader = {
-            fragmentShaderSource: bilateral.default,
-            uniforms: [
-                {
-                    name: 'u_radius',
-                    type: 'int',
-                    value: MAX_RADIUS,
-                },
-            ],
-        };
 
-        this.editor = new ImageEditor(imageSize, shader);
-        this.editor.setImage(sprite.image, imageSize, false);
+        const imageSize = new Vec2(sprite.image.width, sprite.image.height);
+
+        this.renderer = new TexRenderer(gl2);
+        this.renderer.setFragmentShader(bilateral.default, [
+            new UniformInt('u_texture', TEX_UNITNUMBER),
+        ]);
+        this.renderer.setTexVertex(new Vec2(0, 0), imageSize);
+        this.renderer.setTexPixels(sprite.image);
+
+        this.frameBuffers = [
+            new FrameBuffer().setSize(imageSize),
+            new FrameBuffer().setSize(imageSize),
+        ];
+        this.renderer.renderer.frameBuffer.update(this.frameBuffers[0]);
+        this.renderer.renderer.frameBuffer.update(this.frameBuffers[1]);
     }
     public getResult() {
-        return this.editor.getResult();
+        return this.renderer.renderer.getCanvas();
     }
     public execute(config: BilateralConfig) {
         const { radius } = config;
@@ -44,12 +52,46 @@ export class BilateralWorker extends FilterWorker<BilateralConfig> {
         const executionTimes = Math.ceil(radius / MAX_RADIUS);
         const finalRadius = radius % MAX_RADIUS;
 
-        if (finalRadius > 0) {
-            this.editor.listeners[executionTimes - 1] = ({ setUniformInt }) => {
-                setUniformInt('u_radius', finalRadius);
-            };
+        const {
+            texture: textureManager,
+            uniforms: uniformManager,
+            frameBuffer: fboManager,
+        } = this.renderer.renderer;
+
+        this.renderer.setTexUnitnumber(TEX_UNITNUMBER);
+        this.renderer.activate();
+
+        uniformManager.transfer(
+            new UniformGroup([
+                new UniformInt('u_radius', MAX_RADIUS),
+                new UniformFloat('u_sigmaColor', 0.12),
+                new UniformFloat('u_sigmaSpace', sigmaSpace(MAX_RADIUS)),
+            ])
+        );
+
+        for (let i = 0; i < executionTimes; i++) {
+            fboManager.bind(this.frameBuffers[i % 2]);
+            this.renderer.draw({ flipY: false });
+
+            textureManager.bind(this.frameBuffers[i % 2].clolorTexture, TEX_UNITNUMBER);
         }
 
-        this.editor.execute(executionTimes);
+        uniformManager.transfer(
+            new UniformGroup([
+                new UniformInt('u_radius', finalRadius),
+                new UniformFloat('sigmaSpace', sigmaSpace(finalRadius)),
+            ])
+        );
+
+        fboManager.unbind();
+
+        this.renderer.draw({ flipY: true });
+        this.renderer.deactivate();
+    }
+    public destroy() {
+        this.frameBuffers[0].destroy();
+        this.frameBuffers[1].destroy();
+
+        this.renderer.destroy();
     }
 }
