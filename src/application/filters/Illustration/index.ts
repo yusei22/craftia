5f07f37e287075c61f8ebcd3df841660f3cb4ae3
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-18 19:25:35 +0900

    update:レンダラの仕様変更に合わせる

--- src/application/filters/Illustration/index.ts
+++ src/application/filters/Illustration/index.ts
@@ -1,29 +1,29 @@
-import { Bilateral, BilateralConfig } from '../Bilateral';
-import { Filter, FilterTarget, FilterWorker } from '../Filter';
-import { Sobel, SobelConfig } from '../Sobel';
-import { Context2D } from 'application/core/context-2d';
+import { Bilateral, BilateralWorker } from '../Bilateral';
+import { GLFilter, FilterTarget, FilterWorker } from '../Filter';
+import { Sobel, SobelWorker } from '../Sobel';
+import { AbstractContext2D, Context2D } from 'application/core/context-2d';
 import { Vec2 } from 'application/core/units';
 
 export interface IllustrationConfig {}
 
-export class Illustration extends Filter<IllustrationConfig> {
-    public getWorker(sprite: FilterTarget): IllustrationWorker {
-        return new IllustrationWorker(sprite);
+export class Illustration extends GLFilter<IllustrationConfig> {
+    public getWorker(gl: WebGL2RenderingContext, sprite: FilterTarget): IllustrationWorker {
+        return new IllustrationWorker(gl, sprite);
     }
 }
 
 export class IllustrationWorker extends FilterWorker<IllustrationConfig> {
-    private bilateral: FilterWorker<BilateralConfig>;
-    private sobel: FilterWorker<SobelConfig>;
-    private context: Context2D;
+    private bilateral: BilateralWorker;
+    private sobel: SobelWorker;
+    private context: AbstractContext2D;
 
-    constructor(sprite: FilterTarget) {
+    constructor(gl: WebGL2RenderingContext, sprite: FilterTarget) {
         super(sprite);
         const imageSize = new Vec2(sprite.image.width, sprite.image.height);
         this.context = new Context2D();
         this.context.viewport(imageSize);
-        this.bilateral = new Bilateral().getWorker(sprite);
-        this.sobel = new Sobel().getWorker(sprite);
+        this.bilateral = new Bilateral().getWorker(gl, sprite);
+        this.sobel = new Sobel().getWorker(gl, sprite);
     }
     public getResult() {
         return this.context.getCanvas();
@@ -31,10 +31,10 @@ export class IllustrationWorker extends FilterWorker<IllustrationConfig> {
     public execute() {
         this.context.clear();
         this.bilateral.execute({
-            radius: 100,
+            radius: 50,
         });
-        this.sobel.execute({});
         this.context.drawImage(this.bilateral.getResult(), new Vec2(0, 0));
+        this.sobel.execute();
         this.context.drawImage(this.sobel.getResult(), new Vec2(0, 0));
     }
 }
