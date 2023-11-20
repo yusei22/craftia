f5aa6843413abf7f1c338cf18dd76c330ba546f3
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-20 21:21:12 +0900

    update:Sapeクラスのキャッシュ管理とレンダリング処理に合わせる

--- src/application/sprites/shapes/Rect.ts
+++ src/application/sprites/shapes/Rect.ts
@@ -1,81 +1,75 @@
+import { Cache } from '../Cache';
 import { Shape, ShapePrefs } from '../Shape';
-import { SpriteConfig, SpritePrefs } from '../Sprite';
+import { SpritePrefs } from '../Sprite';
 import { AbstractContext2D } from 'application/core/context-2d';
 import { AtLeastArray, ValueUpdater } from 'application/core/types';
-import { Vec4 } from 'application/core/units';
+import { Vec2 } from 'application/core/units';
 
-interface RectPrefs extends ShapePrefs {
+export interface RectPrefs extends ShapePrefs {
     readonly round: AtLeastArray<4, number>;
 }
 
-class Rect extends Shape<RectPrefs> {
-    /**
-     * コンストラクタ
-     * @param prefs スプライトの環境設定
-     */
-    constructor(prefs: RectPrefs) {
-        const config: SpriteConfig = {
-            line: {
-                lineCap: prefs.strokeCap,
-                lineDashOffset: prefs.strokeDashOffset,
-                lineJoin: prefs.strokeJoin,
-                lineWidth: prefs.strokeWidth,
-            },
-            shadow: {
-                shadowBlur: prefs.shadowBlur,
-                shadowColor: prefs.shadowColor,
-                shadowOffset: prefs.shadowOffset,
-            },
-            text: null,
-            fillStyle: prefs.fillStyle,
-            globalAlpha: prefs.opacity,
-            globalCompositeOperation: prefs.blendMode,
-            strokeStyle: prefs.strokeStyle,
-        };
-        super(config, prefs);
+export class Rect extends Shape<RectPrefs> {
+    constructor(prefs: RectPrefs, cache: Cache<RectPrefs> | null) {
+        super(prefs, cache);
     }
     public setSpritePrefs(valOrUpdater: ValueUpdater<SpritePrefs> | SpritePrefs) {
         const newPrefs =
             typeof valOrUpdater === 'function' ? valOrUpdater(this.prefs) : valOrUpdater;
         const newRasterizedImagePrefs = { ...this.prefs, ...newPrefs };
 
-        return new Rect(newRasterizedImagePrefs);
+        return new Rect(newRasterizedImagePrefs, this.cache);
     }
     public setShapePrefs(valOrUpdater: ShapePrefs | ValueUpdater<ShapePrefs>): Shape<RectPrefs> {
         const newPrefs =
             typeof valOrUpdater === 'function' ? valOrUpdater(this.prefs) : valOrUpdater;
         const newRasterizedImagePrefs = { ...this.prefs, ...newPrefs };
 
-        return new Rect(newRasterizedImagePrefs);
+        return new Rect(newRasterizedImagePrefs, this.cache);
     }
     public setRectPrefs(valOrUpdater: ValueUpdater<RectPrefs> | RectPrefs) {
         const newPrefs =
             typeof valOrUpdater === 'function' ? valOrUpdater(this.prefs) : valOrUpdater;
 
-        return new Rect(newPrefs);
-    }
-    public drawFunc(context: AbstractContext2D): void {
-        context.translate(this.prefs.globalLocation);
-        context.rotate(this.prefs.rotation);
-        context.translate(this.prefs.globalLocation.times(-1));
-        context.roundRect(this.getStartPoint(), this.prefs.scale, this.prefs.round);
-        context.fill();
-        context.stroke();
-    }
-    public drawZoomFunc(context: AbstractContext2D, zoom: number) {
-        const _rect = this.setRectPrefs((curVal) => ({
-            ...curVal,
-            scale: curVal.scale.times(zoom),
-            globalLocation: curVal.globalLocation.times(zoom),
-            strokeWidth: curVal.strokeWidth ? curVal.strokeWidth * zoom : null,
-            round: new Vec4(curVal.round).times(zoom).toArray(),
-        }));
-        _rect.draw(context);
+        return new Rect(newPrefs, this.cache);
     }
     public createStatic() {
         return new Promise<Rect>((resolve) => {
             resolve(this);
         });
     }
+    public getMaxSize() {
+        return this.prefs.scale
+            .add(new Vec2(this.prefs.strokeWidth || 0))
+            .add(new Vec2(this.prefs.shadowBlur || 0).times(2));
+    }
+    protected rasterizeFunc(context: AbstractContext2D) {
+        const rasterizeSize = this.prefs.scale.add(new Vec2(this.prefs.strokeWidth || 0));
+
+        context.viewport(rasterizeSize);
+
+        this.setconfig(context, {
+            line: {
+                lineCap: this.prefs.strokeCap,
+                lineDashOffset: this.prefs.strokeDashOffset,
+                lineJoin: this.prefs.strokeJoin,
+                lineWidth: this.prefs.strokeWidth,
+            },
+            shadow: null,
+            text: null,
+            fillStyle: this.prefs.fillStyle,
+            globalAlpha: null,
+            globalCompositeOperation: null,
+            strokeStyle: this.prefs.strokeStyle,
+        });
+
+        const rectStartPoint = context.size.times(0.5).sub(this.prefs.scale.times(0.5));
+
+        context.roundRect(rectStartPoint, this.prefs.scale, this.prefs.round);
+        context.fill();
+        context.stroke();
+    }
+    public copy() {
+        return new Rect(this.prefs, null);
+    }
 }
-export { Rect };
