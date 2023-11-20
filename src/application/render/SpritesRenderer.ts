755ccc2ab5ea62cbb1cdf28dec410f8f239eacc7
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-20 21:09:36 +0900

    update:スプライトレンダリング時の補助用Context2Dの追加
    update:drawPointメソッド削除

--- src/application/render/SpritesRenderer.ts
+++ src/application/render/SpritesRenderer.ts
@@ -1,14 +1,16 @@
 import { Context2D } from 'application/core/context-2d';
-import { Vec2, Vec4 } from 'application/core/units';
-import { Sprite, FillSolid, Arc } from 'application/sprites';
+import { Vec2 } from 'application/core/units';
+import { Sprite } from 'application/sprites';
 
 /**
  * TODO:キャシュとかで最適化できるようにする
  */
 class SpritesRenderer {
     private context: Context2D;
+    private contextAux: Context2D;
     constructor() {
         this.context = new Context2D({ willReadFrequently: true });
+        this.contextAux = new Context2D({ willReadFrequently: true });
     }
     public viewport(size: Vec2) {
         this.context.viewport(size);
@@ -16,33 +18,8 @@ class SpritesRenderer {
     public render(sprites: Sprite[]) {
         this.context.clear();
         sprites.forEach((sprite) => {
-            sprite.draw(this.context);
+            sprite.draw(this.context, this.contextAux);
         });
-    }
-    public drawPoint(loc: Vec2) {
-        const arc = new Arc({
-            id: '000000000000',
-            name: 'arc00000',
-            anchor: new Vec2(0.5, 0.5),
-            globalLocation: loc,
-            rotation: (0 / 180) * Math.PI,
-            visible: true,
-            blendMode: 'source-over',
-            opacity: 1.0,
-            shadowBlur: 0,
-            shadowColor: '#0000',
-            shadowOffset: new Vec2(0, 0),
-            fillStyle: new FillSolid({ color: new Vec4(255, 0, 0, 1) }),
-            strokeCap: 'round',
-            strokeDashOffset: 0,
-            strokeJoin: 'bevel',
-            strokeWidth: 0,
-            strokeStyle: new FillSolid({ color: new Vec4(0, 0, 0, 0) }),
-            scale: new Vec2(10, 10),
-            startAngle: (0 * Math.PI) / 180,
-            endAngle: (360 * Math.PI) / 180,
-        });
-        arc.draw(this.context);
     }
     public getResult() {
         return this.context.getCanvas();
