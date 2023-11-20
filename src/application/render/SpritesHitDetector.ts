67ccfa56638f534e9c3b8d072778917744f5824f
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-20 21:09:21 +0900

    update:スプライトレンダリング時の補助用Context2Dの追加

--- src/application/render/SpritesHitDetector.ts
+++ src/application/render/SpritesHitDetector.ts
@@ -4,9 +4,10 @@ import { SpriteTree } from 'application/sprites';
 
 class SpritesHitDetector {
     private context: Context2D;
-
+    private contextAux: Context2D;
     constructor() {
         this.context = new Context2D({ willReadFrequently: true });
+        this.contextAux = new Context2D({ willReadFrequently: true });
     }
     public viewport(size: Vec2) {
         this.context.viewport(size);
@@ -15,7 +16,7 @@ class SpritesHitDetector {
         for (let i = sprites.length - 1; i >= 0; i--) {
             this.context.clear();
             //ポイント描画
-            sprites[i].draw(this.context);
+            sprites[i].draw(this.context, this.contextAux);
             //ポイントのImageData得る
             const imageData = this.context.getImageData(point, new Vec2(1, 1));
             if (!isTransparent(imageData)) return sprites[i];
