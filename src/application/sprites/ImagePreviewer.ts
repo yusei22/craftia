101ff0e8e0e39919c29b05d1a44ad092696ef9b7
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-20 21:14:13 +0900

    update:_drawFuncメソッド追加、スプライトの環境設定を用いて描画を行うとは限らない

--- src/application/sprites/ImagePreviewer.ts
+++ src/application/sprites/ImagePreviewer.ts
@@ -1,4 +1,4 @@
-import { Sprite, SpriteConfig, SpritePrefs } from './Sprite';
+import { Sprite, SpritePrefs } from './Sprite';
 import { AbstractContext2D } from 'application/core/context-2d';
 import { ValueUpdater } from 'application/core/types';
 import { Vec2 } from 'application/core/units';
@@ -13,7 +13,23 @@ export abstract class ImagePreviewer extends Sprite<PreviewerPrefs> {
     public readonly source: PreviewerSource;
 
     constructor(source: PreviewerSource, prefs: PreviewerPrefs) {
-        const config: SpriteConfig = {
+        super(prefs);
+        this.source = source;
+    }
+    public abstract setPreviewerPrefs(
+        valOrUpdater: ValueUpdater<PreviewerPrefs> | PreviewerPrefs
+    ): ImagePreviewer;
+
+    public getStartPoint() {
+        const anchorRerativeLoc = new Vec2(
+            this.prefs.anchor.x * this.prefs.scale.x,
+            this.prefs.anchor.y * this.prefs.scale.y
+        );
+        return this.prefs.globalLocation.sub(anchorRerativeLoc);
+    }
+
+    public _drawFunc(context: AbstractContext2D, prefs: PreviewerPrefs) {
+        this.setconfig(context, {
             line: null,
             shadow: {
                 shadowBlur: prefs.shadowBlur,
@@ -25,32 +41,22 @@ export abstract class ImagePreviewer extends Sprite<PreviewerPrefs> {
             globalAlpha: prefs.opacity,
             globalCompositeOperation: prefs.blendMode,
             strokeStyle: null,
-        };
-        super(config, prefs);
-        this.source = source;
+        });
+        context.translate(prefs.globalLocation);
+        context.rotate(prefs.rotation);
+        context.translate(prefs.globalLocation.times(-1));
+        context.drawImage(this.source, this.getStartPoint(), prefs.scale);
     }
-    public abstract setPreviewerPrefs(
-        valOrUpdater: ValueUpdater<PreviewerPrefs> | PreviewerPrefs
-    ): ImagePreviewer;
-    public getStartPoint() {
-        const anchorRerativeLoc = new Vec2(
-            this.prefs.anchor.x * this.prefs.scale.x,
-            this.prefs.anchor.y * this.prefs.scale.y
-        );
-        return this.prefs.globalLocation.sub(anchorRerativeLoc);
-    }
+
     public drawFunc(context: AbstractContext2D) {
-        context.translate(this.prefs.globalLocation);
-        context.rotate(this.prefs.rotation);
-        context.translate(this.prefs.globalLocation.times(-1));
-        context.drawImage(this.source, this.getStartPoint(), this.prefs.scale);
+        this._drawFunc(context, this.prefs);
     }
-    public drawZoomFunc(context: AbstractContext2D, zoom: number) {
-        const _previewer = this.setPreviewerPrefs((curVal) => ({
-            ...curVal,
-            scale: curVal.scale.times(zoom),
-            globalLocation: curVal.globalLocation.times(zoom),
-        }));
-        _previewer.draw(context);
+    public drawZoomFunc(context: AbstractContext2D, contextAux: AbstractContext2D, zoom: number) {
+        this._drawFunc(context, {
+            ...this.prefs,
+            scale: this.prefs.scale.times(zoom),
+            globalLocation: this.prefs.globalLocation.times(zoom),
+            shadowBlur: this.prefs.shadowBlur ? this.prefs.shadowBlur * zoom : null,
+        });
     }
 }
