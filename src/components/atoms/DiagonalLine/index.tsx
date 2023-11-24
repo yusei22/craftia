48171e4a5541b1dc72ee5e11e7b4ccb00dff0583
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-24 22:21:35 +0900

    chore:#27 `css`プロップの設定方法を修正

--- src/components/atoms/DiagonalLine/index.tsx
+++ src/components/atoms/DiagonalLine/index.tsx
@@ -1,15 +1,15 @@
 import { Interpolation, Theme } from '@emotion/react';
-import { ClassAttributes } from 'react';
+import React, { ClassAttributes } from 'react';
 import { Vec2 } from 'application/core/units';
 type DiagonalLineProps = {
     start: Vec2;
     end: Vec2;
     borderWidth?: number;
     color?: string;
-    css?: Interpolation<Theme>;
-} & ClassAttributes<HTMLInputElement>;
+} & ClassAttributes<HTMLDivElement> &
+    React.HTMLAttributes<HTMLDivElement>;
 
-const DiagonalLine = ({ start, end, css, borderWidth = 5, color, ...props }: DiagonalLineProps) => {
+const DiagonalLine = ({ start, end, borderWidth = 5, color, ...props }: DiagonalLineProps) => {
     const _start = start.sub(new Vec2(0, borderWidth / 2));
     const _end = end.sub(new Vec2(0, borderWidth / 2));
 
@@ -20,11 +20,11 @@ const DiagonalLine = ({ start, end, css, borderWidth = 5, color, ...props }: Dia
         position: 'absolute',
         width: `${width}px`,
         height: borderWidth,
-        backgroundColor: color ?? theme.colors.primaryMedium,
+        backgroundColor: color ?? theme.colors.primary400,
         transformOrigin: `0 50%`,
         transform: `translate(${_start.x}px, ${_start.y}px) rotate(${angle}deg)`,
     });
-    return <div css={[css, lineStyle]} {...props}></div>;
+    return <div css={lineStyle} {...props}></div>;
 };
 
 export default DiagonalLine;
