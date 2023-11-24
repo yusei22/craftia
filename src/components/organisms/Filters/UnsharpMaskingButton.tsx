15bbe0109ab543eeb01b159acc273a20099d8639
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-24 22:55:11 +0900

    feat:GLFilterButtonコンポーネントを使用した処理に切り替え

--- src/components/organisms/Filters/UnsharpMaskingButton.tsx
+++ src/components/organisms/Filters/UnsharpMaskingButton.tsx
@@ -1,69 +1,128 @@
-import { useState } from 'react';
-import { FilterButton } from './FilterButton';
+import { useState, useEffect } from 'react';
+import { useSetRecoilState } from 'recoil';
+import { GLFilterButton } from './GLFilterButton';
+import { FilterWorker } from 'application/filters/Filter';
+import { GaussianBlurConfig } from 'application/filters/GaussianBlur';
 import { UnsharpMasking, UnsharpMaskingConfig } from 'application/filters/UnsharpMasking';
-import Slider from 'components/molecules/Slider';
+import { Rasterizedmage, SmartImage } from 'application/sprites';
+import Box from 'components/layout/Box';
+import { ConfirmButtons } from 'components/molecules/ConfirmButtons';
+import { NumberField } from 'components/molecules/NumberField';
 import {
     MAX_CONVOLUTION_RADIUS,
     MAX_UNSHARPMASKING_THRESHOLD,
     MIN_CONVOLUTION_RADIUS,
     MIN_UNSHARPMASKING_THRESHOLD,
 } from 'consts';
+import { useSpriteTreeSaver } from 'dataflow';
+import { floatingWindowAtom } from 'dataflow/windows/floatingWindowAtom';
+import { useSpriteSetterID } from 'hooks/sprites/useSpriteSetterID';
 
 export const UnsharpMaskingButton = ({ children }: { children?: React.ReactNode }) => {
+    return (
+        <GLFilterButton
+            filter={new UnsharpMasking()}
+            label="アンシャープマスキング"
+            WindowContents={UnsharpMaskingWindowContents}
+        >
+            {children}
+        </GLFilterButton>
+    );
+};
+
+const UnsharpMaskingWindowContents = ({
+    sprite,
+    worker,
+    onClose,
+}: {
+    sprite: Rasterizedmage | SmartImage;
+    worker: FilterWorker<GaussianBlurConfig>;
+    onClose: () => unknown;
+}) => {
+    const setWindow = useSetRecoilState(floatingWindowAtom);
+    const setSpriteId = useSpriteSetterID();
+    const saveSpriteTree = useSpriteTreeSaver();
+
     const [config, setConfig] = useState<UnsharpMaskingConfig>({
         radius: 3,
         threshold: 1.5,
     });
-    const labelSize = 90;
-    const sliderSize = 200;
-    const getWindowChildren = (onChangeFunc: () => void) => (
-        <>
-            <Slider
+
+    const onConsent = () => {
+        saveSpriteTree();
+        onClose();
+    };
+
+    const onCancel = () => {
+        setSpriteId(sprite.prefs.id, (sprite) => worker?.getOriginalSprite() || sprite);
+        onClose();
+    };
+
+    useEffect(() => {
+        setWindow((culVal) => ({
+            ...culVal,
+            onClose: onCancel,
+        }));
+        worker.execute(config);
+        setSpriteId(sprite.prefs.id, (sprite) => worker.getPreviewSprite() || sprite);
+    }, []);
+
+    useEffect(() => {
+        worker.execute(config);
+        setSpriteId(sprite.prefs.id, (sprite) => worker?.getPreviewSprite() || sprite);
+    }, [config]);
+
+    return (
+        <Box
+            css={(theme) => ({
+                backgroundColor: theme.colors.white,
+                padding: '20px',
+            })}
+        >
+            <NumberField
                 value={config.radius}
-                uniqueId="unsharpMaskingPanel_radius"
+                id="unsharpMaskingPanelRadius"
                 max={MAX_CONVOLUTION_RADIUS}
                 min={MIN_CONVOLUTION_RADIUS}
                 step={1}
                 label="半径"
-                labelSize={labelSize}
-                sliderSize={sliderSize}
-                onChange={(e) => {
+                setValue={(v) => {
                     setConfig((curVal) => ({
                         ...curVal,
-                        radius: parseFloat(e.target.value),
+                        radius: v,
                     }));
-                    onChangeFunc();
+                }}
+                width={400}
+                css={{
+                    flexFlow: 'column',
                 }}
             />
-            <Slider
+            <NumberField
                 value={config.threshold}
-                uniqueId="unsharpMaskingPanel_threshold"
+                id="unsharpMaskingPanelThreshold"
                 max={MAX_UNSHARPMASKING_THRESHOLD}
                 min={MIN_UNSHARPMASKING_THRESHOLD}
                 step={0.1}
-                sliderSize={sliderSize}
                 label="閾値"
-                labelSize={labelSize}
-                onChange={(e) => {
+                setValue={(v) => {
                     setConfig((curVal) => ({
                         ...curVal,
-                        threshold: parseFloat(e.target.value),
+                        threshold: v,
                     }));
-                    onChangeFunc();
+                }}
+                width={400}
+                css={{
+                    flexFlow: 'column',
                 }}
             />
-        </>
-    );
-    return (
-        <>
-            <FilterButton<UnsharpMaskingConfig>
-                filter={new UnsharpMasking()}
-                config={config}
-                title="アンシャープマスキング"
-                getWindowChildren={getWindowChildren}
-            >
-                {children}
-            </FilterButton>
-        </>
+            <ConfirmButtons
+                css={{
+                    width: '100%',
+                    justifyContent: 'end',
+                }}
+                onCancel={onCancel}
+                onConsent={onConsent}
+            />
+        </Box>
     );
 };
