272753ebc68dab67253f5c9856a00804a169f1f1
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-24 23:32:06 +0900

    feat:レイアウト及びコンポーネントを更新

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import Head from 'next/head';
-import { LayerColumn } from 'components/ecosystem/LayerColumn';
-import { RenderingArea } from 'components/ecosystem/RenderingArea';
-import { RibbonTabs } from 'components/ecosystem/RibbonTabs.ts';
-import { SideBar } from 'components/ecosystem/SideBar';
+import { ColorSection } from 'components/ecosystem/ColorSection';
+import { FooterSection } from 'components/ecosystem/FooterSection';
+import { LayerSection } from 'components/ecosystem/LayerSection';
+import { MenuSection } from 'components/ecosystem/MenuSection';
+import { PenSection } from 'components/ecosystem/PenSection';
+import { RenderingSection } from 'components/ecosystem/RenderingSection';
+import { ToolSection } from 'components/ecosystem/ToolSection';
 import Box from 'components/layout/Box';
+import Container from 'components/layout/Container';
 import Wrapper from 'components/layout/Wrapper';
 import { Provider } from 'components/provider';
 import { MQ } from 'mediaQuery';
@@ -30,35 +34,79 @@ export default function Home() {
                             },
                         }}
                     >
-                        <RibbonTabs></RibbonTabs>
+                        <MenuSection width={'100vw'} height={30} />
                     </Wrapper>
-                    <Wrapper
+                    <Container
+                        css={(theme) => ({
+                            position: 'absolute',
+                            zIndex: 500,
+                            top: 30,
+                            height: 'calc(100% - 45px)',
+                            padding: '10px 5px',
+                            paddingTop: '0px',
+                            backgroundColor: theme.colors.neutral100,
+                        })}
+                    >
+                        <ToolSection
+                            css={(theme) => ({
+                                marginLeft: '5px',
+                                height: '100%',
+                                width: '50px',
+                                borderRadius: '5px',
+                                backgroundColor: theme.colors.neutral200,
+                            })}
+                        />
+                        <Box
+                            css={{
+                                marginLeft: '5px',
+                                height: '100%',
+                            }}
+                        >
+                            <ColorSection
+                                wheelRadius={150}
+                                css={(theme) => ({
+                                    padding: '10px',
+                                    height: '55%',
+                                    borderRadius: '5px',
+                                    width: '200px',
+                                    backgroundColor: theme.colors.neutral200,
+                                })}
+                            />
+                            <PenSection
+                                css={(theme) => ({
+                                    justifyContent: 'start',
+                                    marginTop: 10,
+                                    padding: '10px',
+                                    height: '45%',
+                                    width: '200px',
+                                    borderRadius: '5px',
+                                    backgroundColor: theme.colors.neutral200,
+                                })}
+                            />
+                        </Box>
+                    </Container>
+
+                    <RenderingSection />
+                    <LayerSection
+                        panelWidth={280}
                         css={{
                             position: 'absolute',
                             zIndex: 500,
-                            top: '200PX',
-                            left: '20px',
-                            height: 'calc(100% - 300px)',
+                            top: 30,
+                            right: 0,
+                            height: 'calc(100% - 45px)',
                         }}
-                    >
-                        <SideBar></SideBar>
-                    </Wrapper>
-                    <RenderingArea />
-                    <Box
-                        css={(theme) => ({
-                            backgroundColor: theme.colors.white,
-                            boxShadow: '0 0 35px 0 rgba(0, 0, 0, .2)',
-                            height: 'calc(100% - 500px)',
+                    />
+
+                    <FooterSection
+                        css={{
+                            width: '100vw',
+                            height: '15px',
                             position: 'absolute',
-                            zIndex: 500,
-                            bottom: '50px',
-                            right: '20px',
-                            padding: '10px',
-                            overflowY: 'scroll',
-                        })}
-                    >
-                        <LayerColumn />
-                    </Box>
+                            bottom: 0,
+                            zIndex: 1000,
+                        }}
+                    />
                 </Provider>
             </>
         </>
