91bdcb8bcfa9785209ad200fb84189bd22269c50
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-24 22:39:11 +0900

    chore:#13 cssプロップの設定方法を修正

--- src/components/layout/Wrapper/index.tsx
+++ src/components/layout/Wrapper/index.tsx
@@ -1,26 +1,19 @@
-import { Interpolation, Theme } from '@emotion/react';
 import { ClassAttributes, HTMLAttributes, forwardRef } from 'react';
 
-type WrapperProps = {
-    css?: Interpolation<Theme>;
-} & HTMLAttributes<HTMLDivElement> &
-    ClassAttributes<HTMLDivElement>;
+export type WrapperProps = HTMLAttributes<HTMLDivElement> & ClassAttributes<HTMLDivElement>;
 
-const Wrapper = forwardRef<HTMLDivElement, WrapperProps>(({ css, ...props }, ref) => {// eslint-disable-line
+const Wrapper = forwardRef<HTMLDivElement, WrapperProps>(({ ...props }, ref) => {// eslint-disable-line
     return (
         <div
             ref={ref}
-            css={[
-                {
-                    position: 'relative',
-                    width: 'fit-content',
-                    height: 'fit-content',
-                    margin: '0px',
-                    padding: '0px',
-                    fontSize: '0px',
-                },
-                css,
-            ]}
+            css={{
+                position: 'relative',
+                width: 'fit-content',
+                height: 'fit-content',
+                margin: '0px',
+                padding: '0px',
+                fontSize: '0px',
+            }}
             {...props}
         ></div>
     );
