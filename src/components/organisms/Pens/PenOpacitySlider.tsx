11ae3eac7824b9eee6e3d24c3fdd677c2d80e1a3
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-24 23:13:01 +0900

    feat:#6 コンポーネントのプロパティ追加
    chore:#18 コンポーネントのプロパティ名更新 uniqueId->id

--- src/components/organisms/Pens/PenOpacitySlider.tsx
+++ src/components/organisms/Pens/PenOpacitySlider.tsx
@@ -1,10 +1,11 @@
 import { useRecoilState } from 'recoil';
-import Slider from 'components/molecules/Slider';
+import { NumberField } from 'components/molecules/NumberField';
 import { MAX_PEN_OPACITY, MIN_PEN_OPACITY } from 'consts';
 import { penAtom } from 'dataflow/pens/penAtom';
 
-export const PenOpacitySlider = () => {
+export const PenOpacitySlider = ({ className }: { className?: string }) => {
     const [penVal, setPen] = useRecoilState(penAtom);
+
     const onPenOpacityChange = (opacity: number) => {
         setPen((pen) =>
             pen.setPrefs((prefs) => ({
@@ -14,15 +15,18 @@ export const PenOpacitySlider = () => {
         );
     };
     return (
-        <Slider
-            uniqueId="drawPanel_slider_lineOpacity"
+        <NumberField
+            className={className}
+            id="drawPanelSliderLineOpacity"
             min={Math.round(MIN_PEN_OPACITY * 100)}
             max={Math.round(MAX_PEN_OPACITY * 100)}
             setValue={onPenOpacityChange}
             value={Math.round(penVal.prefs.opacity * 100)}
             step={1}
             label="不透明度"
-            labelSize={100}
+            css={{
+                flexFlow: 'column',
+            }}
         />
     );
 };
