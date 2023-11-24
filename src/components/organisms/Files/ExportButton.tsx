a98bf05ad020ede366192a4e64c57d89bf54c54d
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-24 22:49:54 +0900

    feat:#9 コンポーネントのプロパティ追加

--- src/components/organisms/Files/ExportButton.tsx
+++ src/components/organisms/Files/ExportButton.tsx
@@ -2,27 +2,41 @@ import { useEffect, useState } from 'react';
 import { Vec2 } from 'application/core/units';
 import { SpritesRenderer } from 'application/render/SpritesRenderer';
 import { Sprite } from 'application/sprites/Sprite';
-import Button from 'components/atoms/Button';
+
 import { stageResolutionAtom, spriteTreeAtom } from 'dataflow';
 import { useRecoilValueSyncReader } from 'hooks/useRecoilValueSyncReader';
 
-export const ExportButton = () => {
+export type ExportButtonProps = {
+    children?: React.ReactNode;
+    exportFormat?: string;
+    exportName?: string;
+    className?: string;
+};
+
+export const ExportButton = ({
+    children,
+    exportFormat,
+    exportName,
+    className,
+}: ExportButtonProps) => {
     const [stageRenderer, setStageRenderer] = useState<SpritesRenderer | null>(null);
-    const getSpriteTreeSync = useRecoilValueSyncReader<Sprite<any>[]>();// eslint-disable-line
-    const getArtobardResolutionSync = useRecoilValueSyncReader<Vec2>();
     const [url, setUrl] = useState('#');
-    const [fileName, setFilename] = useState('project.png');
+
+    const getSpriteTreeSync = useRecoilValueSyncReader<Sprite[]>();
+    const getArtobardResolutionSync = useRecoilValueSyncReader<Vec2>();
 
     const onClick = () => {
         if (stageRenderer === null) {
             return;
         }
+
         const resolution = getArtobardResolutionSync(stageResolutionAtom);
         const sprites = getSpriteTreeSync(spriteTreeAtom);
+
         stageRenderer.viewport(resolution);
         stageRenderer.render(sprites);
-        setUrl((stageRenderer.getResult() as HTMLCanvasElement).toDataURL());
-        setFilename('project.png');
+
+        setUrl(stageRenderer.getResult().toDataURL(exportFormat));
     };
 
     useEffect(() => {
@@ -30,8 +44,14 @@ export const ExportButton = () => {
     }, []);
 
     return (
-        <a onClick={onClick} href={url} download={fileName}>
-            <Button variant="translucent">書き出し</Button>
+        <a
+            className={className}
+            onClick={onClick}
+            href={url}
+            download={exportName}
+            css={(theme) => ({ color: theme.colors.black })}
+        >
+            {children}
         </a>
     );
 };
