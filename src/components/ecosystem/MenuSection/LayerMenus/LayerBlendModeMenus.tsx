c6c1dae40bbed18023fdd13fc6f8d6116b727ad6
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-24 22:32:40 +0900

    add:レイヤーブレンドモードのメニューコンポーネント

---
+++ src/components/ecosystem/MenuSection/LayerMenus/LayerBlendModeMenus.tsx
@@ -0,0 +1,52 @@
+import { MenuItem } from '@szhsin/react-menu';
+import Typography from 'components/atoms/Typography';
+import '@szhsin/react-menu/dist/index.css';
+import { BLEND_MODE_LABEL, LAYER_BLEND_MODE_VALUE } from 'consts';
+import { activeSpriteIdsAtom } from 'dataflow/sprites/activeSpriteIdAtom';
+import { useSpritesSetterIds } from 'hooks/sprites/useSpritesSetterIds';
+import { useRecoilValueSyncReader } from 'hooks/useRecoilValueSyncReader';
+import { BlendMode } from 'types';
+
+type BlendModeOption = {
+    value: BlendMode;
+    label: string;
+};
+
+const options: BlendModeOption[] = LAYER_BLEND_MODE_VALUE.map((value) => ({
+    value,
+    label: BLEND_MODE_LABEL[value],
+}));
+
+export const LayerBlendModeMenus = () => {
+    const setSpriteIds = useSpritesSetterIds();
+    const getActiveSpriteIds = useRecoilValueSyncReader<string[]>();
+
+    const setBlendMode = (blendMode: BlendMode) => {
+        const activeIds = getActiveSpriteIds(activeSpriteIdsAtom);
+
+        setSpriteIds(activeIds, (sprite) =>
+            sprite.setSpritePrefs((curPrefs) => ({
+                ...curPrefs,
+                blendMode: blendMode,
+            }))
+        );
+    };
+
+    return (
+        <>
+            {options.map(({ value, label }) => (
+                <MenuItem
+                    onClick={() => {
+                        setBlendMode(value);
+                    }}
+                    key={value}
+                    css={{
+                        width: '100%',
+                    }}
+                >
+                    <Typography variant="small">{label}</Typography>
+                </MenuItem>
+            ))}
+        </>
+    );
+};
