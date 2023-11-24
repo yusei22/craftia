ecb05709d3fe288ce28e98b560c55d1a1fab8e08
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-24 22:54:51 +0900

    feat:GLFilterButtonコンポーネントを使用した処理に切り替え

--- src/components/organisms/Filters/BilateralButton.tsx
+++ src/components/organisms/Filters/BilateralButton.tsx
@@ -1,46 +1,103 @@
-import { useState } from 'react';
-import { FilterButton } from './FilterButton';
+import { useEffect, useState } from 'react';
+import { useSetRecoilState } from 'recoil';
+import { GLFilterButton } from './GLFilterButton';
 import { Bilateral, BilateralConfig } from 'application/filters/Bilateral';
-import Slider from 'components/molecules/Slider';
+import { FilterWorker } from 'application/filters/Filter';
+import { Rasterizedmage, SmartImage } from 'application/sprites';
+import Box from 'components/layout/Box';
+import { ConfirmButtons } from 'components/molecules/ConfirmButtons';
+import { NumberField } from 'components/molecules/NumberField';
 import { MAX_CONVOLUTION_RADIUS, MIN_CONVOLUTION_RADIUS } from 'consts';
+import { useSpriteTreeSaver } from 'dataflow';
+import { floatingWindowAtom } from 'dataflow/windows/floatingWindowAtom';
+import { useSpriteSetterID } from 'hooks/sprites/useSpriteSetterID';
 
 export const BilateralButton = ({ children }: { children?: React.ReactNode }) => {
+    return (
+        <GLFilterButton
+            filter={new Bilateral()}
+            label="バイラテラルフィルタ"
+            WindowContents={BilateralWindowContents}
+        >
+            {children}
+        </GLFilterButton>
+    );
+};
+
+const BilateralWindowContents = ({
+    sprite,
+    worker,
+    onClose,
+}: {
+    sprite: Rasterizedmage | SmartImage;
+    worker: FilterWorker<BilateralConfig>;
+    onClose: () => unknown;
+}) => {
+    const setWindow = useSetRecoilState(floatingWindowAtom);
+    const setSpriteId = useSpriteSetterID();
+    const saveSpriteTree = useSpriteTreeSaver();
+
     const [config, setConfig] = useState<BilateralConfig>({
         radius: 3,
     });
-    const labelSize = 90;
-    const sliderSize = 200;
-    const getWindowChildren = (onChangeFunc: () => void) => (
-        <>
-            <Slider
+
+    const onConsent = () => {
+        saveSpriteTree();
+        onClose();
+    };
+
+    const onCancel = () => {
+        setSpriteId(sprite.prefs.id, (sprite) => worker?.getOriginalSprite() || sprite);
+        onClose();
+    };
+
+    useEffect(() => {
+        setWindow((culVal) => ({
+            ...culVal,
+            onClose: onCancel,
+        }));
+        worker.execute(config);
+        setSpriteId(sprite.prefs.id, (sprite) => worker.getPreviewSprite() || sprite);
+    }, []);
+
+    useEffect(() => {
+        worker.execute(config);
+        setSpriteId(sprite.prefs.id, (sprite) => worker?.getPreviewSprite() || sprite);
+    }, [config]);
+
+    return (
+        <Box
+            css={(theme) => ({
+                backgroundColor: theme.colors.white,
+                padding: '20px',
+            })}
+        >
+            <NumberField
                 value={config.radius}
-                uniqueId="bilateralButton_radius"
+                id="bilateralButton_radius"
                 max={MAX_CONVOLUTION_RADIUS}
                 min={MIN_CONVOLUTION_RADIUS}
                 step={1}
                 label="半径"
-                labelSize={labelSize}
-                sliderSize={sliderSize}
-                onChange={(e) => {
+                setValue={(v) => {
                     setConfig((curVal) => ({
                         ...curVal,
-                        radius: parseFloat(e.target.value),
+                        radius: v,
                     }));
-                    onChangeFunc();
+                }}
+                width={400}
+                css={{
+                    flexFlow: 'column',
                 }}
             />
-        </>
-    );
-    return (
-        <>
-            <FilterButton<BilateralConfig>
-                filter={new Bilateral()}
-                config={config}
-                title="バイラテラルフィルタ(ノイズ除去)"
-                getWindowChildren={getWindowChildren}
-            >
-                {children}
-            </FilterButton>
-        </>
+            <ConfirmButtons
+                css={{
+                    width: '100%',
+                    justifyContent: 'end',
+                }}
+                onCancel={onCancel}
+                onConsent={onConsent}
+            />
+        </Box>
     );
 };
