8457a8ef32db7d9339505400ddcf67d9a8e584c3
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-24 22:22:22 +0900

    chore:#13 `css`プロップの設定方法を修正

--- src/components/atoms/Label/index.tsx
+++ src/components/atoms/Label/index.tsx
@@ -1,22 +1,18 @@
-import { Interpolation, Theme } from '@emotion/react';
+import { Theme } from '@emotion/react';
 import { ClassAttributes, LabelHTMLAttributes } from 'react';
 
 type LabelProps = {
     size?: keyof Theme['fontSize'];
-    css?: Interpolation<Theme>;
 } & LabelHTMLAttributes<HTMLLabelElement> &
     ClassAttributes<HTMLLabelElement>;
 
-const Label = ({ size = 'md', css, ...props }: LabelProps) => {
+const Label = ({ size = 'md', ...props }: LabelProps) => {
     return (
         <label
-            css={[
-                (theme) => ({
-                    fontSize: theme.fontSize[size],
-                    accentColor: theme.colors.primaryDark,
-                }),
-                css,
-            ]}
+            css={(theme) => ({
+                fontSize: theme.fontSize[size],
+                accentColor: theme.colors.primary600,
+            })}
             {...props}
         />
     );
