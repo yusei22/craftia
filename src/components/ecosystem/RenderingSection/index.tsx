e78004c951afcc165d93e503576d17b3c3d0e579
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-24 22:35:42 +0900

    move:フォルダ名変更 RenderingArea->RenderingSection

--- src/components/ecosystem/RenderingSection/index.tsx
+++ src/components/ecosystem/RenderingSection/index.tsx
@@ -1,9 +1,9 @@
 import { useRecoilValue } from 'recoil';
-import { RenderView } from 'components/ecosystem/RenderingArea/RenderView';
-import { RenderViewWrapper } from 'components/ecosystem/RenderingArea/RenderViewWrapper';
+import { RenderView } from 'components/ecosystem/RenderingSection/RenderView';
+import { RenderViewWrapper } from 'components/ecosystem/RenderingSection/RenderViewWrapper';
 import { renderViewListenersAtom } from 'dataflow';
 
-const RenderingArea = () => {
+export const RenderingSection = () => {
     const RenderViewEventFuncs = useRecoilValue(renderViewListenersAtom);
 
     return (
@@ -14,4 +14,3 @@ const RenderingArea = () => {
         </>
     );
 };
-export { RenderingArea };
