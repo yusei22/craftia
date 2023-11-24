94dfda6dc238164ce9bf0f6e9196c1bf85feb16e
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-24 22:45:00 +0900

    rename:コンポーネント名変更 Slider->NumberField

--- src/components/molecules/NumberField/index.stories.tsx
+++ src/components/molecules/NumberField/index.stories.tsx
@@ -1,27 +1,27 @@
 import { useState } from '@storybook/addons';
 import type { Meta, StoryObj } from '@storybook/react';
-import Slider from '.';
+import { NumberField } from '.';
 
 const meta = {
     title: 'Molecules/Slider',
-    component: Slider,
+    component: NumberField,
     tags: ['autodocs'],
-} satisfies Meta<typeof Slider>;
+} satisfies Meta<typeof NumberField>;
 
 export default meta;
-type Story = StoryObj<typeof Slider>;
+type Story = StoryObj<typeof NumberField>;
 
 export const Primary: Story = {
     render: () => {
         const [value, setValue] = useState(50);// eslint-disable-line
         return (
             <>
-                <Slider
-                    uniqueId="sampleSlider"
+                <NumberField
+                    id="sampleSlider"
                     value={value}
                     setValue={setValue}
                     label="Label"
-                ></Slider>
+                ></NumberField>
             </>
         );
     },
