3cb79d49d7d7611905290458c9b08a6ca0d7aed0
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-24 22:49:10 +0900

    feat:#4 コンポーネントのプロパティ追加

--- src/components/organisms/Edits/UndoButton.tsx
+++ src/components/organisms/Edits/UndoButton.tsx
@@ -1,10 +1,16 @@
-import Button from 'components/atoms/Button';
+import Wrapper from 'components/layout/Wrapper';
 import { useSpriteTreeUndo } from 'dataflow';
-export const UndoButton = () => {
+
+export type UndoButtonProps = {
+    children?: React.ReactNode;
+    className?: string;
+};
+
+export const UndoButton = ({ children, className }: UndoButtonProps) => {
     const undo = useSpriteTreeUndo();
     return (
-        <Button onClick={undo} variant="translucent">
-            元に戻す
-        </Button>
+        <Wrapper onClick={undo} className={className}>
+            {children}
+        </Wrapper>
     );
 };
