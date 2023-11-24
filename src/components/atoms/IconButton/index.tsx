b0360640e6553ace6f1bbf77e5ca96ecb6aa67f8
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-24 22:21:51 +0900

    chore#6 `css`プロップの設定方法を修正

--- src/components/atoms/IconButton/index.tsx
+++ src/components/atoms/IconButton/index.tsx
@@ -1,16 +1,13 @@
 import Button, { ButtonProps } from '../Button';
 
-const IconButton = ({ css, ...props }: ButtonProps) => {
+const IconButton = ({ ...props }: ButtonProps) => {
     return (
         <Button
-            css={[
-                css,
-                (theme) => ({
-                    padding: theme.space.sm,
-                    fontSize: '0px',
-                    borderRadius: '50%',
-                }),
-            ]}
+            css={(theme) => ({
+                padding: theme.space.sm,
+                fontSize: '0px',
+                borderRadius: '50%',
+            })}
             {...props}
         />
     );
