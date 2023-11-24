8311dd1134c1f5752045f9fb57e6e39c60637d18
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-24 22:48:59 +0900

    feat:#4 コンポーネントのプロパティ追加

--- src/components/organisms/Edits/RenoButton.tsx
+++ src/components/organisms/Edits/RenoButton.tsx
@@ -1,11 +1,16 @@
-import Button from 'components/atoms/Button';
+import Wrapper from 'components/layout/Wrapper';
 import { useSpriteTreeReno } from 'dataflow';
 
-export const RenoButton = () => {
+export type RenoButtonProps = {
+    children?: React.ReactNode;
+    className?: string;
+};
+
+export const RenoButton = ({ children, className }: RenoButtonProps) => {
     const reno = useSpriteTreeReno();
     return (
-        <Button onClick={reno} variant="translucent">
-            やり直し
-        </Button>
+        <Wrapper onClick={reno} className={className}>
+            {children}
+        </Wrapper>
     );
 };
