078d68a9ad29c48a415b31d2924669c136d851d1
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-24 22:38:27 +0900

    move:フォルダ・コンポーネント名変更 SideBar->ToolSection
    feat:#96 UndoButtonとRenoButton追加
    
    chore:#56 終了タグの省略

--- src/components/ecosystem/ToolSection/index.tsx
+++ src/components/ecosystem/ToolSection/index.tsx
@@ -2,9 +2,13 @@ import ModeEditOutlineIcon from '@mui/icons-material/ModeEditOutline';
 import NearMeIcon from '@mui/icons-material/NearMe';
 import OpenWithIcon from '@mui/icons-material/OpenWith';
 import PanToolIcon from '@mui/icons-material/PanTool';
+import RedoIcon from '@mui/icons-material/Redo';
+import UndoIcon from '@mui/icons-material/Undo';
 import { MouseEventHandler, useState } from 'react';
 import IconButton from 'components/atoms/IconButton';
 import Container from 'components/layout/Container';
+import { RenoButton } from 'components/organisms/Edits/RenoButton';
+import { UndoButton } from 'components/organisms/Edits/UndoButton';
 import { PenTool } from 'components/organisms/Tools/PenTool';
 import { SpriteMoveTool } from 'components/organisms/Tools/SpriteMoveTool';
 import { SpriteSelectTool } from 'components/organisms/Tools/SpriteSelectTool';
@@ -19,7 +23,7 @@ type SideBarIconProps = {
 const SideBarIcon = ({ children, isActive, onClick }: SideBarIconProps) => {
     return (
         <IconButton
-            variant={isActive ? 'primary' : 'translucent'}
+            variant={isActive ? 'translucentDark' : 'translucent'}
             css={{
                 borderRadius: '5px',
             }}
@@ -31,16 +35,18 @@ const SideBarIcon = ({ children, isActive, onClick }: SideBarIconProps) => {
 };
 type SideBarButtonNames = 'select' | 'pen' | 'stagePan' | 'spriteMove';
 
-export const SideBar = () => {
+export type ToolSectionProps = {
+    className?: string;
+};
+
+export const ToolSection = ({ className }: ToolSectionProps) => {
     const [active, setActive] = useState<SideBarButtonNames | null>(null);
 
     return (
         <Container
+            className={className}
             css={(theme) => ({
-                width: '50px',
-                height: '100%',
-                backgroundColor: theme.colors.white,
-                boxShadow: '0 0 35px 0 rgba(0, 0, 0, .2)',
+                backgroundColor: theme.colors.neutral100,
                 flexDirection: 'column',
                 justifyContent: 'flex-start',
                 padding: '20px 0px',
@@ -53,7 +59,7 @@ export const SideBar = () => {
                     }}
                     isActive={active === 'select'}
                 >
-                    <NearMeIcon></NearMeIcon>
+                    <NearMeIcon />
                 </SideBarIcon>
             </SpriteSelectTool>
             <PenTool>
@@ -63,7 +69,7 @@ export const SideBar = () => {
                     }}
                     isActive={active === 'pen'}
                 >
-                    <ModeEditOutlineIcon></ModeEditOutlineIcon>
+                    <ModeEditOutlineIcon />
                 </SideBarIcon>
             </PenTool>
             <StageMoveTool>
@@ -73,7 +79,7 @@ export const SideBar = () => {
                     }}
                     isActive={active === 'stagePan'}
                 >
-                    <PanToolIcon></PanToolIcon>
+                    <PanToolIcon />
                 </SideBarIcon>
             </StageMoveTool>
             <SpriteMoveTool>
@@ -83,9 +89,19 @@ export const SideBar = () => {
                     }}
                     isActive={active === 'spriteMove'}
                 >
-                    <OpenWithIcon></OpenWithIcon>
+                    <OpenWithIcon />
                 </SideBarIcon>
             </SpriteMoveTool>
+            <UndoButton>
+                <SideBarIcon>
+                    <UndoIcon />
+                </SideBarIcon>
+            </UndoButton>
+            <RenoButton>
+                <SideBarIcon>
+                    <RedoIcon />
+                </SideBarIcon>
+            </RenoButton>
         </Container>
     );
 };
