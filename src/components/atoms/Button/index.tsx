4a9d50cc84ab85d613a47cde1b7de388ffa9a1d1
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-24 22:17:40 +0900

    chore:使用するThemeカラーのプロパティ名を更新

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,11 +1,16 @@
-import { Interpolation, Theme, FunctionInterpolation } from '@emotion/react';
+import { Theme, FunctionInterpolation } from '@emotion/react';
 import { ClassAttributes } from 'react';
 
-export type ButtonVariant = 'primary' | 'danger' | 'translucent' | 'warn' | 'netural';
+export type ButtonVariant =
+    | 'primary'
+    | 'danger'
+    | 'translucent'
+    | 'warn'
+    | 'netural'
+    | 'translucentDark';
 
 export type ButtonProps = {
     variant?: ButtonVariant;
-    css?: Interpolation<Theme>;
 } & ClassAttributes<HTMLButtonElement> &
     React.ButtonHTMLAttributes<HTMLButtonElement>;
 
@@ -13,36 +18,36 @@ const variants: {
     [key in ButtonVariant]: FunctionInterpolation<Theme>;
 } = {
     primary: (theme) => ({
-        color: theme.colors.primaryDark,
-        backgroundColor: theme.colors.primaryBrighter,
+        color: theme.colors.text,
+        backgroundColor: theme.colors.primary100,
         fontSize: theme.fontSize.md,
         border: 'none',
         padding: '6px 16px',
 
         ':active': {
-            backgroundColor: theme.colors.primaryBright,
+            backgroundColor: theme.colors.primary200,
         },
     }),
     danger: (theme) => ({
-        color: theme.colors.dangerDark,
-        backgroundColor: theme.colors.dangerBrighter,
+        color: theme.colors.danger700,
+        backgroundColor: theme.colors.danger100,
         fontSize: theme.fontSize.md,
         padding: '6px 16px',
         border: 'none',
 
         ':active': {
-            backgroundColor: theme.colors.dangerBright,
+            backgroundColor: theme.colors.danger300,
         },
     }),
     warn: (theme) => ({
-        color: theme.colors.warnDark,
-        backgroundColor: theme.colors.warnBrighter,
+        color: theme.colors.warn700,
+        backgroundColor: theme.colors.warn100,
         fontSize: theme.fontSize.md,
         padding: '6px 16px',
         border: 'none',
 
         ':active': {
-            backgroundColor: theme.colors.warnBright,
+            backgroundColor: theme.colors.warn300,
         },
     }),
     translucent: (theme) => ({
@@ -59,22 +64,35 @@ const variants: {
         },
     }),
     netural: (theme) => ({
-        color: theme.colors.neutralDark,
+        color: theme.colors.neutral700,
         backgroundColor: 'rgba(255,255,255,0)',
         fontSize: theme.fontSize.md,
         padding: '6px 16px',
         border: 'none',
         ':hover': {
-            backgroundColor: theme.colors.neutralBrighter,
+            backgroundColor: theme.colors.neutral100,
         },
         ':active': {
-            backgroundColor: theme.colors.neutralBright,
+            backgroundColor: theme.colors.neutral300,
+        },
+    }),
+    translucentDark: (theme) => ({
+        color: theme.colors.text,
+        backgroundColor: theme.colors.translucentMedium,
+        fontSize: theme.fontSize.md,
+        padding: '6px 16px',
+        border: 'none',
+        ':hover': {
+            backgroundColor: theme.colors.translucentMedium,
+        },
+        ':active': {
+            backgroundColor: theme.colors.translucentDeep,
         },
     }),
 };
 
-const Button = ({ variant = 'primary', css, ...props }: ButtonProps) => {
-    return <button {...props} css={[variants[variant], css]}></button>;
+const Button = ({ variant = 'primary', ...props }: ButtonProps) => {
+    return <button {...props} css={variants[variant]}></button>;
 };
 
 export default Button;
