a770f245163f3f33c6c748dae481731d9c96530d
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-11-24 23:10:35 +0900

    fix:#27 instanceIdプロパティに値追加
    `Prop `id` did not matc`エラーが生じるため
    
    feat:#39 themeのカラー変更

--- src/components/organisms/Pens/PenBlendModeSelector.tsx
+++ src/components/organisms/Pens/PenBlendModeSelector.tsx
@@ -1,51 +1,63 @@
 import Select from 'react-select';
 import { useRecoilState } from 'recoil';
 import Label from 'components/atoms/Label';
+import Container from 'components/layout/Container';
 import { BLEND_MODE_LABEL, PEN_BLEND_MODE_VALUE } from 'consts';
 import { penAtom } from 'dataflow/pens/penAtom';
+import { defaultTheme } from 'theme';
 
 const options = PEN_BLEND_MODE_VALUE.map((value) => ({
     value,
     label: BLEND_MODE_LABEL[value],
 }));
 
-export const PenBlendModeSelector = () => {
+export const PenBlendModeSelector = ({ className }: { className?: string }) => {
     const [penVal, setPen] = useRecoilState(penAtom);
     return (
         <>
-            <Label size="sm">ブレンドモード：</Label>
-            <Select
-                id="penBlendModeSelector"
-                css={(theme) => ({
-                    width: '180px',
-                    marginTop: '5px',
-                    fontSize: theme.fontSize.sm,
-                })}
-                theme={(theme) => ({
-                    ...theme,
-                    borderRadius: 5,
-                    colors: {
-                        ...theme.colors,
-                        primary25: '#F3F3F3',
-                        primary: '#5AA2AE',
-                    },
-                })}
-                isSearchable={false}
-                menuPosition={'fixed'}
-                options={options}
-                defaultValue={{
-                    value: penVal.prefs.blendMode,
-                    label: BLEND_MODE_LABEL[penVal.prefs.blendMode],
+            <Container
+                className={className}
+                css={{
+                    alignItems: 'start',
                 }}
-                onChange={(value) => {
-                    setPen((pen) =>
-                        pen.setPrefs((prefs) => ({
-                            ...prefs,
-                            blendMode: value?.value ?? prefs.blendMode,
-                        }))
-                    );
-                }}
-            />
+            >
+                <Label size="sm">ブレンドモード：</Label>
+                <Select
+                    id="penBlendModeSelector"
+                    instanceId="penBlendModeSelector"
+                    css={(theme) => ({
+                        width: '180px',
+                        marginTop: '5px',
+                        fontSize: theme.fontSize.sm,
+                        color: theme.colors.neutral900,
+                    })}
+                    theme={(theme) => ({
+                        ...theme,
+                        borderRadius: 0,
+                        colors: {
+                            ...theme.colors,
+
+                            primary25: defaultTheme.colors.neutral200,
+                            primary: defaultTheme.colors.neutral600,
+                        },
+                    })}
+                    isSearchable={false}
+                    menuPosition={'fixed'}
+                    options={options}
+                    defaultValue={{
+                        value: penVal.prefs.blendMode,
+                        label: BLEND_MODE_LABEL[penVal.prefs.blendMode],
+                    }}
+                    onChange={(value) => {
+                        setPen((pen) =>
+                            pen.setPrefs((prefs) => ({
+                                ...prefs,
+                                blendMode: value?.value ?? prefs.blendMode,
+                            }))
+                        );
+                    }}
+                />
+            </Container>
         </>
     );
 };
