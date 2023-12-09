1e10464b9f7978b03f80ec771a6245be91da0238
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-12-09 23:46:13 +0900

    refactor: フック名変更 useGLFilterWorkerGetter->useGLFilterExecutorGetter

--- src/hooks/filters/useGLFilterExecutorGetter.ts
+++ src/hooks/filters/useGLFilterExecutorGetter.ts
@@ -1,20 +1,21 @@
-import { FilterConfigs, FilterWorker, GLFilter } from 'application/filters/Filter';
+import { FilterConfigs, FilterExecutor, GLFilter } from 'application/filters/Filter';
 import { Rasterizedmage, SmartImage } from 'application/sprites';
 import { filterGLAtom } from 'dataflow/gl/filterGLAtom';
 import { useRecoilValueSyncReader } from 'hooks/useRecoilValueSyncReader';
 
-export const useGLFilterWorkerGetter = () => {
+export const useGLFilterExecutorGetter = () => {
     const getFilterGLSync = useRecoilValueSyncReader<WebGL2RenderingContext | null>();
 
     return <T extends FilterConfigs>(
         sprite: Rasterizedmage | SmartImage,
         filter: GLFilter<T>
-    ): FilterWorker<T> | null => {
+    ): FilterExecutor<T> | null => {
         const gl2 = getFilterGLSync(filterGLAtom);
+
         if (!gl2) {
             console.warn('gl2 is null');
             return null;
         }
-        return filter.getWorker(gl2, sprite);
+        return filter.getExecutor(gl2, sprite);
     };
 };
