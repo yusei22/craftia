0b9d7a723ed485cafb5bef540b6b0445b0df6803
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-12-09 22:24:52 +0900

    refactor:#4 クラス名変更 ISpriteWorker->ISpriteExecutor

--- src/application/ISpriteWorker.ts
+++ src/application/ISpriteWorker.ts
@@ -1,6 +1,6 @@
 import { RasterizedPreviewer } from './sprites/RasterizedPreviewer';
 import { SmartImagePreviewer } from './sprites/SmartImagePreviewer';
 
-export interface ISpriteWorker {
+export interface ISpriteExecutor {
     getPreviewSprite(): RasterizedPreviewer | SmartImagePreviewer;
 }
