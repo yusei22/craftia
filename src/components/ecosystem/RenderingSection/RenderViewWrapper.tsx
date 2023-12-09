bbc5bb3da71cd87e6ad76f7c5239a4e058d20a8c
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-12-09 23:24:54 +0900

    feat:#19 cssを更新

--- src/components/ecosystem/RenderingSection/RenderViewWrapper.tsx
+++ src/components/ecosystem/RenderingSection/RenderViewWrapper.tsx
@@ -1,7 +1,7 @@
 import { useDrag, useMove, usePinch, useWheel } from '@use-gesture/react';
 import { useRef } from 'react';
-import Wrapper from 'components/layout/Wrapper';
 import { RenderViewListeners } from 'dataflow';
+import { CheckerBoard } from 'components/atoms/CheckerBoard';
 type RenderViewWrapperProps = {
     children?: React.ReactNode;
     events: RenderViewListeners;
@@ -16,19 +16,14 @@ const RenderViewWrapper = ({ children, events }: RenderViewWrapperProps) => {
     useMove(events.onMove, { eventOptions: { passive: false }, target: wrapperRef });
 
     return (
-        <Wrapper
+        <CheckerBoard
+            ref={wrapperRef}
             css={{
                 touchAction: 'none',
-                background: `
-            repeating-conic-gradient(
-                #e5e5e5 0% 25%, transparent 0% 50%
-            ) 50% / 15px 15px
-            `,
             }}
-            ref={wrapperRef}
         >
             {children}
-        </Wrapper>
+        </CheckerBoard>
     );
 };
 export { RenderViewWrapper };
