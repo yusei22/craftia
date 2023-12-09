53eec3335d2a762bcd9f9f5bf1b0dedf61bbdf20
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-12-09 22:39:00 +0900

    docs: ソースコードのコメントを更新

--- src/application/core/web-gl2/buffer/VertexBuffer.ts
+++ src/application/core/web-gl2/buffer/VertexBuffer.ts
@@ -1,10 +1,28 @@
 import { AbstractBuffer } from './AbstractBuffer';
 import { GLBuffer } from './GLBuffer';
 
-/**gzVboに渡せる`ArrayBuffer` */
+/**頂点バッファで使用可能な型付き配列 */
 type VertexBuferData = Float32Array | Float64Array;
 
+/**
+ * WebGLへの転送を実現する、頂点データのラッパー
+ * 
+ * @example
+ * 
+ * const indexBuffer = new VertexBuffer(
+ *   new Float32Array([0, 1, 2, 1, 3, 2]),
+ *   WebGL2RenderingContext.STATIC_DRAW
+ * );
+ * 
+ */
 export class VertexBuffer extends AbstractBuffer<GLBuffer, VertexBuferData> {
+    /**
+     * GLBufferを返す
+     * 必要があればGLBuffeを新規作成する
+     * 
+     * @param gl WebGL2Context
+     * @returns GLBuffer
+     */
     protected generateGLBuffer(gl: WebGL2RenderingContext) {
         return (this.glBuffer = new GLBuffer(gl, gl.ARRAY_BUFFER));
     }
