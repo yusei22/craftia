db0d3fe35d2e2df2541e2982e40951d7385612ac
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-12-09 23:33:52 +0900

    refactor:フィルタ実行部分を削除
    move:Inversionフォルダに移動

---
+++ src/components/organisms/Filters/Inversion/index.tsx
@@ -0,0 +1,36 @@
+import { GLFilterButton, GLFilterControlPanelsProps } from '../GLFilterButton';
+import { Inversion, InversionConfig } from 'application/filters/Inversion';
+import Box from 'components/layout/Box';
+import { ConfirmButtons } from 'components/molecules/ConfirmButtons';
+
+export const InversionButton = ({ children }: { children?: React.ReactNode }) => {
+    return (
+        <GLFilterButton
+            filter={new Inversion()}
+            filterLabel={"反転"}
+            filterInitalConfigs={{}}
+            ControlPanels={InversionWindowContents}
+        >
+            {children}
+        </GLFilterButton>
+    );
+};
+
+const InversionWindowContents = ({
+    onCancel,
+    onConsent,
+}: GLFilterControlPanelsProps<InversionConfig>) => {
+    return (
+        <Box>
+            <ConfirmButtons
+                css={{
+                    width: '100%',
+                    justifyContent: 'end',
+                    marginTop: 10,
+                }}
+                onCancel={onCancel}
+                onConsent={onConsent}
+            />
+        </Box>
+    );
+};
