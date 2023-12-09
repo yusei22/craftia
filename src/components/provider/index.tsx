1488f0f11f0848a24063bc066d465198e44615c5
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-12-09 23:39:34 +0900

    feat:#11 ThemeProviderを追加

--- src/components/provider/index.tsx
+++ src/components/provider/index.tsx
@@ -3,21 +3,24 @@ import { PenProvider } from './PenProvider';
 import { RenderViewListenerProvider } from './RenderViewListenerProvider';
 import { RenderViewProvider } from './RenderViewProvider';
 import { SpriteTreeProvider } from './SpriteTreeProvider';
+import { ThemeProvider } from './ThemeProvider';
 import { FloatingWindowProvider } from './WindowProvider';
 
 const Provider = ({ children }: { children?: React.ReactNode }) => {
     return (
-        <RenderViewProvider>
-            <RenderViewListenerProvider>
-                <FilterGLProvider>
-                    <PenProvider>
-                        <SpriteTreeProvider>
-                            <FloatingWindowProvider>{children}</FloatingWindowProvider>
-                        </SpriteTreeProvider>
-                    </PenProvider>
-                </FilterGLProvider>
-            </RenderViewListenerProvider>
-        </RenderViewProvider>
+        <ThemeProvider>
+            <RenderViewProvider>
+                <RenderViewListenerProvider>
+                    <FilterGLProvider>
+                        <PenProvider>
+                            <SpriteTreeProvider>
+                                <FloatingWindowProvider>{children}</FloatingWindowProvider>
+                            </SpriteTreeProvider>
+                        </PenProvider>
+                    </FilterGLProvider>
+                </RenderViewListenerProvider>
+            </RenderViewProvider>
+        </ThemeProvider>
     );
 };
 
