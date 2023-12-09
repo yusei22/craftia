5d4dd161de92e8ff4c11ff2b40c0cebd71ac1cb6
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-12-09 22:37:42 +0900

    docs: ソースコードのコメントを更新

--- src/application/core/web-gl2/buffer/IndexBuffer.ts
+++ src/application/core/web-gl2/buffer/IndexBuffer.ts
@@ -4,6 +4,17 @@ import { GLBuffer } from './GLBuffer';
 /**gzIboに渡せる`ArrayBuffer` */
 type IndexBufferData = Int8Array | Int16Array | Int32Array;
 
+/**
+ * WebGLへの転送を実現する、インデックスデータのラッパー  
+ * 
+ * @example  
+ * 
+ * const indexBuffer = new IndexBuffer(  
+ *   new Int32Array([0, 1, 2, 1, 3, 2]),  
+ *   WebGL2RenderingContext.STATIC_DRAW  
+ * );  
+ * 
+ */
 export class IndexBuffer extends AbstractBuffer<GLBuffer, IndexBufferData> {
     protected generateGLBuffer(gl2: WebGL2RenderingContext) {
         return (this.glBuffer = new GLBuffer(gl2, gl2.ELEMENT_ARRAY_BUFFER));
