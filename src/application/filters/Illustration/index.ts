1a3a672742d97546c37397b82930adfade71c087
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-12-09 22:58:47 +0900

    refactor:#15 クラス名変更 IllustrationWorker->IllustrationExecutor

--- src/application/filters/Illustration/index.ts
+++ src/application/filters/Illustration/index.ts
@@ -1,20 +1,20 @@
-import { Bilateral, BilateralWorker } from '../Bilateral';
-import { GLFilter, FilterTarget, FilterWorker } from '../Filter';
-import { Sobel, SobelWorker } from '../Sobel';
+import { Bilateral, BilateralExecutor } from '../Bilateral';
+import { GLFilter, FilterTarget, FilterExecutor } from '../Filter';
+import { Sobel, SobelExecutor } from '../Sobel';
 import { AbstractContext2D, Context2D } from 'application/core/context-2d';
 import { Vec2 } from 'application/core/units';
 
 export interface IllustrationConfig {}
 
 export class Illustration extends GLFilter<IllustrationConfig> {
-    public getWorker(gl: WebGL2RenderingContext, sprite: FilterTarget): IllustrationWorker {
-        return new IllustrationWorker(gl, sprite);
+    public getExecutor(gl: WebGL2RenderingContext, sprite: FilterTarget): IllustrationExecutor {
+        return new IllustrationExecutor(gl, sprite);
     }
 }
 
-export class IllustrationWorker extends FilterWorker<IllustrationConfig> {
-    private bilateral: BilateralWorker;
-    private sobel: SobelWorker;
+export class IllustrationExecutor extends FilterExecutor<IllustrationConfig> {
+    private bilateral: BilateralExecutor;
+    private sobel: SobelExecutor;
     private context: AbstractContext2D;
 
     constructor(gl: WebGL2RenderingContext, sprite: FilterTarget) {
@@ -22,8 +22,8 @@ export class IllustrationWorker extends FilterWorker<IllustrationConfig> {
         const imageSize = new Vec2(sprite.image.width, sprite.image.height);
         this.context = new Context2D();
         this.context.viewport(imageSize);
-        this.bilateral = new Bilateral().getWorker(gl, sprite);
-        this.sobel = new Sobel().getWorker(gl, sprite);
+        this.bilateral = new Bilateral().getExecutor(gl, sprite);
+        this.sobel = new Sobel().getExecutor(gl, sprite);
     }
     public getResult() {
         return this.context.getCanvas();
