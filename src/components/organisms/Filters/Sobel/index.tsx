425bf15a79a42a9973de2a3f7529350268c38474
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-12-09 23:34:12 +0900

    refactor:フィルタ実行部分を削除
    move:Sobelフォルダに移動

---
+++ src/components/organisms/Filters/Sobel/index.tsx
@@ -0,0 +1,36 @@
+import { GLFilterButton, GLFilterControlPanelsProps } from '../GLFilterButton';
+import { Sobel, SobelConfig } from 'application/filters/Sobel';
+import Box from 'components/layout/Box';
+import { ConfirmButtons } from 'components/molecules/ConfirmButtons';
+
+export const SobelButton = ({ children }: { children?: React.ReactNode }) => {
+    return (
+        <GLFilterButton
+            filter={new Sobel()}
+            filterLabel={"ソーベル線画抽出"}
+            filterInitalConfigs={{}}
+            ControlPanels={SobelWindowContents}
+        >
+            {children}
+        </GLFilterButton>
+    );
+};
+
+const SobelWindowContents = ({
+    onCancel,
+    onConsent,
+}: GLFilterControlPanelsProps<SobelConfig>) => {
+    return (
+        <Box>
+            <ConfirmButtons
+                css={{
+                    width: '100%',
+                    justifyContent: 'end',
+                    marginTop: 10,
+                }}
+                onCancel={onCancel}
+                onConsent={onConsent}
+            />
+        </Box>
+    );
+};
