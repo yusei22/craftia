de5acc8eab23156258bfd4679e3f74491c0454f0
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-12-09 23:18:30 +0900

    refactor: ベースのコンポーネントをdiv要素に変更

--- src/components/atoms/CheckerBoard/index.tsx
+++ src/components/atoms/CheckerBoard/index.tsx
@@ -1,16 +1,24 @@
-import Wrapper, { WrapperProps } from 'components/layout/Wrapper';
+import { ClassAttributes, HTMLAttributes, forwardRef } from 'react';
 
-export const CheckerBoard = (props: Omit<WrapperProps, 'ref'>) => {
+export type CheckerBoardProps = HTMLAttributes<HTMLDivElement> & ClassAttributes<HTMLDivElement>;
+
+export const CheckerBoard = forwardRef<HTMLDivElement, CheckerBoardProps>(({ ...props }, ref) => {// eslint-disable-line
     return (
-        <Wrapper
-            css={(theme) => ({
-                margin: '0px 10px ',
+        <div
+            ref={ref}
+            css={theme => ({
+                position: 'relative',
+                width: 'fit-content',
+                height: 'fit-content',
+                margin: '0px',
+                padding: '0px',
+                fontSize: '0px',
                 background: `
                 repeating-conic-gradient(
-                    #e5e5e5 0% 25%, ${theme.colors.white} 0% 50%
+                    ${theme.colors.neutral300} 0% 25%, ${theme.colors.neutral100} 0% 50%
                 ) 50% / 15px 15px`,
             })}
             {...props}
-        />
+        ></div>
     );
-};
+});
