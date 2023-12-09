5df72e6b23b611125ad2a4815440a55dd843a4e5
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-12-09 23:15:41 +0900

    add: 画像の基本描画を行うスプライト

---
+++ src/application/sprites/BaseImage.ts
@@ -0,0 +1,69 @@
+import { BaseSprite, BaseSpritePrefs } from './BaseSprite';
+import { AbstractContext2D } from 'application/core/context-2d';
+import { ValueUpdater } from 'application/core/types';
+import { Vec2 } from 'application/core/units';
+import { CacheDrawConfig } from './Cache';
+
+export interface BaseImageDrawConfig extends BaseSpritePrefs {
+    readonly scale: Vec2;
+    readonly rotation: number;
+}
+
+export class BaseImage extends BaseSprite {
+    readonly configs: BaseImageDrawConfig;
+    readonly source: CanvasImageSource;
+
+    constructor(source: CanvasImageSource, configs: BaseImageDrawConfig) {
+        super();
+        this.configs = configs;
+        this.source = source;
+    }
+
+    protected _drawFunc(context: AbstractContext2D, configs: BaseImageDrawConfig) {
+        const anchorRerativeLoc = new Vec2(
+            configs.anchor.x * configs.scale.x,
+            configs.anchor.y * configs.scale.y
+        );
+        const startPoint = configs.globalLocation.sub(anchorRerativeLoc);
+
+        this.setconfig(context, {
+            line: null,
+            shadow: {
+                shadowBlur: configs.shadowBlur,
+                shadowColor: configs.shadowColor,
+                shadowOffset: configs.shadowOffset,
+            },
+            text: null,
+            fillStyle: null,
+            globalAlpha: configs.opacity,
+            globalCompositeOperation: configs.blendMode,
+            strokeStyle: null,
+        });
+
+        context.translate(configs.globalLocation);
+        context.rotate(configs.rotation);
+        context.translate(configs.globalLocation.times(-1));
+        context.drawImage(this.source, startPoint, configs.scale);
+    }
+    public setSouce(newVal: CanvasImageSource) {
+        return new BaseImage(newVal, this.configs);
+    }
+    public setConfigs(valOrUpdater: ValueUpdater<CacheDrawConfig> | CacheDrawConfig) {
+        const configs =
+            typeof valOrUpdater === 'function' ? valOrUpdater(this.configs) : valOrUpdater;
+        return new BaseImage(this.source, configs);
+    }
+
+    public drawFunc(context: AbstractContext2D): void {
+        this._drawFunc(context, this.configs)
+    }
+
+    public drawZoomFunc(context: AbstractContext2D, contextAux: AbstractContext2D, zoom: number) {
+        this._drawFunc(context, {
+            ...this.configs,
+            scale: this.configs.scale.times(zoom),
+            globalLocation: this.configs.globalLocation.times(zoom),
+            shadowBlur: this.configs.shadowBlur ? this.configs.shadowBlur * zoom : null,
+        });
+    }
+}
