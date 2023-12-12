e7961742496f6698abc274a7f924907ff4c34dac
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-12-12 20:55:34 +0900

    update:#34 ツール選択時に、ツールメニューのコンテンツをセットする #1

--- src/components/organisms/Tools/StageMoveTool.tsx
+++ src/components/organisms/Tools/StageMoveTool.tsx
@@ -2,6 +2,7 @@ import { useSetRecoilState } from 'recoil';
 import { Vec2 } from 'application/core/units';
 import Wrapper from 'components/layout/Wrapper';
 import { stageTransformAtom, renderViewListenersAtom } from 'dataflow';
+import { toolMenuAtom } from 'dataflow/toolMenues/toolMenuAtom';
 import { useRenderViewMouseGesture } from 'hooks/renderViews/useRenderViewMouseGesture';
 import { useRenderViewTouchGesture } from 'hooks/renderViews/useRenderViewTouchGesture';
 import { useRenderViewWheelGesture } from 'hooks/renderViews/useRenderViewWheelGesture';
@@ -12,6 +13,8 @@ export const StageMoveTool = ({ children }: { children?: React.ReactNode }) => {
     const { onMove } = useRenderViewMouseGesture();
     const setRenderViewListeners = useSetRecoilState(renderViewListenersAtom);
     const setStageTransform = useSetRecoilState(stageTransformAtom);
+
+    const setToolMenu = useSetRecoilState(toolMenuAtom);
 
     const onClick = () => {
         setRenderViewListeners({
@@ -28,6 +31,7 @@ export const StageMoveTool = ({ children }: { children?: React.ReactNode }) => {
             onMove,
             onClick: () => {},
         });
+        setToolMenu(() => <></>);
     };
 
     return <Wrapper onClick={onClick}> {children} </Wrapper>;
