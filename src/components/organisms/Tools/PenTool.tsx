e1219bd5c2565147c1d1703d2aa403bf119cdfdb
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-12-12 20:54:49 +0900

    update:#38 ツール選択時に、ツールメニューのコンテンツをセットする #1

--- src/components/organisms/Tools/PenTool.tsx
+++ src/components/organisms/Tools/PenTool.tsx
@@ -1,6 +1,8 @@
 import { useSetRecoilState } from 'recoil';
+import { PenMenues } from '../PenMenues';
 import Wrapper from 'components/layout/Wrapper';
 import { renderViewListenersAtom } from 'dataflow';
+import { toolMenuAtom } from 'dataflow/toolMenues/toolMenuAtom';
 import { usePen } from 'hooks/pens/usePen';
 import { useRenderViewMouseGesture } from 'hooks/renderViews/useRenderViewMouseGesture';
 import { useRenderViewTouchGesture } from 'hooks/renderViews/useRenderViewTouchGesture';
@@ -12,6 +14,8 @@ export const PenTool = ({ children }: { children?: React.ReactNode }) => {
     const { onMove } = useRenderViewMouseGesture();
     const { onDrag, onDragStart, onDragEnd } = usePen();
     const setRenderViewListeners = useSetRecoilState(renderViewListenersAtom);
+
+    const setToolMenu = useSetRecoilState(toolMenuAtom);
 
     const onClick = () => {
         setRenderViewListeners({
@@ -31,6 +35,7 @@ export const PenTool = ({ children }: { children?: React.ReactNode }) => {
             onMove,
             onClick: () => {},
         });
+        setToolMenu(PenMenues);
     };
 
     return <Wrapper onClick={onClick}> {children} </Wrapper>;
