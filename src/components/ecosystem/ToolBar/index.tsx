be0f552dcb7f1f6380618be9e93f21c7570f7f4d
yusei22 <126656209+PineBokkuri@users.noreply.github.com>
2023-12-15 21:07:47 +0900

    move:フォルダ名 ToolSection->ToolBar に変更のため #7
    feat:css変更
    
    chore:コンポーネント名更新
    
    feat:ツールボタンの並び替え

--- src/components/ecosystem/ToolBar/index.tsx
+++ src/components/ecosystem/ToolBar/index.tsx
@@ -1,83 +1,90 @@
-import ModeEditOutlineIcon from '@mui/icons-material/ModeEditOutline';
+import LayersIcon from '@mui/icons-material/Layers';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
 import NearMeIcon from '@mui/icons-material/NearMe';
 import OpenWithIcon from '@mui/icons-material/OpenWith';
 import PanToolIcon from '@mui/icons-material/PanTool';
 import RedoIcon from '@mui/icons-material/Redo';
 import UndoIcon from '@mui/icons-material/Undo';
 import { MouseEventHandler, useState } from 'react';
+import { useRecoilValue } from 'recoil';
+import { ColorTool } from '../../organisms/Tools/ColorTool';
 import IconButton from 'components/atoms/IconButton';
-import Box from 'components/layout/Box';
 import Container from 'components/layout/Container';
+import { LayerSectionVisibilityButton } from 'components/organisms/Displays/LayerSectionVisibilityButton';
 import { RenoButton } from 'components/organisms/Edits/RenoButton';
 import { UndoButton } from 'components/organisms/Edits/UndoButton';
 import { PenTool } from 'components/organisms/Tools/PenTool';
 import { SpriteMoveTool } from 'components/organisms/Tools/SpriteMoveTool';
 import { SpriteSelectTool } from 'components/organisms/Tools/SpriteSelectTool';
 import { StageMoveTool } from 'components/organisms/Tools/StageMoveTool';
+import { layerSectionVisibilityAtom } from 'dataflow/displays/layerSectionVisibilityAtom';
 
-type SideBarIconProps = {
+type ToolIconButtonProps = {
     children?: React.ReactNode;
     label?: string;
     isActive?: boolean;
     onClick?: MouseEventHandler<HTMLButtonElement>;
 };
 
-const SideBarIcon = ({ children, label, isActive, onClick }: SideBarIconProps) => {
+const ToolIconButton = ({ children, isActive, onClick }: ToolIconButtonProps) => {
     return (
         <IconButton
             variant="transparent"
             css={(theme) => ({
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
                 width: '100%',
-                padding: '5px 6px',
-                margin: '3px 0px',
-                borderRadius: 5,
-
-                backgroundColor: isActive ? theme.colors.neutral200 : '#0000',
-                color: theme.colors.neutral700,
+                padding: '4px 7px',
+                backgroundColor: isActive ? theme.colors.neutral300 : '#0000',
+                color: theme.colors.neutral800,
+                borderRadius: isActive ? '0% 20% 20% 0%' : '20%',
+                borderLeft: isActive ? `3px solid ${theme.colors.primary600}` : `3px solid #0000`,
                 ':hover': {
-                    color: theme.colors.neutral800,
+                    backgroundColor: theme.colors.neutral300,
                 },
             })}
             onClick={onClick}
         >
-            <Container
-                css={{
-                    width: '100%',
-                    flexFlow: 'column',
-                }}
-            >
-                {children}
-                <Box
-                    css={(theme) => ({
-                        fontSize: theme.fontSize.xs,
-                    })}
-                >
-                    {label}
-                </Box>
-            </Container>
+            {children}
         </IconButton>
     );
 };
-type SideBarButtonNames = 'select' | 'pen' | 'stagePan' | 'spriteMove';
 
-export type ToolSectionProps = {
+export type ToolButtonNames = 'select' | 'pen' | 'stagePan' | 'spriteMove';
+
+export type ToolBarProps = {
     className?: string;
 };
 
-export const ToolSection = ({ className }: ToolSectionProps) => {
-    const [active, setActive] = useState<SideBarButtonNames | null>(null);
+export const ToolBar = ({ className }: ToolBarProps) => {
+    const [active, setActive] = useState<ToolButtonNames | null>(null);
+    const layerSectionVisibility = useRecoilValue(layerSectionVisibilityAtom);
 
     return (
         <Container
             className={className}
-            css={{
+            css={(theme) => ({
+                width: '100%',
+                height: '100%',
                 flexDirection: 'column',
                 justifyContent: 'flex-start',
-                padding: '10px 5px',
-            }}
+                padding: 5,
+                backgroundColor: theme.colors.neutral200,
+
+                overflowY: 'auto',
+                '::-webkit-scrollbar': {
+                    display: 'none',
+                },
+                '-ms-overflow-style': {
+                    display: 'none',
+                },
+                scrollbarWidth: 'none',
+                gap: 6,
+            })}
         >
             <SpriteSelectTool>
-                <SideBarIcon
+                <ToolIconButton
                     onClick={() => {
                         setActive('select');
                     }}
@@ -85,50 +92,55 @@ export const ToolSection = ({ className }: ToolSectionProps) => {
                     label="select"
                 >
                     <NearMeIcon />
-                </SideBarIcon>
+                </ToolIconButton>
             </SpriteSelectTool>
             <PenTool>
-                <SideBarIcon
+                <ToolIconButton
                     onClick={() => {
                         setActive('pen');
                     }}
                     isActive={active === 'pen'}
-                    label="draw"
                 >
-                    <ModeEditOutlineIcon />
-                </SideBarIcon>
+                    <ModeEditIcon />
+                </ToolIconButton>
             </PenTool>
             <StageMoveTool>
-                <SideBarIcon
+                <ToolIconButton
                     onClick={() => {
                         setActive('stagePan');
                     }}
                     isActive={active === 'stagePan'}
-                    label="pan"
                 >
                     <PanToolIcon />
-                </SideBarIcon>
+                </ToolIconButton>
             </StageMoveTool>
             <SpriteMoveTool>
-                <SideBarIcon
+                <ToolIconButton
                     onClick={() => {
                         setActive('spriteMove');
                     }}
                     isActive={active === 'spriteMove'}
-                    label="move"
                 >
                     <OpenWithIcon />
-                </SideBarIcon>
+                </ToolIconButton>
             </SpriteMoveTool>
+            <LayerSectionVisibilityButton>
+                <ToolIconButton isActive={layerSectionVisibility}>
+                    <LayersIcon />
+                </ToolIconButton>
+            </LayerSectionVisibilityButton>
+            <ToolIconButton>
+                <ColorTool />
+            </ToolIconButton>
             <UndoButton>
-                <SideBarIcon label="undo">
+                <ToolIconButton>
                     <UndoIcon />
-                </SideBarIcon>
+                </ToolIconButton>
             </UndoButton>
             <RenoButton>
-                <SideBarIcon label="reno">
+                <ToolIconButton>
                     <RedoIcon />
-                </SideBarIcon>
+                </ToolIconButton>
             </RenoButton>
         </Container>
     );
